1ec4a51b07c54a92a900147107d72ca424cdf7fe
abenjamin9 <alexbenjamin@google.com>
2020-04-15 20:19:02 -0400

    Skeletal Node app for data-service.

---
+++ data-serving/data-service/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+// Default route handler.
+app.get("/", (req, res) => {
+    res.send('Data service under construction.');
+});
+
+// Start the server and listen on the default port.
+app.listen(port, () => {
+    console.log(`App running at http://localhost:${port}.`);
+})
