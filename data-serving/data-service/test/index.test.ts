16f8b9df9d967fa0557d3a4e74b6937b8cee30e7
abenjamin9 <alexbenjamin@google.com>
2020-04-16 19:08:43 -0400

    Controller and app unit tests.

---
+++ data-serving/data-service/test/index.test.ts
@@ -0,0 +1,9 @@
+import request from "supertest";
+import app from "../src/index";
+
+describe("GET /random-url", () => {
+    it("should return 404", (done) => {
+        request(app).get("/random-url")
+            .expect(404, done);
+    });
+});
