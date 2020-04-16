b2b56cc07042146ddef6f887d2de976a7ef8621b
abenjamin9 <alexbenjamin@google.com>
2020-04-16 19:08:04 -0400

    Controllers and routing for case data API read methods.

--- data-serving/data-service/src/index.ts
+++ data-serving/data-service/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 // Controllers (route handlers).
 import * as homeController from "./controllers/home";
+import * as caseController from "./controllers/case";
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.set("port", process.env.PORT || 3000);
 
 // Configure app routes.
 app.get("/", homeController.index);
+app.get("/cases/:id", caseController.getCase);
+app.get("/cases", caseController.listCases);
 
 export default app;
