e578e2aa487483f575108cd78497244c9d81faa3
abenjamin9 <alexbenjamin@google.com>
2020-04-17 15:33:17 -0400

    Linting and formatting code. Removed a (redundant) base config from eslintrc which was causing issues.

--- data-serving/data-service/src/index.ts
+++ data-serving/data-service/src/index.ts
@@ -1,17 +1,17 @@
-import express from "express";
+import express from 'express';
 
 // Controllers (route handlers).
-import * as homeController from "./controllers/home";
-import * as caseController from "./controllers/case";
+import * as homeController from './controllers/home';
+import * as caseController from './controllers/case';
 
 const app = express();
 
 // Express configuration.
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 // Configure app routes.
-app.get("/", homeController.index);
-app.get("/cases/:id", caseController.getCase);
-app.get("/cases", caseController.listCases);
+app.get('/', homeController.index);
+app.get('/cases/:id', caseController.getCase);
+app.get('/cases', caseController.listCases);
 
 export default app;
--- data-serving/data-service/test/index.test.ts
+++ data-serving/data-service/test/index.test.ts
@@ -1,9 +1,8 @@
-import request from "supertest";
-import app from "../src/index";
+import request from 'supertest';
+import app from '../src/index';
 
-describe("GET /random-url", () => {
-    it("should return 404", (done) => {
-        request(app).get("/random-url")
-            .expect(404, done);
+describe('GET /random-url', () => {
+    it('should return 404', (done) => {
+        request(app).get('/random-url').expect(404, done);
     });
 });
