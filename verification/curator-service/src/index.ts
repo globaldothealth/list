c9f17e49f216c61ce588d52df33f715b58631156
abenjamin9 <alexbenjamin@google.com>
2020-04-19 19:04:51 -0400

    Separate curator-service app/server.

--- verification/curator-service/src/index.ts
+++ verification/curator-service/src/index.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+
+// Express configuration.
+app.set('port', process.env.PORT || 3000);
 
 // Default route handler.
 app.get('/', (req, res) => {
     res.send('Curator service under construction.');
 });
 
-// Start the server and listen on the default port.
-app.listen(port, () => {
-    console.log(`App running at http://localhost:${port}.`);
-});
+export default app;
