f92ed64861e6f80869212090f68832bd6222c926
abenjamin9 <alexbenjamin@google.com>
2020-04-19 19:00:05 -0400

    Adding linter/formatter to curator-service.

--- verification/curator-service/src/index.ts
+++ verification/curator-service/src/index.ts
@@ -1,14 +1,14 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
 // Default route handler.
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.send('Curator service under construction.');
 });
 
 // Start the server and listen on the default port.
 app.listen(port, () => {
     console.log(`App running at http://localhost:${port}.`);
-})
+});
