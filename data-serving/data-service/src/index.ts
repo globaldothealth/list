16d7865bb401b75b936498c9d034f9d800e05272
axmb <63060924+axmb@users.noreply.github.com>
2020-04-20 10:54:11 -0400

    Rebasing dev branch

--- data-serving/data-service/src/index.ts
+++ data-serving/data-service/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 // Controllers (route handlers).
 import * as homeController from './controllers/home';
@@ -8,10 +9,13 @@ const app = express();
 
 // Express configuration.
 app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configure app routes.
-app.get('/', homeController.index);
-app.get('/cases/:id', caseController.getCase);
-app.get('/cases', caseController.listCases);
+app.get('/', homeController.get);
+app.get('/cases/:id', caseController.get);
+app.get('/cases', caseController.list);
+app.post('/cases', caseController.create);
 
 export default app;
