9b50ec7725da38c1b90ce9fb358e48cce93e2535
attwad <attwad@users.noreply.github.com>
2020-04-20 16:05:02 +0200

    Add config api routes and chose a different default port for curation service (#11)
    * chose a different default port for curation service to not conflict with data service service
    
    * add not implemented api routes
    
    * merge tests

--- verification/curator-service/src/index.ts
+++ verification/curator-service/src/index.ts
@@ -2,13 +2,22 @@ import express from 'express';
 
 // Controllers (route handlers).
 import * as homeController from './controllers/home';
+import * as sourcesController from './controllers/sources';
 
 const app = express();
 
 // Express configuration.
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 
-// Configure app routes.
+// Configure frontend app routes.
 app.get('/', homeController.index);
 
+// Configure API app routes.
+var apiRouter = express.Router();
+apiRouter.get('/', sourcesController.list);
+apiRouter.get('/:id', sourcesController.get);
+apiRouter.post('/', sourcesController.create);
+apiRouter.put('/:id', sourcesController.update);
+apiRouter.delete('/:id', sourcesController.del);
+app.use('/api', apiRouter);
 export default app;
---
+++ verification/curator-service/test/sources.test.ts
@@ -0,0 +1,29 @@
+import request from 'supertest';
+import app from '../src/index';
+
+describe('GET', () => {
+    it('list should return 501 Not Implemented', (done) => {
+        request(app).get('/api').expect(501, done);
+    });
+    it('one item should return 501 Not Implemented', (done) => {
+        request(app).get('/api/42').expect(501, done);
+    });
+});
+
+describe('PUT', () => {
+    it('should return 501 Not Implemented', (done) => {
+        request(app).put('/api/id').expect(501, done);
+    });
+});
+
+describe('POST', () => {
+    it('should return 501 Not Implemented', (done) => {
+        request(app).post('/api').expect(501, done);
+    });
+});
+
+describe('DELETE', () => {
+    it('should return 501 Not Implemented', (done) => {
+        request(app).delete('/api/42').expect(501, done);
+    });
+});
