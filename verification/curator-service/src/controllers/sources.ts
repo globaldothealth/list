89abbf26e896cacc0b62fd16d04afee6ede1bfc4
Timothe Faudot <timothe@google.com>
2020-04-20 15:51:08 +0200

    add not implemented api routes

---
+++ verification/curator-service/src/controllers/sources.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express';
+
+/**
+ * List the sources.
+ */
+export const list = (req: Request, res: Response) => {
+    res.sendStatus(501);
+    return;
+};
+
+/**
+ * Get a single source.
+ */
+export const get = (req: Request, res: Response) => {
+    res.sendStatus(501);
+    return;
+};
+
+/**
+ * Update a single source.
+ */
+export const update = (req: Request, res: Response) => {
+    res.sendStatus(501);
+    return;
+};
+
+/**
+ * Create a single source.
+ */
+export const create = (req: Request, res: Response) => {
+    res.sendStatus(501);
+    return;
+};
+
+/**
+ * Delete a single source.
+ */
+export const del = (req: Request, res: Response) => {
+    res.sendStatus(501);
+    return;
+};
