c7dbc09389b76b920941001c8ea16b30b961afc5
Timothe Faudot <timothe@google.com>
2020-04-20 15:54:05 +0200

    merge tests

--- verification/curator-service/test/sources.test.ts
+++ verification/curator-service/test/sources.test.ts
@@ -1,14 +1,11 @@
 import request from 'supertest';
 import app from '../src/index';
 
-describe('GET /', () => {
-    it('should return 501 Not Implemented', (done) => {
+describe('GET', () => {
+    it('list should return 501 Not Implemented', (done) => {
         request(app).get('/api').expect(501, done);
     });
-});
-
-describe('GET', () => {
-    it('should return 501 Not Implemented', (done) => {
+    it('one item should return 501 Not Implemented', (done) => {
         request(app).get('/api/42').expect(501, done);
     });
 });
