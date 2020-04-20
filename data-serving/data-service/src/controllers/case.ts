1e2ca3cfa9456b3800b8ced0004b1eb81825c32d
axmb <63060924+axmb@users.noreply.github.com>
2020-04-20 10:54:11 -0400

    Base create method for case data (#12)
    * Add a simple create method, along with setting up body parsing and request validation.
    
    * Updating controller method names (aligning curation/data on this convention).
    
    * Small typo fix.

--- data-serving/data-service/src/controllers/case.ts
+++ data-serving/data-service/src/controllers/case.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express';
+import { check, validationResult } from 'express-validator';
 
 /**
  * Get a specific case.
  *
  * Handles HTTP GET /cases/:id.
  */
-export const getCase = (req: Request, res: Response) => {
+export const get = (req: Request, res: Response) => {
     res.send(`Triggered get case with ID ${req.params.id}.`);
 };
 
@@ -14,6 +15,28 @@ export const getCase = (req: Request, res: Response) => {
  *
  * Handles HTTP GET /cases.
  */
-export const listCases = (req: Request, res: Response) => {
+export const list = (req: Request, res: Response) => {
     res.send('Triggered list cases.');
+};
+
+/**
+ * Create a case.
+ *
+ * Handles HTTP POST /cases.
+ *
+ * For now, just attempts to parse an "age" field and return it in the response.
+ * Returns 422 if "age" isn't present in the request body.
+ */
+export const create = async (req: Request, res: Response) => {
+    await check('age', 'Age must be a valid number')
+        .not()
+        .isEmpty()
+        .isNumeric()
+        .run(req);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
+    res.send(`Triggered create case with age: ${req.body.age}`);
 };
