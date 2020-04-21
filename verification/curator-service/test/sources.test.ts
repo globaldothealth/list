b8ae7b95c22bdef36e526c32071053e9dc99ef2b
Timothe Faudot <timothe@google.com>
2020-04-21 11:52:27 +0200

    add sources suffix to /api

--- verification/curator-service/test/sources.test.ts
+++ verification/curator-service/test/sources.test.ts
@@ -3,27 +3,27 @@ import app from '../src/index';
 
 describe('GET', () => {
     it('list should return 501 Not Implemented', (done) => {
-        request(app).get('/api').expect(501, done);
+        request(app).get('/api/sources').expect(501, done);
     });
     it('one item should return 501 Not Implemented', (done) => {
-        request(app).get('/api/42').expect(501, done);
+        request(app).get('/api/sources/42').expect(501, done);
     });
 });
 
 describe('PUT', () => {
     it('should return 501 Not Implemented', (done) => {
-        request(app).put('/api/id').expect(501, done);
+        request(app).put('/api/sources/id').expect(501, done);
     });
 });
 
 describe('POST', () => {
     it('should return 501 Not Implemented', (done) => {
-        request(app).post('/api').expect(501, done);
+        request(app).post('/api/sources/').expect(501, done);
     });
 });
 
 describe('DELETE', () => {
     it('should return 501 Not Implemented', (done) => {
-        request(app).delete('/api/42').expect(501, done);
+        request(app).delete('/api/sources/42').expect(501, done);
     });
 });
