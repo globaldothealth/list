968277ffcdd25a35eb91b049feddd07d677ceda4
abenjamin9 <alexbenjamin@google.com>
2020-04-22 10:39:40 -0400

    Remove default driver references.

--- data-serving/data-service/src/util/validate-env.ts
+++ data-serving/data-service/src/util/validate-env.ts
@@ -3,7 +3,6 @@ import { CleanEnv, cleanEnv, str, port } from 'envalid';
 export default function validateEnv(): Readonly<{
     DB_CONNECTION_STRING: string;
     DB_NAME: string;
-    DB_COLLECTION: string;
     PORT: number;
 }> &
     CleanEnv & {
@@ -18,10 +17,6 @@ export default function validateEnv(): Readonly<{
         DB_NAME: str({
             desc: 'Name of the database to be connected to.',
             devDefault: 'dev',
-        }),
-        DB_COLLECTION: str({
-            desc: 'Name of the MongoDB collection to operate on.',
-            devDefault: 'covid19',
         }),
         PORT: port({ default: 3000 }),
     });
