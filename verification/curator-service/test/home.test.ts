ff483d93db6b3dd2a51196982928701b702ef2f4
abenjamin9 <alexbenjamin@google.com>
2020-04-22 21:57:47 -0400

    Render summary from data-service API on the curator-service home page.

--- verification/curator-service/test/home.test.ts
+++ verification/curator-service/test/home.test.ts
@@ -1,8 +1,28 @@
 import request from 'supertest';
 import app from '../src/index';
+import axios, { AxiosResponse } from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('GET /', () => {
     it('should return 200 OK', (done) => {
+        const cases = [
+            {
+                _id: 'abc123',
+                outcome: 'recovered',
+                date: '2020-04-21',
+            },
+        ];
+        const axiosResponse: AxiosResponse = {
+            data: cases,
+            status: 200,
+            statusText: 'OK',
+            config: {},
+            headers: {},
+        };
+        mockedAxios.get.mockResolvedValue(axiosResponse);
+
         request(app).get('/').expect(200, done);
     });
 });
