c2c72d922a7d934423da1ce59af696017aa71ad8
attwad <attwad@users.noreply.github.com>
2020-04-23 09:44:09 +0200

    Implement CRUD api for sources (#29)
    * get tests added
    
    * added update test
    
    * added create tests
    
    * added del tests
    
    * mention id in 404s
    
    * removed test mongo setup
    
    * using saved doc directly

--- verification/curator-service/src/index.ts
+++ verification/curator-service/src/index.ts
@@ -1,12 +1,14 @@
-import express from 'express';
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import express from 'express';
 import mongoose from 'mongoose';
-
 // Controllers (route handlers).
 import * as homeController from './controllers/home';
 import * as sourcesController from './controllers/sources';
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 dotenv.config();
 
@@ -32,6 +34,7 @@ app.use('/api', apiRouter);
         await mongoose.connect(process.env.DB_CONNECTION_STRING || '', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            useFindAndModify: false,
         });
         console.log('Connected to the database');
     } catch (e) {
