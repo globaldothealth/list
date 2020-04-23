33f6a168ce678ddae0a78b55b6db9b85e39154c5
axmb <63060924+axmb@users.noreply.github.com>
2020-04-23 02:57:24 -0400

    Update mongoose options, case API validation mechanism, and corresponding testing (#30)
    * Updating some mongoose options in data-service.
    
    * Updating validation and testing re: new options.
    
    * Small syntactic improvement.

--- data-serving/data-service/src/controllers/case.ts
+++ data-serving/data-service/src/controllers/case.ts
@@ -32,24 +32,21 @@ export const list = async (req: Request, res: Response): Promise<void> => {
  * Handles HTTP POST /api/cases.
  */
 export const create = async (req: Request, res: Response): Promise<void> => {
-    await check('outcome', `Outcome must be one of: ${validOutcomes}`)
-        .isIn(validOutcomes)
-        .run(req);
-    await check('date', 'Date must be a valid ISO 8601 date.')
-        .isISO8601()
-        .run(req);
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        res.status(422).json({ errors: errors.array() });
+    try {
+        const c = new Case({
+            date: req.body.date,
+            outcome: req.body.outcome,
+        });
+        const result = await c.save();
+        res.json(result);
+    } catch (err) {
+        if (err.name === 'ValidationError') {
+            res.status(422).json(err.message);
+            return;
+        }
+        res.status(500).json(err.message);
         return;
     }
-
-    const c = new Case({
-        date: req.body.date,
-        outcome: req.body.outcome,
-    });
-    const result = await c.save();
-    res.json(result);
 };
 
 /**
@@ -58,14 +55,24 @@ export const create = async (req: Request, res: Response): Promise<void> => {
  * Handles HTTP PUT /api/cases/:id.
  */
 export const update = async (req: Request, res: Response): Promise<void> => {
-    const c = await Case.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-    });
-    if (!c) {
-        res.status(404).send(`Case with ID ${req.params.id} not found.`);
+    try {
+        const c = await Case.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if (!c) {
+            res.status(404).send(`Case with ID ${req.params.id} not found.`);
+            return;
+        }
+        res.json(c);
+    } catch (err) {
+        if (err.name === 'ValidationError') {
+            res.status(422).json(err.message);
+            return;
+        }
+        res.status(500).json(err.message);
         return;
     }
-    res.json(c);
 };
 
 /**
