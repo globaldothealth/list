abd4161019cf73393fb8221569c60ab4c3275af8
Timothé <attwad@users.noreply.github.com>
2020-04-24 16:52:52 +0200

    add basic model validation tests (#42)
    * add basic model validation tests
    
    * remove unused import

---
+++ verification/curator-service/api/test/configs.schema.test.ts
@@ -0,0 +1,29 @@
+import { Source } from '../src/model/source';
+
+describe('Source schema', () => {
+    it('should build a valid source', () => {
+        const errors = new Source({
+            name: 'test source',
+            origin: {
+                url: 'http://foo.bar',
+            },
+            automation: {
+                scheduleExpression: 'cron(0 12 * * ? *)',
+            },
+        }).validateSync();
+        expect(errors).toBeUndefined();
+    });
+    it('should fail validation if source is invalid', () => {
+        const errors = new Source({}).validateSync();
+        expect(errors).toBeDefined();
+        expect(errors?.toString()).toMatch('Enter a name');
+    });
+    it('should fail validation without origin url', () => {
+        const errors = new Source({
+            name: 'test source',
+            origin: {},
+        }).validateSync();
+        expect(errors).toBeDefined();
+        expect(errors?.toString()).toMatch('Enter an origin');
+    });
+});
