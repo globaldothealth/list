18130132081853b293e848d8e6cd7e47d02acaa3
Timothé <attwad@users.noreply.github.com>
2020-04-24 15:14:30 +0200

    Completed schemas for sources (#39)
    * added json schema validator for curator sources db
    
    * completed schema for sources
    
    * remove unused import
    
    * move source schema
    
    * use array fields and left todo for validator

--- verification/curator-service/api/src/model/source.ts
+++ verification/curator-service/api/src/model/source.ts
@@ -15,17 +15,67 @@ const originSchema = new mongoose.Schema({
     },
 });
 
+interface Field {
+    name: string;
+    regexp: string;
+}
+
+const fieldSchema = new mongoose.Schema({
+    url: {
+        type: String,
+        required: 'Enter a dotted.field.name',
+    },
+    regexp: {
+        type: String,
+        required: 'Enter a field extraction regexp',
+    },
+});
+
+interface Parsing {
+    fields: Array<Field>;
+}
+
+const parsingSchema = new mongoose.Schema({
+    fields: [fieldSchema],
+});
+
+interface Automation {
+    name: string;
+    tag: string;
+    active: boolean;
+    scheduleExpression: string;
+    parsing: Parsing;
+}
+
+const automationSchema = new mongoose.Schema({
+    name: {
+        name: {
+            type: String,
+            required: 'Enter an automation rule name',
+        },
+        tag: String,
+        active: Boolean,
+        // TODO: Should probably use a validator for "cron(..." or "rate(...".
+        scheduleExpression: String,
+        parsing: parsingSchema,
+    },
+});
+
 const sourceSchema = new mongoose.Schema({
     name: {
         type: String,
         required: 'Enter a name',
     },
     origin: originSchema,
+    format: String,
+    automation: automationSchema,
 });
 
 type SourceDocument = mongoose.Document & {
     name: string;
     origin: Origin;
+    format: string;
+    automation: Automation;
 };
 
 export const Source = mongoose.model<SourceDocument>('Source', sourceSchema);
---
+++ verification/curator-service/api/src/schemas/README.md
@@ -0,0 +1,3 @@
+# Mongodb schema validators.
+
+This directory contains schema validators for the various collections stored in mongodb.
