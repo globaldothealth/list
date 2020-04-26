c01c5ce45d4665060ce389807c6ccb96411a51a8
axmb <alexbenjamin@google.com>
2020-04-26 18:10:11 -0400

    Shuffling linelist data fetching to an API endpoint on curator-service.

---
+++ verification/curator-service/api/src/controllers/cases.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import axios from 'axios';
+
+export interface Case {
+    _id: string;
+    outcome: string;
+    date: Date;
+}
+
+/**
+ * List linelist case data from dataserver.
+ *
+ * Handles HTTP GET /api/cases.
+ */
+export const list = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const response = await axios.get<[Case]>('/cases');
+        const cases = response.data;
+        res.json(cases);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+};
