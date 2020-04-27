691b1f8bab078903691bf32f617356b27a4cfbc0
axmb <alexbenjamin@google.com>
2020-04-27 19:02:07 -0400

    Render stylized app in material.

--- verification/curator-service/ui/src/index.tsx
+++ verification/curator-service/ui/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import 'typeface-roboto';
 
 ReactDOM.render(
   <React.StrictMode>
