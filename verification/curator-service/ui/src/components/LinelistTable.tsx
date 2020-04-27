b4e3839704da4265eb0d8c2f3e7738977653a642
axmb <alexbenjamin@google.com>
2020-04-26 21:29:38 -0400

    Add LinelistTable component and testing.

---
+++ verification/curator-service/ui/src/components/LinelistTable.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import axios from 'axios';
+import { Case } from '../../../api/src/controllers/cases'
+import LinelistRow from "./LinelistRow";
+import Table from 'react-bootstrap/Table';
+
+interface TableState {
+    errorMessage: string,
+    isLoaded: boolean,
+    linelist: Case[]
+}
+
+export default class LinelistTable extends React.Component<{}, TableState> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            errorMessage: '',
+            isLoaded: false,
+            linelist: []
+        };
+    }
+
+    async componentDidMount() {
+        try {
+            const response = await axios.get<Case[]>('/api/cases');
+            this.setState({
+                isLoaded: true,
+                linelist: response.data
+            });
+        } catch (e) {
+            this.setState({
+                isLoaded: true,
+                errorMessage: e.message
+            });
+        }
+    }
+
+    render() {
+        const { errorMessage, isLoaded, linelist } = this.state;
+        if (errorMessage.length > 0) {
+            return <div>Error: {errorMessage}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <Table bordered hover striped>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Date</th>
+                            <th>Outcome</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {linelist.map(item => (
+                            <LinelistRow
+                                case={item}
+                                key={item._id}
+                            />
+                        ))}
+                    </tbody>
+                </Table>
+            )
+        }
+    }
+}
