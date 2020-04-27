3cbfa475d70f05daadb8f073f97e34ff848c64da
Timothé <attwad@users.noreply.github.com>
2020-04-27 14:50:15 +0200

    added pagination support to /api/sources (#47)
    * added pagination support to /api/sources
    
    * simpler limit expr

--- verification/curator-service/api/src/controllers/sources.ts
+++ verification/curator-service/api/src/controllers/sources.ts
@@ -3,10 +3,36 @@ import { Source } from '../model/source';
 
 /**
  * List the sources.
+ * Response will contain {sources: [list of sources]}
+ * and potentially another nextPage: <num> if more results are available.
+ * Default values of 10 for limit and 1 for page is used.
  */
 export const list = async (req: Request, res: Response): Promise<void> => {
-    const docs = await Source.find({});
-    res.json(docs);
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    if (page < 1) {
+        res.status(422).json('page must be > 0');
+        return;
+    }
+    if (limit < 1) {
+        res.status(422).json('limit must be > 0');
+        return;
+    }
+    const docs = await Source.find({})
+        .skip(limit * (page - 1))
+        .limit(limit + 1);
+    // If we have more items than limit, add a response param
+    // indicating that there is more to fetch on the next page.
+    if (docs.length == limit + 1) {
+        docs.splice(limit);
+        res.json({
+            sources: docs,
+            nextPage: page + 1,
+        });
+        return;
+    }
+    // If we fetched all available data, just return it.
+    res.json({ sources: docs });
 };
 
 /**
