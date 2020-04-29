679b108b9d2490f02065d869fa120a5b99217711
Timothé <attwad@users.noreply.github.com>
2020-04-29 21:13:15 +0200

    Add react-router to map /new to a WIP page (#56)
    * added react router dom npm dep
    
    * added types for react router
    
    * route /new to new page
    
    * Move out of dev deps
    
    * remove depdeps altogether

--- verification/curator-service/ui/src/components/App.test.tsx
+++ verification/curator-service/ui/src/components/App.test.tsx
@@ -1,8 +1,13 @@
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>, div);
+  expect(div.textContent).toContain('epid');
 });
