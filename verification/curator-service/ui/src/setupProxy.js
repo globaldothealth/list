ab8a5c42547cdedc38505bea98978512dcdae1a5
Timothe Faudot <timothe@google.com>
2020-05-07 21:32:10 +0200

    proxy requests from 3002 to 3001 so that the UI only talks to itself

---
+++ verification/curator-service/ui/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = function (app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            // Proxy API requests to curator service.
+            target: process.env.REACT_APP_PROXY_URL || "http://localhost:3001",
+            changeOrigin: true,
+        })
+    );
+};
