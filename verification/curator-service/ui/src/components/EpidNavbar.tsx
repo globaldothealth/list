722adfaf21c66bbd5c835a7e1fad396d1f4409be
Timothe Faudot <timothe@google.com>
2020-05-07 10:49:50 +0200

    remove unused import

--- verification/curator-service/ui/src/components/EpidNavbar.tsx
+++ verification/curator-service/ui/src/components/EpidNavbar.tsx
@@ -1,7 +1,6 @@
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
 import HomeIcon from '@material-ui/icons/Home';
 import IconButton from '@material-ui/core/IconButton';
 import React from "react";
