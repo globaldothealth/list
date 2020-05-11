041bdaae4773d0caacf68b6606245f7cf668b08f
Kelly Moran <khmoran@google.com>
2020-05-11 13:44:45 -0400

    Pull the source schema + interface into its own file

---
+++ data-serving/data-service/src/model/source.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const fieldRequiredValidator = [
+    function (this: SourceDocument) {
+        return this.id == null && this.url == null && this.other == null;
+    },
+    'Source must specify id, url, or other',
+];
+
+export const sourceSchema = new mongoose.Schema(
+    {
+        id: {
+            type: String,
+            required: fieldRequiredValidator,
+        },
+        url: {
+            type: String,
+            required: fieldRequiredValidator,
+        },
+        other: {
+            type: String,
+            required: fieldRequiredValidator,
+        },
+    },
+    { strict: true },
+);
+
+export type SourceDocument = mongoose.Document & {
+    id: string;
+    url: string;
+    other: string;
+};
+
+export const Source = mongoose.model<SourceDocument>('Source', sourceSchema);
