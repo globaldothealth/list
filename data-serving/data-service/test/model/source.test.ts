014383b461bceda8e4456eb36260361b3556bc5a
Kelly Moran <khmoran@google.com>
2020-05-11 13:44:45 -0400

    Pull the source schema + interface into its own file

---
+++ data-serving/data-service/test/model/source.test.ts
@@ -0,0 +1,30 @@
+import { Error } from 'mongoose';
+import { Source } from '../../src/model/source';
+
+describe('validate', () => {
+    it('empty source is invalid', async () => {
+        return new Source({}).validate((e) => {
+            expect(e.name).toBe(Error.ValidationError.name);
+        });
+    });
+
+    it('a source with only an id is valid', async () => {
+        return new Source({ id: 'abc' }).validate();
+    });
+
+    it('a source with only url is valid', async () => {
+        return new Source({ url: 'http://abc.def' }).validate();
+    });
+
+    it('a source with only "other" is valid', async () => {
+        return new Source({ other: 'ghi' }).validate();
+    });
+
+    it('a fully specified source is valid', async () => {
+        return new Source({
+            id: 'abc',
+            url: 'http://abc.def',
+            other: 'ghi',
+        }).validate();
+    });
+});
