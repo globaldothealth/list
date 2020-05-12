00db06059842921716859948b2190e7bd1647663
Timothe Faudot <timothe@google.com>
2020-05-12 13:56:58 +0200

    serve react app from express, does not work yet

--- verification/curator-service/ui/src/setupProxy.js
+++ verification/curator-service/ui/src/setupProxy.js
@@ -1,11 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
-    app.use(
-        /\/(api|auth)/,
-        createProxyMiddleware({
-            // Proxy API requests to curator service.
-            target: process.env.REACT_APP_PROXY_URL || "http://localhost:3001",
-            changeOrigin: true,
-        })
-    );
+    if (process.env.REACT_APP_PROXY_URL) {
+        app.use(
+            /\/(api|auth)/,
+            createProxyMiddleware({
+                // Proxy API requests to curator service when running the development server.
+                target: process.env.REACT_APP_PROXY_URL,
+                changeOrigin: true,
+            })
+        );
+    }
 };
