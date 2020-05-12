9deb2ee96ca6518599464a32cdf7095b769f38cb
Timothe Faudot <timothe@google.com>
2020-05-12 11:13:46 +0200

    comments dev script

--- dev/run_stack.sh
+++ dev/run_stack.sh
@@ -2,8 +2,9 @@
 set -e
 # Store current directory.
 pushd `pwd`
+# We have to run docker-compose from this directory for it to pick up the .env file.
 cd `dirname "$0"`
-# We have to run docker-compose from the root directory for it to pick up the .env file.
 docker-compose -f docker-compose.yml config
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build
+# Restore directory.
 popd
