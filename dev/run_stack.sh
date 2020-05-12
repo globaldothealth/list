8c9c2523740e7956d954d431845e46d61451c738
Timothe Faudot <timothe@google.com>
2020-05-12 11:06:00 +0200

    fix docker build with new context

--- dev/run_stack.sh
+++ dev/run_stack.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 set -e
-docker-compose -f `dirname "$0"`/docker-compose.yml -f `dirname "$0"`/docker-compose.dev.yml up --build
+# Store current directory.
+pushd `pwd`
+cd `dirname "$0"`
+# We have to run docker-compose from the root directory for it to pick up the .env file.
+docker-compose -f docker-compose.yml config
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build
+popd
