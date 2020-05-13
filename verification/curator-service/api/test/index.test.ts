be061514f629586045e8cbce4ff93d9fc53e7859
Timothe Faudot <timothe@google.com>
2020-05-13 12:47:49 +0200

    serve UI in curator server

--- verification/curator-service/api/test/index.test.ts
+++ verification/curator-service/api/test/index.test.ts
@@ -20,7 +20,7 @@ afterAll(() => {
 });
 
 describe('GET /random-url', () => {
-    it('should return 404', (done) => {
-        request(app).get('/random-url').expect(404, done);
+    it('should return index page', (done) => {
+        request(app).get('/random-url').expect(200, done);
     });
 });
