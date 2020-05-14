80e9246fa0217d4606f692fb69bdb99a7c6750f2
Kelly Moran <khmoran@google.com>
2020-05-14 14:59:35 -0400

    Add open-ended date range tests

--- data-serving/data-service/test/model/date-range.test.ts
+++ data-serving/data-service/test/model/date-range.test.ts
@@ -1,6 +1,6 @@
 import { DateRangeDocument, dateRangeSchema } from '../../src/model/date-range';
 
-import fullDateRange from './data/date-range.full.json';
+import fullModel from './data/date-range.full.json';
 import mongoose from 'mongoose';
 
 const DateRange = mongoose.model<DateRangeDocument>(
@@ -13,7 +13,21 @@ describe('validate', () => {
         return new DateRange({}).validate();
     });
 
+    it('an open-ended date range document is valid', async () => {
+        const openEndedModel = Object.assign({}, fullModel);
+        delete fullModel.end;
+
+        return new DateRange(openEndedModel).validate();
+    });
+
+    it('an open-start date range document is valid', async () => {
+        const openStartModel = Object.assign({}, fullModel);
+        delete fullModel.start;
+
+        return new DateRange(openStartModel).validate();
+    });
+
     it('a fully specified date range document is valid', async () => {
-        return new DateRange(fullDateRange).validate();
+        return new DateRange(fullModel).validate();
     });
 });
