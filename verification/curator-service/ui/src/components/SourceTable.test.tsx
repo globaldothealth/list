af95bfc19f82f1030cf8e8bc2b2312f5c4b64154
axmb <alexbenjamin@google.com>
2020-05-13 20:09:53 -0400

    Baseline unit test for SourceTable component.

---
+++ verification/curator-service/ui/src/components/SourceTable.test.tsx
@@ -0,0 +1,57 @@
+import '@testing-library/jest-dom/extend-expect';
+
+import SourceTable from './SourceTable';
+import React from 'react';
+import axios from 'axios';
+import { render } from '@testing-library/react';
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+afterEach(() => {
+  mockedAxios.get.mockClear();
+  mockedAxios.delete.mockClear();
+  mockedAxios.post.mockClear();
+  mockedAxios.put.mockClear();
+});
+
+it('loads and displays sources', async () => {
+  const sources = [
+    {
+      _id: 'abc123',
+      name: 'source name',
+      format: 'format',
+      origin: {
+        url: "origin url",
+        license: "origin license",
+      },
+      automation: {
+        name: 'automation name',
+        tag: 'automation tag',
+        active: true,
+        scheduleExpression: "automation schedule",
+        parsing: {
+          fields: [{ name: "field name", regexp: "field regexp" }]
+        }
+      }
+    },
+  ];
+  const axiosResponse = {
+    data: {
+      sources: sources,
+      total: 15,
+    },
+    status: 200,
+    statusText: 'OK',
+    config: {},
+    headers: {},
+  };
+  mockedAxios.get.mockResolvedValueOnce(axiosResponse);
+
+  const { findByText } = render(<SourceTable />)
+
+  expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+  expect(mockedAxios.get).toHaveBeenCalledWith('/api/sources/?limit=10&page=1');
+  const items = await findByText(/abc123/);
+  expect(items).toBeInTheDocument();
+})
