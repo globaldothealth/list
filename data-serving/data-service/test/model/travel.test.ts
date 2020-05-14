721951dd218c4fdd81725bd740dfb226cd27bc0f
Kelly Moran <khmoran@google.com>
2020-05-14 15:36:56 -0400

    Add full and minimal json files for each schema, and use them in tests (with spread operators to modify properties as necsesary)

--- data-serving/data-service/test/model/travel.test.ts
+++ data-serving/data-service/test/model/travel.test.ts
@@ -1,8 +1,8 @@
 import { TravelDocument, travelSchema } from '../../src/model/travel';
 
 import { Error } from 'mongoose';
-import fullDateRange from './data/date-range.full.json';
-import fullLocation from './data/location.full.json';
+import fullModel from './data/travel.full.json';
+import minimalModel from './data/travel.minimal.json';
 import mongoose from 'mongoose';
 
 const Travel = mongoose.model<TravelDocument>('Travel', travelSchema);
@@ -10,22 +10,18 @@ const Travel = mongoose.model<TravelDocument>('Travel', travelSchema);
 describe('validate', () => {
     it('an unknown travel purpose is invalid', async () => {
         return new Travel({
-            purpose: 'Needed to just get away, you know?',
+            ...fullModel,
+            ...{ purpose: 'Needed to just get away, you know?' },
         }).validate((e) => {
             expect(e.name).toBe(Error.ValidationError.name);
         });
     });
 
-    it('an empty travel document is valid', async () => {
-        return new Travel({}).validate();
+    it('a minimal travel document is valid', async () => {
+        return new Travel(minimalModel).validate();
     });
 
     it('a fully specified travel document is valid', async () => {
-        return new Travel({
-            location: fullLocation,
-            dateRange: fullDateRange,
-            purpose: 'Family',
-            additionalInformation: 'Skeletal lamping',
-        }).validate();
+        return new Travel(fullModel).validate();
     });
 });
