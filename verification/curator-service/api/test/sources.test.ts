429a517b6c576538e9d9139b2e926f444d770c16
axmb <alexbenjamin@google.com>
2020-05-15 12:57:08 -0400

    Update sources controller unit tests.

--- verification/curator-service/api/test/sources.test.ts
+++ verification/curator-service/api/test/sources.test.ts
@@ -130,10 +130,16 @@ describe('PUT', () => {
 
 describe('POST', () => {
     it('should return the created source', async () => {
-        request(app)
+        const source = {
+            name: 'some name',
+            origin: { url: 'http://what.ever' },
+        };
+        const res = await request(app)
             .post('/api/sources')
-            .send({ name: 'some name', origin: { url: 'http://what.ever' } })
-            .expect(201);
+            .send(source)
+            .expect('Content-Type', /json/)
+            .expect(200);
+        expect(res.body.name).toEqual(source.name);
     });
     it('should not create invalid source', async () => {
         const res = await request(app).post('/api/sources').expect(422);
