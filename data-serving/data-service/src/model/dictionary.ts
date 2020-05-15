eca34f01487b144a62bb521f37f6cb393368947c
Kelly Moran <khmoran@google.com>
2020-05-14 20:01:31 -0400

    Enforce uniqueness on dictionary values and add test

--- data-serving/data-service/src/model/dictionary.ts
+++ data-serving/data-service/src/model/dictionary.ts
@@ -1,5 +1,13 @@
 import mongoose from 'mongoose';
 
+const uniqueValuesValidator = {
+    validator: function (values: [string]): boolean {
+        const unique = new Set(values);
+        return values.length == unique.size;
+    },
+    message: 'Dictionary values must be unique',
+};
+
 /**
  * Schema for a field that follows a dictionary value format, where some values
  * are explicitly provided and others are imputed. Examples include the symptoms
@@ -8,11 +16,11 @@ import mongoose from 'mongoose';
 export const dictionarySchema = new mongoose.Schema({
     provided: {
         type: [String],
-        uniqueItems: true,
+        validate: uniqueValuesValidator,
     },
     imputed: {
         type: [String],
-        uniqueItems: true,
+        validate: uniqueValuesValidator,
     },
 });
 
