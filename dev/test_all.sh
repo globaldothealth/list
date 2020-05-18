3ce6236d0c390d0c33bdefa303160e7e72c63b1a
allysonjp715 <allysonjp@google.com>
2020-05-18 13:52:23 +0100

    Remove outdated comments and update command

--- dev/test_all.sh
+++ dev/test_all.sh
@@ -3,4 +3,5 @@ set -e
 npm --prefix=`dirname "$0"`/../verification/curator-service/api/ run-script test-silent
 npm --prefix=`dirname "$0"`/../data-serving/data-service/ run-script test-silent
 npm --prefix=`dirname "$0"`/../verification/curator-service/ui/ run-script test-silent
-npm --prefix=`dirname "$0"`/../verification/curator-service/ui/ run-script cypress-ci-test
+# cypress-run requires having a local stack running
+npm --prefix=`dirname "$0"`/../verification/curator-service/ui/ run-script cypress-run
