7cfd6736778a3e2a0b1e60c850d6775ff4ee32b2
allysonjp715 <allysonjp@google.com>
2020-05-18 18:11:25 +0100

    Clear DB at start of each test, and reseed DB at the end of all tests.

--- verification/curator-service/ui/cypress/integration/components/LinelistTableTest.spec.ts
+++ verification/curator-service/ui/cypress/integration/components/LinelistTableTest.spec.ts
@@ -1,4 +1,8 @@
 describe('Linelist table', function () {
+    beforeEach(() => {
+        cy.task('clearDB', {});
+    })
+
     it('Can add a case', function () {
         cy.visit('/cases');
         cy.get('button[title="Add"]').first().click();
@@ -6,8 +10,6 @@ describe('Linelist table', function () {
         cy.get('input[placeholder="Notes"]').clear().type('test notes');
         cy.get('input[placeholder="Source URL"]').clear().type('www.example.com');
         cy.get('button[title="Save"]').click();
-        // Assert the added row is present once we have cleared the DB before
-        // the test. If the database hasn't been cleared, we don't know if
-        // the element is on this page or not.
+        cy.contains('test notes');
     })
 }) 
