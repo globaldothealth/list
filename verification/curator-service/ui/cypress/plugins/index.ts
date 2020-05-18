eac523e5250e449c5f781dfa0039a87d77d8fa1c
allysonjp715 <allysonjp@google.com>
2020-05-18 18:15:07 +0100

    s/var/const

--- verification/curator-service/ui/cypress/plugins/index.ts
+++ verification/curator-service/ui/cypress/plugins/index.ts
@@ -1,7 +1,7 @@
 // Need to use require in plugins file 
 // https://stackoverflow.com/questions/59743577/is-there-anyway-to-query-mongodb-in-cypress-test
 const MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+const url = "mongodb://localhost:27017/";
 
 module.exports = (on: any, config: any) => {
   on('task', {
