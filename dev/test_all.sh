6262188c45f21faf60bf7e4b48412b893a8dc8d2
allysonjp715 <allysonjp@google.com>
2020-05-18 11:40:39 +0100

    Run integration tests in CI

--- dev/test_all.sh
+++ dev/test_all.sh
@@ -3,3 +3,4 @@ set -e
 npm --prefix=`dirname "$0"`/../verification/curator-service/api/ run-script test-silent
 npm --prefix=`dirname "$0"`/../data-serving/data-service/ run-script test-silent
 npm --prefix=`dirname "$0"`/../verification/curator-service/ui/ run-script test-silent
+npm --prefix=`dirname "$0"`/../verification/curator-service/ui/ run-script cypress-ci-test
