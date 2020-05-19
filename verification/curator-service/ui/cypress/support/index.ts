9a1a30e2dbbdbeb6a0d997bf0d15adcf28de1615
allysonjp715 <allysonjp@google.com>
2020-05-19 14:47:05 +0100

    Init DB at start entire test suite

--- verification/curator-service/ui/cypress/support/index.ts
+++ verification/curator-service/ui/cypress/support/index.ts
@@ -1,7 +1,13 @@
 import './commands'
 
+// Before all tests have run, initialize the database. This removes all data, 
+// applies the schema and adds any indexes.
+before(() => {
+    cy.exec('npm run init-case-db');
+});
+
 // After all tests have run, seed database with initial data.
 after(() => {
     cy.task('clearDB', {});
-    cy.exec('npm run import-data');
+    cy.exec('npm run import-case-data');
 });
