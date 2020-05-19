c7b4c380f47ed5aa554eab31835d9f5c74084c94
Kelly Moran <khmoran@google.com>
2020-05-19 13:48:02 -0400

    Separate case.events test into two: one for empty events and one for nonempty but without confirmed

--- data-serving/data-service/test/model/case.test.ts
+++ data-serving/data-service/test/model/case.test.ts
@@ -1,6 +1,7 @@
 import { Case } from '../../src/model/case';
 import { Error } from 'mongoose';
 import fullModel from './data/case.full.json';
+import minimalEvent from './data/event.minimal.json';
 import minimalModel from './data/case.minimal.json';
 
 describe('validate', () => {
@@ -10,8 +11,18 @@ describe('validate', () => {
         });
     });
 
-    it('model without confirmed event is invalid', async () => {
+    it('model without events is invalid', async () => {
         return new Case({ ...minimalModel, events: [] }).validate((e) => {
+            expect(e.name).toBe(Error.ValidationError.name);
+        });
+    });
+
+    it('model without confirmed event is invalid', async () => {
+        const notConfirmedEvent = { ...minimalEvent, name: 'not-confirmed' };
+        return new Case({
+            ...minimalModel,
+            events: [notConfirmedEvent],
+        }).validate((e) => {
             expect(e.name).toBe(Error.ValidationError.name);
         });
     });
