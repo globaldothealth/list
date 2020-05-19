4b3deed775dbd994a2e1d4b7b5a6df5b69125608
Graham Lee <graham.lee@cs.ox.ac.uk>
2020-05-19 14:18:04 +0100

    Do more than just rejecting every request.

--- verification/curator-service/api/test/access.test.ts
+++ verification/curator-service/api/test/access.test.ts
@@ -4,7 +4,12 @@ const accessControl = (
   config: Object,
 ) => (
   curry((config, req, res, next) => {
-    res.sendStatus(403);
+    const expectedRoles = config[req.url]['GET'];
+    if (expectedRoles.indexOf(req.user.role) !== -1) {
+      next();
+    } else {
+      res.sendStatus(403);
+    }
   })(config)
 );
 
@@ -34,5 +39,32 @@ describe('access control middleware', () => {
 
     expect(next).not.toHaveBeenCalled();
     expect(res.sendStatus).toHaveBeenCalledWith(403);
+  });
+
+  it('permits a call for which the user is in an allowed role', () => {
+    const user = {
+      role: 'curator',
+    };
+    const url = '/sources';
+    const rbacConfig = {
+      [url]: {
+        'GET': ['curator'],
+      },
+    };
+    const req = { 
+      user,
+      url,
+    };
+    const res = {
+      sendStatus: jest.fn(),
+    };
+    const next = jest.fn();
+
+    const access = accessControl(rbacConfig);
+
+    access(req, res, next);
+
+    expect(next).toHaveBeenCalled();
+    expect(res.sendStatus).not.toHaveBeenCalled();
   });
 });
