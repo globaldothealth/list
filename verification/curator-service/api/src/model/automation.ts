99cf11f7c9e88a4d5bb9357f722309c626f85efc
axmb <alexbenjamin@google.com>
2020-05-19 21:58:11 -0400

    Extract a validated automation model.

---
+++ verification/curator-service/api/src/model/automation.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import { ParserDocument, parserSchema } from './parser';
+import { RegexParsingDocument, regexParsingSchema } from './regex-parsing';
+import { ScheduleDocument, scheduleSchema } from './schedule';
+
+export const automationParsingValidator = {
+    validator: function (automation: AutomationDocument): boolean {
+        return (
+            (automation.parser != null || automation.regexParsing != null) &&
+            !(automation.parser != null && automation.regexParsing != null)
+        );
+    },
+    message: 'Exactly one of either parser or regexParsing must be supplied.',
+};
+
+export const automationSchema = new mongoose.Schema({
+    parser: parserSchema,
+    regexParsing: regexParsingSchema,
+    schedule: {
+        type: scheduleSchema,
+        required: true,
+    },
+});
+
+export type AutomationDocument = mongoose.Document & {
+    parser: ParserDocument;
+    regexParsing: RegexParsingDocument;
+    schedule: ScheduleDocument;
+};
