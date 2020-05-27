01b97dcb7ed1ceee463f33481c1ded78cf479acf
axmb <alexbenjamin@google.com>
2020-05-27 13:31:23 -0400

    Add a validated schedule expression to the Source schedule model.

--- verification/curator-service/api/src/model/schedule.ts
+++ verification/curator-service/api/src/model/schedule.ts
@@ -6,8 +6,16 @@ export const scheduleSchema = new mongoose.Schema({
         required: 'Enter a CloudWatch schedule rule AWS Lambda ARN',
         match: [/^arn\:aws\:events\:.+\:.+\:rule\/.+/],
     },
+    awsScheduleExpression: {
+        type: String,
+        required: 'Enter a CloudWatch rule schedule expression',
+        match: [
+            /^((rate\(((1 (minute|hour|day))|([1-9]+ (minutes|hours|days)))\))|(cron\((.+\s){5}.+\)))/,
+        ],
+    },
 });
 
 export type ScheduleDocument = mongoose.Document & {
     awsRuleArn: string;
+    awsScheduleExpression: string;
 };
