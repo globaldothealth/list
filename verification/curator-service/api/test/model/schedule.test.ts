88d6bf67a804d6ecec72ad46d1b66b28dd05f94d
axmb <alexbenjamin@google.com>
2020-05-27 15:53:23 -0400

    Make source.automation.schedule.awsRuleArn optional.

--- verification/curator-service/api/test/model/schedule.test.ts
+++ verification/curator-service/api/test/model/schedule.test.ts
@@ -2,20 +2,12 @@ import { ScheduleDocument, scheduleSchema } from '../../src/model/schedule';
 
 import { Error } from 'mongoose';
 import fullModel from './data/schedule.full.json';
+import minimalModel from './data/schedule.minimal.json';
 import mongoose from 'mongoose';
 
 const Schedule = mongoose.model<ScheduleDocument>('Schedule', scheduleSchema);
 
 describe('validate', () => {
-    it('a schedule without an AWS rule ARN is invalid', async () => {
-        const missingArn = { ...fullModel };
-        delete missingArn.awsRuleArn;
-
-        return new Schedule(missingArn).validate((e) => {
-            expect(e.name).toBe(Error.ValidationError.name);
-        });
-    });
-
     it('a schedule with a misformatted AWS rule ARN is invalid', async () => {
         const badArn = { ...fullModel };
         badArn.awsRuleArn = 'invalid:arn:aws:events:region:rule/field';
@@ -41,6 +33,10 @@ describe('validate', () => {
         return new Schedule(badSchedule).validate((e) => {
             expect(e.name).toBe(Error.ValidationError.name);
         });
+    });
+
+    it('a minimal schedule is valid', async () => {
+        return new Schedule(minimalModel).validate();
     });
 
     it('a fully specified schedule is valid', async () => {
