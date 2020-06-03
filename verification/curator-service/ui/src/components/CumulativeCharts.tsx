2f45ce0dbd12ee6fd0488347a75a1b5ff89fdd53
allysonjp715 <allysonjp@google.com>
2020-06-03 11:21:46 +0100

    Remove old comment

--- verification/curator-service/ui/src/components/CumulativeCharts.tsx
+++ verification/curator-service/ui/src/components/CumulativeCharts.tsx
@@ -13,7 +13,6 @@ const styles = (theme: Theme) =>
         chart: {
             boxShadow:
                 '0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.20)',
-            // elevation: '2dp',
             margin: theme.spacing(1),
         },
         wideColumn: {
