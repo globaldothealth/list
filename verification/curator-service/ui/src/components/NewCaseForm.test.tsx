161e5a8566e01ebd202962ad886f68dfec18511a
allysonjp715 <allysonjp@google.com>
2020-06-09 17:06:56 +0100

    Resurrect NewCaseForm

---
+++ verification/curator-service/ui/src/components/NewCaseForm.test.tsx
@@ -0,0 +1,88 @@
+import { fireEvent, render, wait } from '@testing-library/react';
+
+import NewCaseForm from './NewCaseForm';
+import React from 'react';
+import axios from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const user = {
+    _id: 'testUser',
+    name: 'Alice Smith',
+    email: 'foo@bar.com',
+    roles: ['admin', 'curator'],
+};
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+it('renders form', () => {
+    const { getByText } = render(<NewCaseForm user={user} />);
+    expect(getByText(/Submit case/i)).toBeInTheDocument();
+    expect(getByText(/Demographics/i)).toBeInTheDocument();
+    expect(getByText(/Location/i)).toBeInTheDocument();
+    expect(getByText(/Events/i)).toBeInTheDocument();
+    expect(getByText(/Source URL/i)).toBeInTheDocument();
+});
+
+it('submits case ok', async () => {
+    const axiosResponse = {
+        data: {},
+        status: 200,
+        statusText: 'OK',
+        config: {},
+        headers: {},
+    };
+    mockedAxios.post.mockResolvedValueOnce(axiosResponse);
+
+    const { getByText } = render(<NewCaseForm user={user} />);
+    expect(getByText(/Submit case/i)).toBeInTheDocument();
+
+    await wait(() => {
+        fireEvent.click(getByText(/Submit case/));
+    });
+    expect(mockedAxios.post).toHaveBeenCalledTimes(1);
+    expect(mockedAxios.post).toHaveBeenCalledWith('/api/cases', {
+        demographics: {
+            sex: undefined,
+        },
+        events: {
+            name: 'confirmed',
+            dateRange: {
+                start: '',
+            },
+        },
+        location: {
+            country: '',
+        },
+        revisionMetadata: {
+            date: expect.any(String),
+            id: 0,
+            moderator: 'foo@bar.com',
+        },
+        sources: [
+            {
+                url: '',
+            },
+        ],
+        notes: '',
+    });
+});
+
+it('submits case not ok', async () => {
+    const { getByText } = render(<NewCaseForm user={user} />);
+    expect(getByText(/Submit case/i)).toBeInTheDocument();
+
+    const axiosResponse = {
+        message: 'Validation error: foo.bar',
+    };
+    mockedAxios.post.mockRejectedValueOnce(axiosResponse);
+
+    await wait(() => {
+        fireEvent.click(getByText(/Submit case/));
+    });
+    expect(mockedAxios.post).toHaveBeenCalledTimes(1);
+    expect(getByText(/foo.bar/)).toBeDefined();
+});
