6d40e09b8906610782e441b3dd43f54bd9ca0037
allysonjp715 <allysonjp@google.com>
2020-06-10 11:04:29 +0100

    Add a link to the covid-19 map

--- verification/curator-service/ui/src/components/App.tsx
+++ verification/curator-service/ui/src/components/App.tsx
@@ -136,6 +136,14 @@ class Home extends React.Component<HomeProps, {}> {
                     <div>Login to access Global Health Curator Portal</div>
                 ) : (
                     <nav>
+                        <div>
+                            <a
+                                href="https://covid-19.ghdsi.org/"
+                                target="_blank"
+                            >
+                                Covid-19 Map
+                            </a>
+                        </div>
                         {this.hasAnyRole(['curator', 'reader']) && (
                             <div>
                                 <Link to="/cases">Linelist</Link>
