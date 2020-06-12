595ed049b5b566c1e9a6c479727e007a0b5fe55c
Kelly Moran <khmoran@google.com>
2020-06-11 22:48:56 -0400

    Bring revisionHistory schema in line with spec

--- data-serving/data-service/src/model/revision-metadata.ts
+++ data-serving/data-service/src/model/revision-metadata.ts
@@ -1,17 +1,8 @@
 import { dateFieldInfo } from './date';
 import mongoose from 'mongoose';
 
-export const revisionMetadataSchema = new mongoose.Schema({
-    id: {
-        type: Number,
-        min: 0,
-        validate: {
-            validator: Number.isInteger,
-            message: '{VALUE} is not an integer value',
-        },
-        required: 'Enter a revision id',
-    },
-    moderator: {
+const editMetadataSchema = new mongoose.Schema({
+    curator: {
         type: String,
         required: 'Enter a revision moderator id',
     },
@@ -22,9 +13,39 @@ export const revisionMetadataSchema = new mongoose.Schema({
     notes: String,
 });
 
-export type RevisionMetadataDocument = mongoose.Document & {
-    id: number;
-    moderator: string;
+export const revisionMetadataSchema = new mongoose.Schema({
+    revisionNumber: {
+        type: Number,
+        min: 0,
+        validate: {
+            validator: Number.isInteger,
+            message: '{VALUE} is not an integer value',
+        },
+        required: 'Enter a revision id',
+    },
+    creationMetadata: {
+        type: editMetadataSchema,
+        required: 'Enter creation metadata',
+    },
+    updateMetadata: {
+        type: editMetadataSchema,
+        required: [
+            function (this: RevisionMetadataDocument): boolean {
+                return this.revisionNumber > 0 && !this.updateMetadata;
+            },
+            'Enter update metadata',
+        ],
+    },
+});
+
+type EditMetadataDocument = mongoose.Document & {
+    curator: string;
     date: Date;
     notes: string;
+};
+
+export type RevisionMetadataDocument = mongoose.Document & {
+    revisionNumber: number;
+    creationMetadata: EditMetadataDocument;
+    updateMetadata: EditMetadataDocument;
 };
