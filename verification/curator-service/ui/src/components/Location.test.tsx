474949272aede4dd7501799b6e95845800b63284
Timothé <attwad@users.noreply.github.com>
2020-06-16 12:20:58 +0200

    add component to display geolocation information (#275)

---
+++ verification/curator-service/ui/src/components/Location.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+
+import Location from './Location';
+import React from 'react';
+
+test('shows location when passed location information', async () => {
+    render(
+        <Location
+            location={{
+                type: 'place',
+                country: 'United States',
+                adminArea1: 'Hillsborough County',
+                latitude: 80.45,
+                longitude: 27.9379,
+            }}
+        />,
+    );
+    expect(screen.getByText(/place/i)).toBeInTheDocument();
+    expect(screen.getByText(/united States/i)).toBeInTheDocument();
+    expect(screen.getByText(/Hillsborough County/i)).toBeInTheDocument();
+    expect(screen.getByText(/80.4500/i)).toBeInTheDocument();
+    expect(screen.getByText(/27.9379/i)).toBeInTheDocument();
+    expect(screen.getByText(/N\/A/i)).toBeInTheDocument();
+});
+
+test('shows empty defaults when location undefined', async () => {
+    render(<Location location={undefined} />);
+    expect(screen.getByText(/Country/i)).toBeInTheDocument();
+});
