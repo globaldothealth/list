715beaa0adba2c334a9940439794f47d12b27624
Timothé <attwad@users.noreply.github.com>
2020-06-17 10:03:13 +0200

    Add mapbox geocoding capability (#268)
    * add mapbox js lib
    
    * add mapbox typings
    
    * add env var for mapbox token
    
    * add mapbox token to env definition
    
    * add geocoder interface
    
    * Plug geocoder in index.ts
    
    * proper mapbox defaults in dev
    
    * add readme for geocoding
    
    * more comments on readme
    
    * implementation of mapbox geocoder
    
    * unit tests for geocoder
    
    * add fake geocoder for CI
    
    * merge ui
    
    * unstage bad merge
    
    * use bool env vars
    
    * Oneliner return
    
    * remove useless return
    
    * fix env def
    
    * s/region/state for US
    
    * add comment and todo for test
    
    * update geocode types with latest schema changes
    
    * fix types

---
+++ verification/curator-service/api/src/geocoding/fake.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+
+import { GeocodeResult } from './geocoder';
+
+export default class FakeGeocoder {
+    private entries: Map<string, GeocodeResult>;
+
+    constructor() {
+        this.entries = new Map<string, GeocodeResult>();
+    }
+    async geocode(query: string): Promise<GeocodeResult[]> {
+        const entry = this.entries.get(query);
+        return entry ? [entry] : [];
+    }
+
+    seed = (req: Request, res: Response): void => {
+        const body = req.body as GeocodeResult;
+        this.entries.set(body.name, body);
+        res.sendStatus(200);
+    };
+
+    clear = (req: Request, res: Response): void => {
+        this.entries.clear();
+        res.sendStatus(200);
+    };
+}
---
+++ verification/curator-service/api/src/geocoding/geocoder.ts
@@ -0,0 +1,23 @@
+// A single result from a Geocoder.
+export interface GeocodeResult {
+    geometry: {
+        latitude: number;
+        longitude: number;
+    };
+    country: string;
+    // First administrative division (state in the US, Länder in Germany, ...).
+    administrativeAreaLevel1: string | undefined;
+    // Second administrative division (county in the US, departments in France, ...).
+    administrativeAreaLevel2: string | undefined;
+    // Third administrative division (cities usually).
+    administrativeAreaLevel3: string | undefined;
+    // A precise location, such as an establishment or POI.
+    place: string | undefined;
+    // Human readable place name.
+    name: string;
+}
+
+// A geocoder can geocode queries into places.
+export interface Geocoder {
+    geocode(query: string): Promise<GeocodeResult[]>;
+}
