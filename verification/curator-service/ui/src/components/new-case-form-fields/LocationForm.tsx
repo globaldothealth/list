7d0f0fe902ab44f3bae5e4491564a0dc5168edb5
Timothe Faudot <timothe@google.com>
2020-06-18 17:36:24 +0200

    use proper textfield import

--- verification/curator-service/ui/src/components/new-case-form-fields/LocationForm.tsx
+++ verification/curator-service/ui/src/components/new-case-form-fields/LocationForm.tsx
@@ -2,24 +2,26 @@ import { Field } from 'formik';
 import Location from './Location';
 import React from 'react';
 import Scroll from 'react-scroll';
-import { TextField } from '@material-ui/core';
+import { TextField } from 'formik-material-ui';
 
-const LocationForm = (): JSX.Element => {
-    // TODO: suggest and pass location.
-    return (
-        <Scroll.Element name="location">
-            <fieldset>
-                <legend>Location</legend>
-                <Field
-                    label="Location"
-                    name="locationQuery"
-                    type="text"
-                    component={TextField}
-                />
-                <Location location={undefined} />
-            </fieldset>
-        </Scroll.Element>
-    );
-};
+class LocationForm extends React.Component<{}, {}> {
+    render(): JSX.Element {
+        return (
+            // TODO: suggest and pass location.
+            <Scroll.Element name="location">
+                <fieldset>
+                    <legend>Location</legend>
+                    <Field
+                        label="Location"
+                        name="locationQuery"
+                        type="text"
+                        component={TextField}
+                    />
+                    <Location location={undefined} />
+                </fieldset>
+            </Scroll.Element>
+        );
+    }
+}
 
 export default LocationForm;
