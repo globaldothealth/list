e9c286cac8be9d63c78539bd4f94e60a877d7dd3
Timothé <attwad@users.noreply.github.com>
2020-06-18 17:51:10 +0200

    Move Location form to its own component (#296)
    * move location to its own component
    
    * fix p nesting
    
    * use proper textfield import

---
+++ verification/curator-service/ui/src/components/new-case-form-fields/LocationForm.tsx
@@ -0,0 +1,27 @@
+import { Field } from 'formik';
+import Location from './Location';
+import React from 'react';
+import Scroll from 'react-scroll';
+import { TextField } from 'formik-material-ui';
+
+class LocationForm extends React.Component<{}, {}> {
+    render(): JSX.Element {
+        return (
+            // TODO: suggest and pass location.
+            <Scroll.Element name="location">
+                <fieldset>
+                    <legend>Location</legend>
+                    <Field
+                        label="Location"
+                        name="locationQuery"
+                        type="text"
+                        component={TextField}
+                    />
+                    <Location location={undefined} />
+                </fieldset>
+            </Scroll.Element>
+        );
+    }
+}
+
+export default LocationForm;
