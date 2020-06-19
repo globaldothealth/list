61d3d6c182e49915ca96b548dc6b484eb611d10c
Timothé <attwad@users.noreply.github.com>
2020-06-19 11:03:26 +0200

    Prepare for geo suggestions (#302)
    * fix case when features have no context
    
    * add location to form values in preparation to sending it
    
    * make notes multiline
    
    * make location query field fullwidth
    
    * fix test for notes

--- verification/curator-service/ui/src/components/new-case-form-fields/Location.tsx
+++ verification/curator-service/ui/src/components/new-case-form-fields/Location.tsx
@@ -15,14 +15,24 @@ const styles = () =>
         },
     });
 
-interface Loc {
-    type: string;
+export interface Loc {
+    geometry: {
+        latitude: number;
+        longitude: number;
+    };
     country: string;
-    adminArea1?: string;
-    adminArea2?: string;
-    adminArea3?: string;
-    latitude: number;
-    longitude: number;
+    // First administrative division (state in the US, Länder in Germany, ...).
+    administrativeAreaLevel1: string | undefined;
+    // Second administrative division (county in the US, departments in France, ...).
+    administrativeAreaLevel2: string | undefined;
+    // Third administrative division (cities usually).
+    administrativeAreaLevel3: string | undefined;
+    // A precise location, such as an establishment or POI.
+    place: string | undefined;
+    // Human readable place name.
+    name: string;
+    // How granular the geocode is.
+    geoResolution: string;
 }
 
 // Cf. https://material-ui.com/guides/typescript/#augmenting-your-props-using-withstyles
@@ -39,7 +49,7 @@ class Location extends React.Component<Props, {}> {
                     <p>
                         <Typography variant="caption">Location Type</Typography>
                     </p>
-                    <p>{this.props.location?.type}</p>
+                    <p>{this.props.location?.geoResolution}</p>
                 </div>
                 <div className={classes.column}>
                     <p>
@@ -51,31 +61,43 @@ class Location extends React.Component<Props, {}> {
                     <p>
                         <Typography variant="caption">Admin area 1</Typography>
                     </p>
-                    <p>{this.props.location?.adminArea1 ?? 'N/A'}</p>
+                    <p>
+                        {this.props.location?.administrativeAreaLevel1 ?? 'N/A'}
+                    </p>
                 </div>
                 <div className={classes.column}>
                     <p>
                         <Typography variant="caption">Admin area 2</Typography>
                     </p>
-                    <p>{this.props.location?.adminArea2 ?? 'N/A'}</p>
+                    <p>
+                        {this.props.location?.administrativeAreaLevel2 ?? 'N/A'}
+                    </p>
                 </div>
                 <div className={classes.column}>
                     <p>
                         <Typography variant="caption">Admin area 3</Typography>
                     </p>
-                    <p>{this.props.location?.adminArea3 ?? 'N/A'}</p>
+                    <p>
+                        {this.props.location?.administrativeAreaLevel3 ?? 'N/A'}
+                    </p>
                 </div>
                 <div className={classes.column}>
                     <p>
                         <Typography variant="caption">Latitude</Typography>
                     </p>
-                    <p>{this.props.location?.latitude?.toFixed(4) ?? '-'}</p>
+                    <p>
+                        {this.props.location?.geometry?.latitude?.toFixed(4) ??
+                            '-'}
+                    </p>
                 </div>
                 <div className={classes.column}>
                     <p>
                         <Typography variant="caption">Longitude</Typography>
                     </p>
-                    <p>{this.props.location?.longitude?.toFixed(4) ?? '-'}</p>
+                    <p>
+                        {this.props.location?.geometry?.longitude?.toFixed(4) ??
+                            '-'}
+                    </p>
                 </div>
             </div>
         );
