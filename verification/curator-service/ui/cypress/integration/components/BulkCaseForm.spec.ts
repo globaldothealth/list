e648665c8fed4988dde2b80e7a1933feb4e78b3e
axmb <alexbenjamin@google.com>
2020-06-23 22:28:58 -0400

    Cypress test for bulk entry using cypress-file-upload.

---
+++ verification/curator-service/ui/cypress/integration/components/BulkCaseForm.spec.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+describe('Bulk upload form', function () {
+    beforeEach(() => {
+        cy.task('clearCasesDB', {});
+        cy.login();
+    });
+
+    it('Can upload CSV', function () {
+        cy.visit('/cases');
+        cy.contains('No records to display');
+
+        cy.visit('/cases/bulk');
+        const csvFixture = '../fixtures/bulk_data.csv';
+        cy.get('input[type="file"]').attachFile(csvFixture);
+        cy.contains('Success!');
+
+        cy.visit('/cases');
+        cy.contains('No records to display').should('not.exist');
+        cy.contains('Male');
+        cy.contains('42');
+        cy.contains('Canada');
+        cy.contains('6/22/2020');
+        cy.contains('foo.bar');
+    });
+
+    it('Does not upload bad data', function () {
+        cy.visit('/cases');
+        cy.contains('No records to display');
+
+        cy.visit('/cases/bulk');
+        const csvFixture = '../fixtures/bad_bulk_data.csv';
+        cy.get('input[type="file"]').attachFile(csvFixture);
+        cy.contains('Request failed');
+
+        cy.visit('/cases');
+        cy.contains('No records to display');
+    });
+});
