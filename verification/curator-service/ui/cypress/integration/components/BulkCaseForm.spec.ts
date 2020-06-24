8be5224788e1890e464f7753e3e6ce3368d8008a
axmb <alexbenjamin@google.com>
2020-06-24 10:50:03 -0400

    Expand Cypress testing for multi-row and upsert.

--- verification/curator-service/ui/cypress/integration/components/BulkCaseForm.spec.ts
+++ verification/curator-service/ui/cypress/integration/components/BulkCaseForm.spec.ts
@@ -15,11 +15,39 @@ describe('Bulk upload form', function () {
         cy.contains('Success!');
 
         cy.visit('/cases');
+        // Common data.
         cy.contains('No records to display').should('not.exist');
         cy.contains('Male');
         cy.contains('42');
         cy.contains('Canada');
+        // First record.
         cy.contains('foo.bar');
+        // Second record.
+        cy.contains('bar.baz');
+    });
+
+    it('Upserts data', function () {
+        cy.visit('/cases');
+        cy.contains('No records to display');
+
+        cy.visit('/cases/bulk');
+        const csvFixture = '../fixtures/bulk_data.csv';
+        cy.get('input[type="file"]').attachFile(csvFixture);
+        cy.contains('Success!');
+
+        cy.visit('/cases');
+        cy.contains('No records to display').should('not.exist');
+        cy.contains('foo.bar');
+        cy.contains('new.url').should('not.exist');
+
+        cy.visit('/cases/bulk');
+        const updatedCsvFixture = '../fixtures/updated_bulk_data.csv';
+        cy.get('input[type="file"]').attachFile(updatedCsvFixture);
+        cy.contains('Success!');
+
+        cy.visit('/cases');
+        cy.contains('foo.bar').should('not.exist');
+        cy.contains('new.url');
     });
 
     it('Does not upload bad data', function () {
