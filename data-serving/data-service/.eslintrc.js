ea2a68d790dc605a764d27cdd46bbdeab56ebffa
Timothé <attwad@users.noreply.github.com>
2020-06-25 11:26:49 +0200

    no bitwise ope (#346)

--- data-serving/data-service/.eslintrc.js
+++ data-serving/data-service/.eslintrc.js
@@ -21,6 +21,9 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        "no-bitwise": [
+            "error"
+        ],
         "indent": [
             "error",
             4
