efdd2601b38938146b6df9ae5838336504f792aa
Timothé <attwad@users.noreply.github.com>
2020-06-26 12:11:28 +0200

    Add edit case component (#354)
    * merge
    
    * correct age conversion
    
    * add unit tests for edit case component
    
    * add integration tests for edit case
    
    * fix integration test
    
    * address Allyson comments
    
    * remove unused strings convs

--- verification/curator-service/ui/src/components/NewCaseForm.tsx
+++ verification/curator-service/ui/src/components/NewCaseForm.tsx
@@ -4,11 +4,13 @@ import { Button, LinearProgress } from '@material-ui/core';
 import { Form, Formik } from 'formik';
 import { green, grey, red } from '@material-ui/core/colors';
 
+import { Case } from './Case';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 import Demographics from './new-case-form-fields/Demographics';
 import ErrorIcon from '@material-ui/icons/Error';
 import Events from './new-case-form-fields/Events';
 import LocationForm from './new-case-form-fields/LocationForm';
+import MuiAlert from '@material-ui/lab/Alert';
 import NewCaseFormValues from './new-case-form-fields/NewCaseFormValues';
 import Notes from './new-case-form-fields/Notes';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
@@ -18,17 +20,11 @@ import Source from './new-case-form-fields/Source';
 import Symptoms from './new-case-form-fields/Symptoms';
 import Transmission from './new-case-form-fields/Transmission';
 import TravelHistory from './new-case-form-fields/TravelHistory';
+import User from './User';
 import { WithStyles } from '@material-ui/core/styles/withStyles';
 import axios from 'axios';
 import { createStyles } from '@material-ui/core/styles';
 import { withStyles } from '@material-ui/core';
-
-interface User {
-    _id: string;
-    name: string;
-    email: string;
-    roles: string[];
-}
 
 const styles = () =>
     createStyles({
@@ -47,8 +43,95 @@ const styles = () =>
         },
     });
 
+function initialValuesFromCase(c?: Case): NewCaseFormValues {
+    if (!c) {
+        return {
+            sex: undefined,
+            minAge: undefined,
+            maxAge: undefined,
+            age: undefined,
+            ethnicity: undefined,
+            nationalities: [],
+            profession: undefined,
+            location: undefined,
+            confirmedDate: null,
+            methodOfConfirmation: undefined,
+            onsetSymptomsDate: null,
+            firstClinicalConsultationDate: null,
+            selfIsolationDate: null,
+            admittedToHospital: undefined,
+            hospitalAdmissionDate: null,
+            icuAdmissionDate: null,
+            outcomeDate: null,
+            outcome: undefined,
+            symptoms: [],
+            transmissionRoutes: [],
+            transmissionPlaces: [],
+            transmissionLinkedCaseIds: [],
+            travelHistory: [],
+            sourceUrl: '',
+            notes: '',
+        };
+    }
+    return {
+        sex: c.demographics?.sex,
+        minAge:
+            c.demographics?.ageRange?.start !== c.demographics?.ageRange?.end
+                ? c.demographics?.ageRange?.start
+                : undefined,
+        maxAge:
+            c.demographics?.ageRange?.start !== c.demographics?.ageRange?.end
+                ? c.demographics?.ageRange?.end
+                : undefined,
+        age:
+            c.demographics?.ageRange?.start === c.demographics?.ageRange?.end
+                ? c.demographics?.ageRange?.start
+                : undefined,
+        ethnicity: c.demographics?.ethnicity,
+        nationalities: c.demographics?.nationalities,
+        profession: c.demographics?.profession,
+        location: c.location,
+        confirmedDate:
+            c.events.find((event) => event.name === 'confirmed')?.dateRange
+                ?.start || null,
+        methodOfConfirmation: c.events.find(
+            (event) => event.name === 'confirmed',
+        )?.value,
+        onsetSymptomsDate:
+            c.events.find((event) => event.name === 'onsetSymptoms')?.dateRange
+                ?.start || null,
+        firstClinicalConsultationDate:
+            c.events.find((event) => event.name === 'firstClinicalConsultation')
+                ?.dateRange?.start || null,
+        selfIsolationDate:
+            c.events.find((event) => event.name === 'selfIsolation')?.dateRange
+                ?.start || null,
+        admittedToHospital: c.events.find(
+            (event) => event.name === 'hospitalAdmission',
+        )?.value,
+        hospitalAdmissionDate:
+            c.events.find((event) => event.name === 'hospitalAdmission')
+                ?.dateRange?.start || null,
+        icuAdmissionDate:
+            c.events.find((event) => event.name === 'icuAdmission')?.dateRange
+                ?.start || null,
+        outcomeDate:
+            c.events.find((event) => event.name === 'outcome')?.dateRange
+                ?.start || null,
+        outcome: c.events.find((event) => event.name === 'outcome')?.value,
+        symptoms: c.symptoms?.values,
+        transmissionRoutes: c.transmission?.routes,
+        transmissionPlaces: c.transmission?.places,
+        transmissionLinkedCaseIds: c.transmission?.linkedCaseIds,
+        travelHistory: c.travelHistory?.travel,
+        sourceUrl: c.sources?.length > 0 ? c.sources[0].url : '',
+        notes: c.notes,
+    };
+}
+
 interface Props extends WithStyles<typeof styles> {
     user: User;
+    initialCase?: Case;
 }
 
 interface NewCaseFormState {
@@ -107,104 +190,107 @@ class NewCaseForm extends React.Component<Props, NewCaseFormState> {
         const ageRange = values.age
             ? { start: values.age, end: values.age }
             : { start: values.minAge, end: values.maxAge };
+        const newCase = {
+            demographics: {
+                sex: values.sex,
+                ageRange: ageRange,
+                ethnicity: values.ethnicity,
+                nationalities: values.nationalities,
+                profession: values.profession,
+            },
+            location: values.location,
+            events: [
+                {
+                    name: 'confirmed',
+                    dates: values.confirmedDate,
+                    value: values.methodOfConfirmation,
+                },
+                {
+                    name: 'onsetSymptoms',
+                    dates: values.onsetSymptomsDate,
+                    value: undefined,
+                },
+                {
+                    name: 'firstClinicalConsultation',
+                    dates: values.firstClinicalConsultationDate,
+                    value: undefined,
+                },
+                {
+                    name: 'selfIsolation',
+                    dates: values.selfIsolationDate,
+                    value: undefined,
+                },
+                {
+                    name: 'hospitalAdmission',
+                    dates:
+                        values.admittedToHospital === 'Yes'
+                            ? values.hospitalAdmissionDate
+                            : undefined,
+                    value: values.admittedToHospital,
+                },
+                {
+                    name: 'icuAdmission',
+                    dates: values.icuAdmissionDate,
+                    value: undefined,
+                },
+                {
+                    name: 'outcome',
+                    dates:
+                        values.outcome !== undefined
+                            ? values.outcomeDate
+                            : undefined,
+                    value: values.outcome,
+                },
+            ]
+                .filter((elem) => elem.dates !== null)
+                .map((elem) => {
+                    return {
+                        name: elem.name,
+                        dateRange: {
+                            start: elem.dates,
+                            end: elem.dates,
+                        },
+                        value: elem.value,
+                    };
+                }),
+            symptoms: {
+                values: values.symptoms,
+            },
+            transmission: {
+                routes: values.transmissionRoutes,
+                places: values.transmissionPlaces,
+                linkedCaseIds: values.transmissionLinkedCaseIds,
+            },
+            sources: [
+                {
+                    url: values.sourceUrl,
+                },
+            ],
+            travelHistory: {
+                travel: values.travelHistory,
+            },
+            notes: values.notes,
+            revisionMetadata: {
+                revisionNumber: 0,
+                creationMetadata: {
+                    curator: this.props.user.email,
+                    date: new Date().toISOString(),
+                },
+            },
+        };
         try {
-            await axios.post('/api/cases', {
-                demographics: {
-                    sex: values.sex,
-                    ageRange: ageRange,
-                    ethnicity: values.ethnicity,
-                    nationalities: values.nationalities,
-                    profession: values.profession,
-                },
-                location: values.location,
-                events: [
-                    {
-                        name: 'confirmed',
-                        dates: values.confirmedDate,
-                        value: values.methodOfConfirmation,
-                    },
-                    {
-                        name: 'onsetSymptoms',
-                        dates: values.onsetSymptomsDate,
-                        value: undefined,
-                    },
-                    {
-                        name: 'firstClinicalConsultation',
-                        dates: values.firstClinicalConsultationDate,
-                        value: undefined,
-                    },
-                    {
-                        name: 'selfIsolation',
-                        dates: values.selfIsolationDate,
-                        value: undefined,
-                    },
-                    {
-                        name: 'hospitalAdmission',
-                        dates:
-                            values.admittedToHospital === 'Yes'
-                                ? values.hospitalAdmissionDate
-                                : undefined,
-                        value: values.admittedToHospital,
-                    },
-                    {
-                        name: 'icuAdmission',
-                        dates: values.icuAdmissionDate,
-                        value: undefined,
-                    },
-                    {
-                        name: 'outcome',
-                        dates:
-                            values.outcome !== undefined
-                                ? values.outcomeDate
-                                : undefined,
-                        value: values.outcome,
-                    },
-                ]
-                    .filter((elem) => elem.dates !== null)
-                    .map((elem) => {
-                        return {
-                            name: elem.name,
-                            dateRange: {
-                                start: elem.dates,
-                                end: elem.dates,
-                            },
-                            value: elem.value,
-                        };
-                    }),
-                symptoms: {
-                    values: values.symptoms,
-                },
-                transmission: {
-                    routes: values.transmissionRoutes,
-                    places: values.transmissionPlaces,
-                    linkedCaseIds: values.transmissionLinkedCaseIds,
-                },
-                sources: [
-                    {
-                        url: values.sourceUrl,
-                    },
-                ],
-                travelHistory: {
-                    travel: values.travelHistory,
-                },
-                notes: values.notes,
-                revisionMetadata: {
-                    revisionNumber: 0,
-                    creationMetadata: {
-                        curator: this.props.user.email,
-                        date: new Date().toISOString(),
-                    },
-                },
-            });
+            // Update or create depending on the presence of the initial case ID.
+            if (this.props.initialCase?._id) {
+                await axios.put(
+                    `/api/cases/${this.props.initialCase?._id}`,
+                    newCase,
+                );
+            } else {
+                await axios.post('/api/cases', newCase);
+            }
             this.setState({ errorMessage: '' });
         } catch (e) {
-            if (e.response) {
-                this.setState({ errorMessage: e.response.data });
-            } else if (e.request) {
-                this.setState({ errorMessage: e.request });
-            } else {
-                this.setState({ errorMessage: e.message });
-            }
+            this.setState({ errorMessage: JSON.stringify(e) });
         }
     }
 
@@ -247,36 +333,10 @@ class NewCaseForm extends React.Component<Props, NewCaseFormState> {
     }
 
     render(): JSX.Element {
-        const { classes } = this.props;
+        const { classes, initialCase } = this.props;
         return (
             <Formik
-                initialValues={{
-                    sex: undefined,
-                    minAge: undefined,
-                    maxAge: undefined,
-                    age: undefined,
-                    ethnicity: undefined,
-                    nationalities: [],
-                    profession: undefined,
-                    location: undefined,
-                    confirmedDate: null,
-                    methodOfConfirmation: undefined,
-                    onsetSymptomsDate: null,
-                    firstClinicalConsultationDate: null,
-                    selfIsolationDate: null,
-                    admittedToHospital: undefined,
-                    hospitalAdmissionDate: null,
-                    icuAdmissionDate: null,
-                    outcomeDate: null,
-                    outcome: undefined,
-                    symptoms: [],
-                    transmissionRoutes: [],
-                    transmissionPlaces: [],
-                    transmissionLinkedCaseIds: [],
-                    travelHistory: [],
-                    sourceUrl: '',
-                    notes: '',
-                }}
+                initialValues={initialValuesFromCase(initialCase)}
                 validationSchema={NewCaseValidation}
                 onSubmit={(values) => this.submitCase(values)}
             >
@@ -298,13 +358,13 @@ class NewCaseForm extends React.Component<Props, NewCaseFormState> {
                                     isChecked:
                                         values.sex !== undefined ||
                                         (values.age !== undefined &&
-                                            values.age !== '') ||
+                                            values.age.toString() !== '') ||
                                         (values.minAge !== undefined &&
-                                            values.minAge !== '' &&
+                                            values.minAge.toString() !== '' &&
                                             values.maxAge !== undefined &&
-                                            values.maxAge !== '') ||
+                                            values.maxAge.toString() !== '') ||
                                         values.ethnicity !== undefined ||
-                                        values.nationalities.length > 0 ||
+                                        values.nationalities?.length > 0 ||
                                         values.profession !== undefined,
                                     hasError:
                                         errors.sex !== undefined ||
@@ -373,7 +433,7 @@ class NewCaseForm extends React.Component<Props, NewCaseFormState> {
                                 onClick={(): void => this.scrollTo('symptoms')}
                             >
                                 {this.tableOfContentsIcon({
-                                    isChecked: values.symptoms.length > 0,
+                                    isChecked: values.symptoms?.length > 0,
                                     hasError: errors.symptoms !== undefined,
                                 })}
                                 Symptoms
@@ -386,10 +446,10 @@ class NewCaseForm extends React.Component<Props, NewCaseFormState> {
                             >
                                 {this.tableOfContentsIcon({
                                     isChecked:
-                                        values.transmissionRoutes.length > 0 ||
-                                        values.transmissionPlaces.length > 0 ||
+                                        values.transmissionRoutes?.length > 0 ||
+                                        values.transmissionPlaces?.length > 0 ||
                                         values.transmissionLinkedCaseIds
-                                            .length > 0,
+                                            ?.length > 0,
                                     hasError:
                                         errors.transmissionRoutes !==
                                             undefined ||
@@ -407,7 +467,7 @@ class NewCaseForm extends React.Component<Props, NewCaseFormState> {
                                 }
                             >
                                 {this.tableOfContentsIcon({
-                                    isChecked: values.travelHistory.length > 0,
+                                    isChecked: values.travelHistory?.length > 0,
                                     hasError:
                                         errors.travelHistory !== undefined,
                                 })}
@@ -418,7 +478,7 @@ class NewCaseForm extends React.Component<Props, NewCaseFormState> {
                                 onClick={(): void => this.scrollTo('source')}
                             >
                                 {this.tableOfContentsIcon({
-                                    isChecked: values.sourceUrl.trim() !== '',
+                                    isChecked: values.sourceUrl?.trim() !== '',
                                     hasError: errors.sourceUrl !== undefined,
                                 })}
                                 Source
@@ -428,7 +488,7 @@ class NewCaseForm extends React.Component<Props, NewCaseFormState> {
                                 onClick={(): void => this.scrollTo('notes')}
                             >
                                 {this.tableOfContentsIcon({
-                                    isChecked: values.notes.trim() !== '',
+                                    isChecked: values.notes?.trim() !== '',
                                     hasError: errors.notes !== undefined,
                                 })}
                                 Notes
@@ -455,10 +515,16 @@ class NewCaseForm extends React.Component<Props, NewCaseFormState> {
                                 >
                                     Submit case
                                 </Button>
-                                {this.state.errorMessage && (
-                                    <h3>{this.state.errorMessage as string}</h3>
-                                )}
                             </Form>
+                            {this.state.errorMessage && (
+                                <MuiAlert
+                                    elevation={6}
+                                    variant="filled"
+                                    severity="error"
+                                >
+                                    {this.state.errorMessage}
+                                </MuiAlert>
+                            )}
                         </div>
                     </div>
                 )}
