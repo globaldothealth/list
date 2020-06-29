799beaa6e0f008fd024b5637ca50c665159642d2
Kelly Moran <khmoran@google.com>
2020-06-29 13:26:27 -0400

    Remove numLocations from travel history per discussion EOW

--- data-serving/data-service/src/model/travel-history.ts
+++ data-serving/data-service/src/model/travel-history.ts
@@ -1,16 +1,13 @@
 import { TravelDocument, travelSchema } from './travel';
 
 import mongoose from 'mongoose';
-import { positiveIntFieldInfo } from './positive-int';
 
 export const travelHistorySchema = new mongoose.Schema({
-    numLocations: positiveIntFieldInfo,
     travel: [travelSchema],
     traveledPrior30Days: Boolean,
 });
 
 export type TravelHistoryDocument = mongoose.Document & {
-    numLocations: number;
     travel: [TravelDocument];
     traveledPrior30Days: boolean;
 };
