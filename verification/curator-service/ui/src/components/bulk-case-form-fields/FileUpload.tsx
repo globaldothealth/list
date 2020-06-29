478fc666e68af6c65d1a2bf2f8a6d7530520eb28
axmb <alexbenjamin@google.com>
2020-06-28 20:21:09 -0400

    Remove top-level FileUpload div.

--- verification/curator-service/ui/src/components/bulk-case-form-fields/FileUpload.tsx
+++ verification/curator-service/ui/src/components/bulk-case-form-fields/FileUpload.tsx
@@ -12,25 +12,23 @@ export default function FileUpload(): JSX.Element {
     const { setFieldValue } = useFormikContext();
     const classes = useStyles();
     return (
-        <div>
-            <fieldset>
-                <legend>CSV Data</legend>
-                <input
-                    className={classes.csvInput}
-                    data-testid="csv-input"
-                    id="file"
-                    name="file"
-                    type="file"
-                    accept=".csv"
-                    onChange={(
-                        event: React.ChangeEvent<HTMLInputElement>,
-                    ): void => {
-                        if (event.currentTarget.files) {
-                            setFieldValue('file', event.currentTarget.files[0]);
-                        }
-                    }}
-                />
-            </fieldset>
-        </div>
+        <fieldset>
+            <legend>CSV Data</legend>
+            <input
+                className={classes.csvInput}
+                data-testid="csv-input"
+                id="file"
+                name="file"
+                type="file"
+                accept=".csv"
+                onChange={(
+                    event: React.ChangeEvent<HTMLInputElement>,
+                ): void => {
+                    if (event.currentTarget.files) {
+                        setFieldValue('file', event.currentTarget.files[0]);
+                    }
+                }}
+            />
+        </fieldset>
     );
 }
