197aecc25ffe71d1e2ec9f4ed20ba0387421f936
Kelly Moran <khmoran@google.com>
2020-06-30 16:27:27 -0400

    By default, only drop imported data as part of setup-db script

--- dev/setup_db.sh
+++ dev/setup_db.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+DELETE_ALL_DOCUMENTS=true \
 npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script setup-cases
 npm --prefix=`dirname "$0"`/../data-serving/data-service/ run-script import-data
