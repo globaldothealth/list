5e765da3c70d3e1b266e19eabb4958497ff36084
Timothé <attwad@users.noreply.github.com>
2020-06-30 14:13:38 +0200

    case data and demographics rendering of case (#382)
    * case data and demographics rendering of case
    
    * fix integ test

--- verification/curator-service/ui/src/components/ViewCase.tsx
+++ verification/curator-service/ui/src/components/ViewCase.tsx
@@ -1,8 +1,16 @@
+import {
+    Container,
+    Grid,
+    LinearProgress,
+    Paper,
+    Typography,
+} from '@material-ui/core';
+
 import { Case } from './Case';
-import { LinearProgress } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
 import React from 'react';
 import axios from 'axios';
+import { makeStyles } from '@material-ui/core';
 
 interface Props {
     id: string;
@@ -51,8 +59,119 @@ interface CaseDetailsProps {
     c: Case;
 }
 
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        background: theme.palette.background.default,
+        marginTop: '1em',
+    },
+    grid: {
+        margin: '1em',
+    },
+    sectionTitle: {
+        margin: '1em',
+    },
+}));
+
+function ageRange(range?: { start: number; end: number }): string {
+    if (!range) {
+        return '';
+    }
+    return range.start === range.end
+        ? `${range.start}`
+        : `${range.start}-${range.end}`;
+}
+
 function CaseDetails(props: CaseDetailsProps): JSX.Element {
-    return <pre>{JSON.stringify(props.c)}</pre>;
+    const classes = useStyles();
+    return (
+        <Container maxWidth="sm">
+            <Typography variant="h5">Case {props.c._id}</Typography>
+            <Paper className={classes.paper} variant="outlined" square>
+                <Typography className={classes.sectionTitle} variant="overline">
+                    Case data
+                </Typography>
+                <Grid container className={classes.grid}>
+                    <RowHeader title="Data source" />
+                    {/* TODO: Display data source name once we store those */}
+                    <RowContent content="" />
+
+                    <RowHeader title="Data source link" />
+                    <RowContent
+                        content={
+                            props.c.sources.length > 0
+                                ? props.c.sources[0].url
+                                : ''
+                        }
+                        isLink
+                    />
+
+                    <RowHeader title="Date of creation" />
+                    <RowContent
+                        content={
+                            props.c.revisionMetadata?.creationMetadata?.date ||
+                            ''
+                        }
+                    />
+
+                    <RowHeader title="Created by" />
+                    <RowContent
+                        content={
+                            props.c.revisionMetadata?.creationMetadata
+                                ?.curator || ''
+                        }
+                    />
+                </Grid>
+            </Paper>
+
+            <Paper className={classes.paper} variant="outlined" square>
+                <Typography className={classes.sectionTitle} variant="overline">
+                    Demographics
+                </Typography>
+                <Grid container className={classes.grid}>
+                    <RowHeader title="Age" />
+                    <RowContent
+                        content={ageRange(props.c.demographics?.ageRange)}
+                    />
+
+                    <RowHeader title="Sex" />
+                    <RowContent content={props.c.demographics?.sex} />
+
+                    <RowHeader title="Profession" />
+                    <RowContent content={props.c.demographics?.profession} />
+
+                    <RowHeader title="Nationality" />
+                    <RowContent
+                        content={props.c.demographics?.nationalities?.join(
+                            ', ',
+                        )}
+                    />
+
+                    <RowHeader title="Ethnicity" />
+                    <RowContent content={props.c.demographics?.ethnicity} />
+                </Grid>
+            </Paper>
+        </Container>
+    );
+}
+
+function RowHeader(props: { title: string }): JSX.Element {
+    return (
+        <Grid item xs={4}>
+            <Typography variant="body2">{props.title}</Typography>
+        </Grid>
+    );
+}
+
+function RowContent(props: { content: string; isLink?: boolean }): JSX.Element {
+    return (
+        <Grid item xs={8}>
+            {props.isLink ? (
+                <a href={props.content}>{props.content}</a>
+            ) : (
+                props.content
+            )}
+        </Grid>
+    );
 }
 
 export default ViewCase;
