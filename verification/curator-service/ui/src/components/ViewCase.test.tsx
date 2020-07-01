1d47c4432add1b93eafcfd18a32cafe82e34fd13
Timothé <attwad@users.noreply.github.com>
2020-07-01 13:58:54 +0200

    Finish details page (#398)
    * add pathogens
    
    * add other sources

--- verification/curator-service/ui/src/components/ViewCase.test.tsx
+++ verification/curator-service/ui/src/components/ViewCase.test.tsx
@@ -22,68 +22,84 @@ it('loads and displays case', async () => {
     };
     mockedAxios.get.mockResolvedValueOnce(axiosResponse);
 
-    const { findByText } = render(<ViewCase id="abc123" />);
+    const { findByText, getByText } = render(<ViewCase id="abc123" />);
     expect(mockedAxios.get).toHaveBeenCalledTimes(1);
     expect(mockedAxios.get).toHaveBeenCalledWith('/api/cases/abc123');
     // Case data.
     expect(await findByText(/5ef8e943dfe6e00030892d58/)).toBeInTheDocument();
     expect(
-        await findByText(
+        getByText(
             'https://www.colorado.gov/pacific/cdphe/news/10-new-presumptive-positive-cases-colorado-cdphe-confirms-limited-community-spread-covid-19',
         ),
     ).toBeInTheDocument();
-    expect(await findByText('abc123')).toBeInTheDocument();
-    expect(await findByText('2020-01-20')).toBeInTheDocument();
+    expect(getByText('sourceId')).toBeInTheDocument();
+    expect(getByText('twitter.com/a-tweet')).toBeInTheDocument();
+    expect(getByText('news.org/an-article')).toBeInTheDocument();
+    expect(getByText('abc123')).toBeInTheDocument();
+    expect(getByText('2020-01-20')).toBeInTheDocument();
     expect(
-        await findByText('Contact of a confirmed case at work.'),
+        getByText('Contact of a confirmed case at work.'),
     ).toBeInTheDocument();
     // Demographics.
-    expect(await findByText('Female')).toBeInTheDocument();
-    expect(await findByText('50-59')).toBeInTheDocument();
-    expect(await findByText('Horse breeder')).toBeInTheDocument();
-    expect(await findByText('Swedish')).toBeInTheDocument();
-    expect(await findByText('Asian')).toBeInTheDocument();
+    expect(getByText('Female')).toBeInTheDocument();
+    expect(getByText('50-59')).toBeInTheDocument();
+    expect(getByText('Horse breeder')).toBeInTheDocument();
+    expect(getByText('Swedish')).toBeInTheDocument();
+    expect(getByText('Asian')).toBeInTheDocument();
     // Location.
-    expect(await findByText('France')).toBeInTheDocument();
-    expect(await findByText('Île-de-F')).toBeInTheDocument();
-    expect(await findByText('Wuhan')).toBeInTheDocument();
-    expect(await findByText('Paris')).toBeInTheDocument();
-    expect(await findByText('Admin2')).toBeInTheDocument();
-    expect(await findByText(/2.3522/)).toBeInTheDocument();
-    expect(await findByText(/48.85/)).toBeInTheDocument();
+    expect(getByText('France')).toBeInTheDocument();
+    expect(getByText('Île-de-F')).toBeInTheDocument();
+    expect(getByText('Wuhan')).toBeInTheDocument();
+    expect(getByText('Paris')).toBeInTheDocument();
+    expect(getByText('Admin2')).toBeInTheDocument();
+    expect(getByText(/2.3522/)).toBeInTheDocument();
+    expect(getByText(/48.85/)).toBeInTheDocument();
     // Events.
-    expect(await findByText('2020-01-01')).toBeInTheDocument();
-    expect(await findByText('2020-01-02')).toBeInTheDocument();
-    expect(await findByText('2020-01-03')).toBeInTheDocument();
-    expect(await findByText('2020-01-04 - 2020-01-05')).toBeInTheDocument();
-    expect(await findByText('2020-01-06')).toBeInTheDocument();
-    expect(await findByText('Recovered')).toBeInTheDocument();
-    expect(await findByText('PCR test')).toBeInTheDocument();
+    expect(getByText('2020-01-01')).toBeInTheDocument();
+    expect(getByText('2020-01-02')).toBeInTheDocument();
+    expect(getByText('2020-01-03')).toBeInTheDocument();
+    expect(getByText('2020-01-04 - 2020-01-05')).toBeInTheDocument();
+    expect(getByText('2020-01-06')).toBeInTheDocument();
+    expect(getByText('Recovered')).toBeInTheDocument();
+    expect(getByText('PCR test')).toBeInTheDocument();
     // Symptoms.
-    expect(await findByText(/Severe pneumonia/)).toBeInTheDocument();
-    expect(await findByText(/Dyspnea/)).toBeInTheDocument();
-    expect(await findByText(/Weakness/)).toBeInTheDocument();
-    expect(await findByText(/Hypertension/)).toBeInTheDocument();
-    expect(await findByText(/Type 2 diabetes/)).toBeInTheDocument();
-    expect(await findByText(/Coronary heart disease/)).toBeInTheDocument();
-    expect(await findByText(/Lung cancer/)).toBeInTheDocument();
+    expect(getByText(/Severe pneumonia/)).toBeInTheDocument();
+    expect(getByText(/Dyspnea/)).toBeInTheDocument();
+    expect(getByText(/Weakness/)).toBeInTheDocument();
+    expect(getByText(/Hypertension/)).toBeInTheDocument();
+    expect(getByText(/Type 2 diabetes/)).toBeInTheDocument();
+    expect(getByText(/Coronary heart disease/)).toBeInTheDocument();
+    expect(getByText(/Lung cancer/)).toBeInTheDocument();
     // Transmission.
-    expect(await findByText(/Vector borne/)).toBeInTheDocument();
-    expect(await findByText(/Gym/)).toBeInTheDocument();
-    expect(await findByText(/bbf8e943dfe6e00030892dcc/)).toBeInTheDocument();
-    expect(await findByText(/aaf8e943dfe6e00030892dee/)).toBeInTheDocument();
+    expect(getByText(/Vector borne/)).toBeInTheDocument();
+    expect(getByText(/Gym/)).toBeInTheDocument();
+    expect(getByText(/bbf8e943dfe6e00030892dcc/)).toBeInTheDocument();
+    expect(getByText(/aaf8e943dfe6e00030892dee/)).toBeInTheDocument();
     // Travel history.
-    expect(await findByText('2020-02-10 - 2020-02-17')).toBeInTheDocument();
-    expect(await findByText('United States')).toBeInTheDocument();
-    expect(await findByText('New York')).toBeInTheDocument();
-    expect(await findByText('Kings County')).toBeInTheDocument();
-    expect(await findByText('Brooklyn')).toBeInTheDocument();
-    expect(await findByText('Kings Hospital Center')).toBeInTheDocument();
-    expect(await findByText('Point')).toBeInTheDocument();
-    expect(await findByText(/40.68/)).toBeInTheDocument();
-    expect(await findByText(/73.97/)).toBeInTheDocument();
-    expect(await findByText('Plane')).toBeInTheDocument();
-    expect(await findByText('Family')).toBeInTheDocument();
+    expect(getByText('2020-02-10 - 2020-02-17')).toBeInTheDocument();
+    expect(getByText('United States')).toBeInTheDocument();
+    expect(getByText('New York')).toBeInTheDocument();
+    expect(getByText('Kings County')).toBeInTheDocument();
+    expect(getByText('Brooklyn')).toBeInTheDocument();
+    expect(getByText('Kings Hospital Center')).toBeInTheDocument();
+    expect(getByText('Point')).toBeInTheDocument();
+    expect(getByText(/40.68/)).toBeInTheDocument();
+    expect(getByText(/73.97/)).toBeInTheDocument();
+    expect(getByText('Plane')).toBeInTheDocument();
+    expect(getByText('Family')).toBeInTheDocument();
+    // Pathogens and genome.
+    expect(getByText(/Pneumonia \(104\)/)).toBeInTheDocument();
+    expect(getByText('2019-12-30')).toBeInTheDocument();
+    expect(
+        getByText('https://www.ncbi.nlm.nih.gov/nuccore/NC_045512'),
+    ).toBeInTheDocument();
+    expect(getByText('NC_045512.2')).toBeInTheDocument();
+    expect(
+        getByText(
+            'Severe acute respiratory syndrome coronavirus 2 isolate Wuhan-Hu-1, complete genome',
+        ),
+    ).toBeInTheDocument();
+    expect(getByText('33000')).toBeInTheDocument();
 });
 
 it('displays API errors', async () => {
