de980238e43b9f6b6d164f0e500e5215524be1d7
Timothe Faudot <timothe@google.com>
2020-07-01 10:22:55 +0200

    add map to location display

--- verification/curator-service/ui/src/components/new-case-form-fields/Location.tsx
+++ verification/curator-service/ui/src/components/new-case-form-fields/Location.tsx
@@ -1,4 +1,4 @@
-import { Typography, withStyles } from '@material-ui/core';
+import { Container, Divider, Typography, withStyles } from '@material-ui/core';
 
 import { Location as Loc } from '../Case';
 import React from 'react';
@@ -14,6 +14,13 @@ const styles = () =>
         column: {
             marginRight: '1em',
         },
+        iframe: {
+            border: 'none',
+            display: 'inline-block',
+        },
+        map: {
+            textAlign: 'center',
+        },
     });
 
 // Cf. https://material-ui.com/guides/typescript/#augmenting-your-props-using-withstyles
@@ -25,62 +32,87 @@ class Location extends React.Component<Props, {}> {
     render(): JSX.Element {
         const { classes } = this.props;
         return (
-            <div className={classes.root}>
-                <div className={classes.column}>
-                    <p>
-                        <Typography variant="caption">Location Type</Typography>
-                    </p>
-                    <p>{this.props.location?.geoResolution}</p>
+            <>
+                <div className={classes.root}>
+                    <div className={classes.column}>
+                        <p>
+                            <Typography variant="caption">
+                                Location Type
+                            </Typography>
+                        </p>
+                        <p>{this.props.location?.geoResolution}</p>
+                    </div>
+                    <div className={classes.column}>
+                        <p>
+                            <Typography variant="caption">Country</Typography>
+                        </p>
+                        <p>{this.props.location?.country}</p>
+                    </div>
+                    <div className={classes.column}>
+                        <p>
+                            <Typography variant="caption">
+                                Admin area 1
+                            </Typography>
+                        </p>
+                        <p>
+                            {this.props.location?.administrativeAreaLevel1 ||
+                                'N/A'}
+                        </p>
+                    </div>
+                    <div className={classes.column}>
+                        <p>
+                            <Typography variant="caption">
+                                Admin area 2
+                            </Typography>
+                        </p>
+                        <p>
+                            {this.props.location?.administrativeAreaLevel2 ||
+                                'N/A'}
+                        </p>
+                    </div>
+                    <div className={classes.column}>
+                        <p>
+                            <Typography variant="caption">
+                                Admin area 3
+                            </Typography>
+                        </p>
+                        <p>
+                            {this.props.location?.administrativeAreaLevel3 ||
+                                'N/A'}
+                        </p>
+                    </div>
+                    <div className={classes.column}>
+                        <p>
+                            <Typography variant="caption">Latitude</Typography>
+                        </p>
+                        <p>
+                            {this.props.location?.geometry?.latitude?.toFixed(
+                                4,
+                            ) || '-'}
+                        </p>
+                    </div>
+                    <div className={classes.column}>
+                        <p>
+                            <Typography variant="caption">Longitude</Typography>
+                        </p>
+                        <p>
+                            {this.props.location?.geometry?.longitude?.toFixed(
+                                4,
+                            ) || '-'}
+                        </p>
+                    </div>
                 </div>
-                <div className={classes.column}>
-                    <p>
-                        <Typography variant="caption">Country</Typography>
-                    </p>
-                    <p>{this.props.location?.country}</p>
-                </div>
-                <div className={classes.column}>
-                    <p>
-                        <Typography variant="caption">Admin area 1</Typography>
-                    </p>
-                    <p>
-                        {this.props.location?.administrativeAreaLevel1 || 'N/A'}
-                    </p>
-                </div>
-                <div className={classes.column}>
-                    <p>
-                        <Typography variant="caption">Admin area 2</Typography>
-                    </p>
-                    <p>
-                        {this.props.location?.administrativeAreaLevel2 || 'N/A'}
-                    </p>
-                </div>
-                <div className={classes.column}>
-                    <p>
-                        <Typography variant="caption">Admin area 3</Typography>
-                    </p>
-                    <p>
-                        {this.props.location?.administrativeAreaLevel3 || 'N/A'}
-                    </p>
-                </div>
-                <div className={classes.column}>
-                    <p>
-                        <Typography variant="caption">Latitude</Typography>
-                    </p>
-                    <p>
-                        {this.props.location?.geometry?.latitude?.toFixed(4) ||
-                            '-'}
-                    </p>
-                </div>
-                <div className={classes.column}>
-                    <p>
-                        <Typography variant="caption">Longitude</Typography>
-                    </p>
-                    <p>
-                        {this.props.location?.geometry?.longitude?.toFixed(4) ||
-                            '-'}
-                    </p>
-                </div>
-            </div>
+                {this.props.location?.geometry !== undefined && (
+                    <div className={classes.map}>
+                        <Divider variant="middle" />
+                        <iframe
+                            className={classes.iframe}
+                            title="map"
+                            src={`https://api.mapbox.com/styles/v1/mapbox/light-v10/static/${this.props.location?.geometry?.longitude},${this.props.location?.geometry?.latitude},5,0/300x200?access_token=${process.env.REACT_MAPBOX_TOKEN}`}
+                        ></iframe>
+                    </div>
+                )}
+            </>
         );
     }
 }
