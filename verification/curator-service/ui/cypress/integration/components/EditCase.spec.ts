37cc7b6a4516ef35aabeb7e25e8e56d14ff59cc0
Timothé <attwad@users.noreply.github.com>
2020-07-02 12:15:48 +0200

    Full edit checks in cypress tests (#419)
    * full edit case checks in cypress
    
    * add fixture
    
    * todo for preexisting cods

--- verification/curator-service/ui/cypress/integration/components/EditCase.spec.ts
+++ verification/curator-service/ui/cypress/integration/components/EditCase.spec.ts
@@ -60,10 +60,79 @@ describe('Edit case', function () {
             expect(resp.body.cases).to.have.lengthOf(1);
             cy.visit(`/cases/edit/${resp.body.cases[0]._id}`);
             // Check that we could parse the original case.
-            cy.contains('France');
+            // Source.
+            cy.get('input[name="sourceUrl"]').should(
+                'have.value',
+                'https://www.colorado.gov/pacific/cdphe/news/10-new-presumptive-positive-cases-colorado-cdphe-confirms-limited-community-spread-covid-19',
+            );
+            // Demographics.
+            cy.get('input[name="sex"]').should('have.value', 'Female');
+            cy.get('input[name="minAge"]').should('have.value', '50');
+            cy.get('input[name="maxAge"]').should('have.value', '59');
+            // TODO: tedious: check "horse breeder"
             cy.contains('Swedish');
-            cy.contains('Female');
-            cy.contains('Actor').should('not.exist');
+            cy.contains('Asian');
+            // Location.
+            cy.contains('France');
+            cy.contains('Paris');
+            cy.contains('Île-de-F');
+            cy.contains('Admin2');
+            cy.contains('2.3522');
+            cy.contains('48.8566');
+            // Events.
+            cy.get('input[name="onsetSymptomsDate"]').should(
+                'have.value',
+                '2020/01/01',
+            );
+            cy.get('input[name="confirmedDate"]').should(
+                'have.value',
+                '2020/01/02',
+            );
+            cy.get('input[name="methodOfConfirmation"]').should(
+                'have.value',
+                'PCR test',
+            );
+            cy.get('input[name="hospitalAdmissionDate"]').should(
+                'have.value',
+                '2020/01/03',
+            );
+            cy.get('input[name="icuAdmissionDate"]').should(
+                'have.value',
+                '2020/01/04',
+            );
+            cy.get('input[name="outcome"]').should('have.value', 'Recovered');
+            // Symptoms.
+            cy.contains('Severe pneumonia');
+            // TODO: Preexisting conditions.
+            // cy.contains('Hypertension');
+            // Travel history.
+            cy.get('input[name="travelHistory[0].dateRange.start"]').should(
+                'have.value',
+                '2020/01/10',
+            );
+            cy.get('input[name="travelHistory[0].dateRange.end"]').should(
+                'have.value',
+                '2020/01/17',
+            );
+            cy.contains('United States');
+            cy.contains('New York');
+            cy.contains('Kings County');
+            cy.contains('Brooklyn');
+            cy.contains('Point');
+            cy.contains('Plane');
+            cy.contains('Family');
+            cy.contains('Yes');
+            // Pathogens.
+            cy.contains('Pneumonia');
+            cy.get('textarea[name="notes"]').should(
+                'have.value',
+                'Contact of a confirmed case at work.',
+            );
+            // Transmission.
+            cy.contains('Vector borne');
+            cy.contains('Gym');
+            cy.contains('bbf8e943dfe6e00030892dcc');
+            cy.contains('aaf8e943dfe6e00030892dee');
             // Change a few things.
             cy.get('div[data-testid="sex"]').click();
             cy.get('li[data-value="Male"').click();
