60c445bd178618baf32819a0eb5f23e2428d2771
Timothe Faudot <timothe@google.com>
2020-07-02 15:51:23 +0200

    functionalize components

--- verification/curator-service/ui/src/components/common-form-fields/Source.tsx
+++ verification/curator-service/ui/src/components/common-form-fields/Source.tsx
@@ -4,24 +4,22 @@ import { RequiredHelperText } from './FormikFields';
 import Scroll from 'react-scroll';
 import { TextField } from 'formik-material-ui';
 
-export default class Source extends React.Component<{}, {}> {
-    render(): JSX.Element {
-        return (
-            <Scroll.Element name="source">
-                <fieldset>
-                    <legend>Source</legend>
-                    <FastField
-                        label="Source URL"
-                        name="sourceUrl"
-                        type="text"
-                        required
-                        placeholder="https://..."
-                        component={TextField}
-                        fullWidth
-                    />
-                    <RequiredHelperText name={'sourceUrl'}></RequiredHelperText>
-                </fieldset>
-            </Scroll.Element>
-        );
-    }
+export default function Source(): JSX.Element {
+    return (
+        <Scroll.Element name="source">
+            <fieldset>
+                <legend>Source</legend>
+                <FastField
+                    label="Source URL"
+                    name="sourceUrl"
+                    type="text"
+                    required
+                    placeholder="https://..."
+                    component={TextField}
+                    fullWidth
+                />
+                <RequiredHelperText name={'sourceUrl'}></RequiredHelperText>
+            </fieldset>
+        </Scroll.Element>
+    );
 }
--- verification/curator-service/ui/src/components/new-case-form-fields/Notes.tsx
+++ verification/curator-service/ui/src/components/new-case-form-fields/Notes.tsx
@@ -3,22 +3,20 @@ import React from 'react';
 import Scroll from 'react-scroll';
 import { TextField } from 'formik-material-ui';
 
-export default class Notes extends React.Component<{}, {}> {
-    render(): JSX.Element {
-        return (
-            <Scroll.Element name="notes">
-                <fieldset>
-                    <legend>Notes</legend>
-                    <FastField
-                        label="Notes"
-                        name="notes"
-                        type="text"
-                        multiline={true}
-                        component={TextField}
-                        fullWidth
-                    />
-                </fieldset>
-            </Scroll.Element>
-        );
-    }
+export default function Notes(): JSX.Element {
+    return (
+        <Scroll.Element name="notes">
+            <fieldset>
+                <legend>Notes</legend>
+                <FastField
+                    label="Notes"
+                    name="notes"
+                    type="text"
+                    multiline={true}
+                    component={TextField}
+                    fullWidth
+                />
+            </fieldset>
+        </Scroll.Element>
+    );
 }
