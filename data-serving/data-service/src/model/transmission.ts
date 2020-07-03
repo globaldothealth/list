064f914f53dc0536c25440ff2f1431d7e9f21b40
Timothe Faudot <timothe@google.com>
2020-07-03 09:38:26 +0200

    freesolo the transmission suggest box

--- data-serving/data-service/src/model/transmission.ts
+++ data-serving/data-service/src/model/transmission.ts
@@ -1,30 +1,12 @@
 import mongoose from 'mongoose';
 
-export enum Route {
-    AirborneInfection = 'Airborne infection',
-    DropletInfection = 'Droplet infection',
-    FecalOral = 'Fecal–oral',
-    Sexual = 'Sexual',
-    Oral = 'Oral',
-    DirectContact = 'Direct contact',
-    Vertical = 'Vertical',
-    Iatrogenic = 'Iatrogenic',
-    VectorBorne = 'Vector borne',
-    Other = 'Other',
-}
-
 export const transmissionSchema = new mongoose.Schema(
     {
         // Ids of other cases of people with whom this person had contact.
         linkedCaseIds: [String],
         // Data dictionary.
         places: [String],
-        routes: [
-            {
-                type: String,
-                enum: Object.values(Route),
-            },
-        ],
+        routes: [String],
     },
     { _id: false },
 );
@@ -32,5 +14,5 @@ export const transmissionSchema = new mongoose.Schema(
 export type TransmissionDocument = mongoose.Document & {
     linkedCaseIds: [string];
     places: [string];
-    routes: [Route];
+    routes: [string];
 };
