f315da3ed47dfd0563e042c503587f63c976dcf9
axmb <alexbenjamin@google.com>
2020-07-10 16:34:44 -0400

    Small formatting updates.

--- verification/curator-service/ui/src/components/bulk-case-form-fields/CaseValidationError.test.ts
+++ verification/curator-service/ui/src/components/bulk-case-form-fields/CaseValidationError.test.ts
@@ -15,7 +15,7 @@ describe('constructed object', () => {
     });
     it('breaks provided API response down to alphabetized, unique fields', () => {
         const o = new CaseValidationError(42, exampleApiResponse);
-        // Only `demographics.sex` and `demographics.ageRange.end` are unique.
+        // Only `demographics.ageRange.end` and `demographics.sex`are unique.
         expect(o.formattedIssues).toHaveLength(2);
         expect(o.formattedIssues).toContain(
             'demographics.ageRange.end: Path `ageRange.end` (419) is more ' +
