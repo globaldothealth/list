6ea4dd36d9f62971e44be9b3719dc7f566b640d2
axmb <alexbenjamin@google.com>
2020-07-13 09:39:36 -0400

    Comment on manipulation logic in CaseValidationError.

--- verification/curator-service/ui/src/components/bulk-case-form-fields/CaseValidationError.ts
+++ verification/curator-service/ui/src/components/bulk-case-form-fields/CaseValidationError.ts
@@ -5,6 +5,29 @@ export default class CaseValidationError {
 
     public readonly formattedIssues: string[];
 
+    /**
+     * Creates a CaseValidationError object based on provided data.
+     *
+     * The logic of interest is in the construction of formattedIssues. The
+     * steps are as follows:
+     *
+     *   1. Remove leading boilerplate from the validation API response.
+     *      TODO: Write an integration test that makes sure the boilerplate is
+     *      up-to-date.
+     *   2. Split the remaining response into discrete validation issues.
+     *   3. Break each issue down to "{field}: {message}" constituents.
+     *   4. Sort these by the field name, ascending.
+     *   5. Filter all fields that are substrings of other fields. In other
+     *      words, this filters down [{demographics: [...]}.
+     *      {demographics.sex: [...]}] to only contain the dict corresponding
+     *      to demographics.sex. Validation issues contain duplicate messages
+     *      for nested fields -- this filtering removes these duplicates,
+     *      leaving only issues keyed by the fully-qualified field name.
+     *   6. Recombine field and message into an interface-friendly string.
+     *
+     * @param rowNumber The CSV row number of the case generating this error.
+     * @param apiResponse The raw text response received from the validation API.
+     */
     constructor(public readonly rowNumber: number, apiResponse: string) {
         const sortedErrorsByField = apiResponse
             .substr(CaseValidationError.leadingErrorText.length)
