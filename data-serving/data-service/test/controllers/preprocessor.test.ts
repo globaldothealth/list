3a588f49c98e25d4cf055cbc7031044584ec4e88
Kelly Moran <khmoran@google.com>
2020-07-22 17:11:53 -0400

    Remove notes; add preprocessor unit tests

--- data-serving/data-service/test/controllers/preprocessor.test.ts
+++ data-serving/data-service/test/controllers/preprocessor.test.ts
@@ -1,55 +1,221 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 
-import { RevisionMetadata } from '../../src/model/revision-metadata';
-import { assert } from 'console';
+import { Case } from '../../src/model/case';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import app from '../../src/index';
 import minimalCase from './../model/data/case.minimal.json';
 import { setRevisionMetadata } from '../../src/controllers/preprocessor';
+import supertest from 'supertest';
 
+let mongoServer: MongoMemoryServer;
 let dateNowSpy: any;
 
 beforeAll(() => {
+    mongoServer = new MongoMemoryServer();
     dateNowSpy = jest
         .spyOn(Date, 'now')
-        .mockImplementation(() => Date.parse('2020-07-20T15:26:07.490Z'));
+        .mockImplementation(() => Date.parse('2020-03-03'));
+});
+
+beforeEach(() => {
+    supertest.agent(app);
+    return Case.deleteMany({});
 });
 
 afterAll(() => {
     dateNowSpy.mockRestore();
+    return mongoServer.stop();
 });
 
 describe('create', () => {
-    it('', async () => {
-        const request = { ...minimalCase, curator: { email: 'abc@def.com' } };
-        const expectedRequest = {
+    it('create sets create metadata', async () => {
+        const requestBody = {
             ...minimalCase,
-            ...{
-                revisionMetadata: {
-                    revisionNumber: 0,
-                    creationMetadata: {
-                        curator: 'abc@def.com',
-                        date: Date.now(),
-                    },
-                },
-            },
+            curator: { email: 'creator@gmail.com' },
         };
         await setRevisionMetadata(
-            { body: request } as Request,
+            { body: requestBody, method: 'POST' } as Request,
             {} as Response,
-            jest.fn,
+            /* next= */ jest.fn,
         );
 
-        expect(expectedRequest).toEqual(request);
+        expect(requestBody).toEqual({
+            ...minimalCase,
+            revisionMetadata: {
+                revisionNumber: 0,
+                creationMetadata: {
+                    curator: 'creator@gmail.com',
+                    date: Date.now(),
+                },
+            },
+        });
     });
 });
 
 describe('update', () => {
-    it('', async () => {
-        // TODO: Add tests
+    it('update sets update metadata and preserves create metadata', async () => {
+        const c = new Case({
+            ...minimalCase,
+            revisionMetadata: {
+                revisionNumber: 0,
+                creationMetadata: {
+                    curator: 'creator@gmail.com',
+                    date: Date.parse('2020-01-01'),
+                },
+            },
+        });
+        await c.save();
+
+        const requestBody = {
+            ...minimalCase,
+            curator: { email: 'updater@gmail.com' },
+        };
+        await setRevisionMetadata(
+            {
+                body: requestBody,
+                method: 'PUT',
+                params: { id: c._id },
+            } as Request<any>,
+            {} as Response,
+            /* next= */ jest.fn,
+        );
+
+        expect(requestBody).toEqual({
+            ...minimalCase,
+            revisionMetadata: {
+                revisionNumber: 1,
+                creationMetadata: {
+                    curator: 'creator@gmail.com',
+                    date: Date.parse('2020-01-01'),
+                },
+                updateMetadata: {
+                    curator: 'updater@gmail.com',
+                    date: Date.now(),
+                },
+            },
+        });
+    });
+    it('update sets update metadata and replaces existing update metadata', async () => {
+        const c = new Case({
+            ...minimalCase,
+            revisionMetadata: {
+                revisionNumber: 1,
+                creationMetadata: {
+                    curator: 'creator@gmail.com',
+                    date: Date.parse('2020-01-01'),
+                },
+                updateMetadata: {
+                    curator: 'updater1@gmail.com',
+                    date: Date.parse('2020-02-02'),
+                },
+            },
+        });
+        await c.save();
+
+        const requestBody = {
+            ...minimalCase,
+            curator: { email: 'updater2@gmail.com' },
+        };
+        await setRevisionMetadata(
+            {
+                body: requestBody,
+                method: 'PUT',
+                params: { id: c._id },
+            } as Request<any>,
+            {} as Response,
+            /* next= */ jest.fn,
+        );
+
+        expect(requestBody).toEqual({
+            ...minimalCase,
+            revisionMetadata: {
+                revisionNumber: 2,
+                creationMetadata: {
+                    curator: 'creator@gmail.com',
+                    date: Date.parse('2020-01-01'),
+                },
+                updateMetadata: {
+                    curator: 'updater2@gmail.com',
+                    date: Date.now(),
+                },
+            },
+        });
     });
 });
 
 describe('upsert', () => {
-    it('', async () => {
-        // TODO: Add tests
+    it('upsert with no existing case sets create metadata', async () => {
+        const upsertCase = {
+            ...minimalCase,
+            caseReference: {
+                ...minimalCase.caseReference,
+                sourceEntryId: 'case_id',
+            },
+        };
+        const requestBody = {
+            ...upsertCase,
+            curator: { email: 'creator@gmail.com' },
+        };
+        await setRevisionMetadata(
+            { body: requestBody, method: 'PUT' } as Request,
+            {} as Response,
+            /* next= */ jest.fn,
+        );
+
+        expect(requestBody).toEqual({
+            ...upsertCase,
+            revisionMetadata: {
+                revisionNumber: 0,
+                creationMetadata: {
+                    curator: 'creator@gmail.com',
+                    date: Date.now(),
+                },
+            },
+        });
+    });
+    it('upsert with existing case sets update metadata', async () => {
+        const upsertCase = {
+            ...minimalCase,
+            caseReference: {
+                ...minimalCase.caseReference,
+                sourceEntryId: 'case_id',
+            },
+        };
+        const c = new Case({
+            ...upsertCase,
+            revisionMetadata: {
+                revisionNumber: 0,
+                creationMetadata: {
+                    curator: 'creator@gmail.com',
+                    date: Date.parse('2020-01-01'),
+                },
+            },
+        });
+        await c.save();
+
+        const requestBody = {
+            ...upsertCase,
+            curator: { email: 'updater@gmail.com' },
+        };
+        await setRevisionMetadata(
+            { body: requestBody, method: 'PUT' } as Request,
+            {} as Response,
+            /* next= */ jest.fn,
+        );
+
+        expect(requestBody).toEqual({
+            ...upsertCase,
+            revisionMetadata: {
+                revisionNumber: 1,
+                creationMetadata: {
+                    curator: 'creator@gmail.com',
+                    date: Date.parse('2020-01-01'),
+                },
+                updateMetadata: {
+                    curator: 'updater@gmail.com',
+                    date: Date.now(),
+                },
+            },
+        });
     });
 });
