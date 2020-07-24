bb17097d69822cd57439201afd7508052ccdb0ca
Timothe Faudot <timothe@google.com>
2020-07-24 16:19:19 +0200

    add script and handling of admin ids mapping

--- verification/curator-service/api/test/geocoding/adminsFetcher.test.ts
+++ verification/curator-service/api/test/geocoding/adminsFetcher.test.ts
@@ -1,14 +1,39 @@
+import { GeocodeResult, Resolution } from '../../src/geocoding/geocoder';
+
+import { Admin } from '../../src/model/admin';
 import MapboxAdminsFetcher from '../../src/geocoding/adminsFetcher';
-import { Resolution } from '../../src/geocoding/geocoder';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import axios from 'axios';
+import mongoose from 'mongoose';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 const testToken = 'test-token';
 
+let mongoServer: MongoMemoryServer;
+
+beforeAll(async () => {
+    mongoServer = new MongoMemoryServer();
+    await mongoose.connect(await mongoServer.getUri(), {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    });
+});
+
 afterEach(() => {
     jest.clearAllMocks();
+});
+
+beforeEach(async () => {
+    await Admin.deleteMany({});
+});
+
+afterAll(async () => {
+    await Admin.deleteMany({});
+    mongoose.disconnect();
+    return mongoServer.stop();
 });
 
 describe('Admins', () => {
@@ -31,6 +56,11 @@ describe('Admins', () => {
     });
 
     it('fetches missing admins', async () => {
+        await Admin.insertMany([
+            { id: 'USAFOO', name: 'some admin 1' },
+            { id: 'USABAR', name: 'some admin 2' },
+            { id: 'USABAZ', name: 'some admin 3' },
+        ]);
         const fetcher = new MapboxAdminsFetcher(testToken);
         mockedAxios.get.mockResolvedValueOnce({
             status: 200,
@@ -55,7 +85,7 @@ describe('Admins', () => {
                     },
                     {
                         properties: {
-                            id: 'USABAR',
+                            id: 'USABAZ',
                             tilequery: {
                                 layer: 'boundaries_admin_3',
                             },
@@ -64,7 +94,7 @@ describe('Admins', () => {
                 ],
             },
         });
-        await fetcher.fillAdmins({
+        const geocode: GeocodeResult = {
             administrativeAreaLevel1: '',
             administrativeAreaLevel2: '',
             administrativeAreaLevel3: '',
@@ -76,9 +106,13 @@ describe('Admins', () => {
             },
             name: 'the',
             place: 'to be',
-        });
+        };
+        await fetcher.fillAdmins(geocode);
         expect(mockedAxios.get).toHaveBeenCalledWith(
             'https://api.mapbox.com/v4/mapbox.enterprise-boundaries-a1-v2,mapbox.enterprise-boundaries-a2-v2,mapbox.enterprise-boundaries-a3-v2/tilequery/45.67,12.34.json?access_token=test-token',
         );
+        expect(geocode.administrativeAreaLevel1).toBe('some admin 1');
+        expect(geocode.administrativeAreaLevel2).toBe('some admin 2');
+        expect(geocode.administrativeAreaLevel3).toBe('some admin 3');
     });
 });
