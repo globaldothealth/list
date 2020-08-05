499583d4a4cb602c896fc2e4589ec9e0d5ef80a3
Timothe Faudot <timothe@google.com>
2020-08-05 10:53:06 +0200

    fix test

--- verification/curator-service/api/test/model/source.test.ts
+++ verification/curator-service/api/test/model/source.test.ts
@@ -19,7 +19,7 @@ describe('validate', () => {
         expect(errors).toBeDefined();
         expect(errors?.toString()).toMatch('Enter a name');
         expect(errors?.toString()).toMatch('Enter an origin');
-        expect(errors?.toString()).toMatch('Enter an format');
+        expect(errors?.toString()).toMatch('Enter a format');
     });
 });
 
