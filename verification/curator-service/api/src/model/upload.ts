0b965a270299843354901f585f89fbbf1723537b
axmb <alexbenjamin@google.com>
2020-08-07 17:34:43 -0400

    Add the upload model.

---
+++ verification/curator-service/api/src/model/upload.ts
@@ -0,0 +1,28 @@
+import { UploadSummaryDocument, uploadSummarySchema } from './upload-summary';
+
+import mongoose from 'mongoose';
+
+const uploadSchema = new mongoose.Schema({
+    status: {
+        type: String,
+        required: true,
+    },
+    summary: {
+        type: uploadSummarySchema,
+        required: true,
+    },
+    created: {
+        type: Date,
+        default: Date.now(),
+        required: true,
+    },
+});
+
+export type UploadDocument = mongoose.Document & {
+    _id: mongoose.Types.ObjectId;
+    status: string;
+    summary: UploadSummaryDocument;
+    created: string;
+};
+
+export const Upload = mongoose.model<UploadDocument>('Upload', uploadSchema);
