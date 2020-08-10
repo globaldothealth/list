84837a2b0db6319c460a463e2bbe6fffdf263027
Timothe Faudot <timothe@google.com>
2020-08-10 14:00:41 +0200

    nicify profile page

--- verification/curator-service/ui/src/components/Profile.tsx
+++ verification/curator-service/ui/src/components/Profile.tsx
@@ -2,6 +2,7 @@ import { Theme, makeStyles } from '@material-ui/core/styles';
 
 import React from 'react';
 import User from './User';
+import { Chip, Paper, Tooltip } from '@material-ui/core';
 
 const styles = makeStyles((theme: Theme) => ({
     root: {
@@ -16,8 +17,9 @@ const styles = makeStyles((theme: Theme) => ({
     email: {
         marginTop: theme.spacing(2),
     },
-    roles: {
+    role: {
         marginTop: theme.spacing(2),
+        marginRight: theme.spacing(1),
     },
 }));
 
@@ -32,18 +34,52 @@ export default function Profile(props: { user: User }): JSX.Element {
             )}
 
             {props.user.name && (
-                <div className={classes.name}>{props.user.name}</div>
+                <div className={classes.name}>
+                    <strong>Name:</strong> {props.user.name}
+                </div>
             )}
 
             {props.user.email && (
-                <div className={classes.email}>{props.user.email}</div>
-            )}
-
-            {props.user.roles && (
-                <div className={classes.roles}>
-                    {props.user.roles.join(', ')}
+                <div className={classes.email}>
+                    <strong>Email: </strong>
+                    {props.user.email}
                 </div>
             )}
+
+            {props.user.roles &&
+                props.user.roles.map((role) => {
+                    const tooltip = (
+                        text: string,
+                        role: string,
+                    ): JSX.Element => {
+                        return (
+                            <Tooltip
+                                className={classes.role}
+                                key={role}
+                                title={text}
+                            >
+                                <Chip variant="outlined" label={role} />
+                            </Tooltip>
+                        );
+                    };
+                    switch (role) {
+                        case 'reader':
+                            return tooltip(
+                                'readers can read the linelist data',
+                                role,
+                            );
+                        case 'curator':
+                            return tooltip(
+                                'curators can submit and verify cases and ingestion sources',
+                                role,
+                            );
+                        case 'admin':
+                            return tooltip(
+                                'admins can administer roles of other users',
+                                role,
+                            );
+                    }
+                })}
         </div>
     );
 }
