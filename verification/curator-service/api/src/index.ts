99552aaccaf68717c1b9cf4142dc85f86cdf6007
allysonjp715 <allysonjp@google.com>
2020-08-11 16:51:55 +0100

    Sync + comments

--- verification/curator-service/api/src/index.ts
+++ verification/curator-service/api/src/index.ts
@@ -1,3 +1,10 @@
+// Set up appmetrics-dash before importing additional dependencies.
+// This ensures that the module captures metrics for dependent systems, like
+// MongoDB.
+import Dash from 'appmetrics-dash';
+if (process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'production') {
+    Dash.attach();
+}
 import * as usersController from './controllers/users';
 
 import { AuthController, mustHaveAnyRole } from './controllers/auth';
@@ -7,7 +14,6 @@ import session, { SessionOptions } from 'express-session';
 import AwsEventsClient from './clients/aws-events-client';
 import AwsLambdaClient from './clients/aws-lambda-client';
 import CasesController from './controllers/cases';
-import Dash from 'appmetrics-dash';
 import FakeGeocoder from './geocoding/fake';
 import GeocodeSuggester from './geocoding/suggest';
 import { Geocoder } from './geocoding/geocoder';
@@ -26,13 +32,6 @@ import passport from 'passport';
 import path from 'path';
 import swaggerUi from 'swagger-ui-express';
 import validateEnv from './util/validate-env';
-
-// Set up appmetrics-dash before importing additional dependencies.
-// This ensures that the module captures metrics for dependent systems, like
-// MongoDB.
-if (process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'production') {
-    Dash.attach();
-}
 
 const app = express();
 
