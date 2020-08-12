e03ecbbe3f25c1d9feee9d8775462b781ddfed6c
axmb <alexbenjamin@google.com>
2020-08-11 21:22:06 -0400

    Use express-status-monitor in lieu of appmetrics-dash

--- data-serving/data-service/src/index.ts
+++ data-serving/data-service/src/index.ts
@@ -1,11 +1,3 @@
-// Set up appmetrics-dash before importing additional dependencies.
-// This ensures that the module captures metrics for dependent systems, like
-// MongoDB.
-import Dash from 'appmetrics-dash';
-if (process.env.NODE_ENV !== 'test') {
-    Dash.attach();
-}
-
 import * as caseController from './controllers/case';
 import * as homeController from './controllers/home';
 
@@ -22,6 +14,7 @@ import YAML from 'yamljs';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import express from 'express';
+import expressStatusMonitor from 'express-status-monitor';
 import mongoose from 'mongoose';
 import swaggerUi from 'swagger-ui-express';
 import validateEnv from './util/validate-env';
@@ -33,6 +26,7 @@ const env = validateEnv();
 
 // Express configuration.
 app.set('port', env.PORT);
+app.use(expressStatusMonitor());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(
     bodyParser.urlencoded({
