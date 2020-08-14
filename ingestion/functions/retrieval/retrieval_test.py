12fc4e313f203958942e4812fb476741bfa0b8b7
Timothé <attwad@users.noreply.github.com>
2020-08-14 22:47:14 +0200

    move common api creds fetching code to layer (#773)

--- ingestion/functions/retrieval/retrieval_test.py
+++ ingestion/functions/retrieval/retrieval_test.py
@@ -46,7 +46,8 @@ def test_lambda_handler_e2e(valid_event, requests_mock, s3):
     from retrieval import retrieval  # Import locally to avoid superseding mock
 
     # Mock/stub retrieving credentials, invoking the parser lambda, and S3.
-    retrieval.obtain_api_credentials = MagicMock(
+    import common_lib  # Import locally to avoid superseding mock
+    common_lib.obtain_api_credentials = MagicMock(
         name="obtain_api_credentials", return_value={})
     retrieval.invoke_parser = MagicMock(name="invoke_parser")
     s3.create_bucket(Bucket=retrieval.OUTPUT_BUCKET)
@@ -79,7 +80,7 @@ def test_lambda_handler_e2e(valid_event, requests_mock, s3):
 
     response = retrieval.lambda_handler(valid_event, "")
 
-    retrieval.obtain_api_credentials.assert_called_once_with()
+    common_lib.obtain_api_credentials.assert_called_once()
     retrieval.invoke_parser.assert_called_once_with(
         lambda_arn, source_id, upload_id, {},
         response["key"],
