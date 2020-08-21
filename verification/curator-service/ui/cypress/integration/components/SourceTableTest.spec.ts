b872f2bf0821adfb4336ce7585aaa6d90567b07e
axmb <alexbenjamin@google.com>
2020-08-20 22:36:07 -0400

    Remove add from the sources table (in favor of the create/modal).

--- verification/curator-service/ui/cypress/integration/components/SourceTableTest.spec.ts
+++ verification/curator-service/ui/cypress/integration/components/SourceTableTest.spec.ts
@@ -5,27 +5,6 @@ describe('Sources table', function () {
         cy.login();
     });
 
-    it('Can add a source', function () {
-        cy.visit('/sources');
-        cy.contains('Example source').should('not.exist');
-
-        cy.get('button[title="Add"]').click();
-        cy.get('input[placeholder="Name"]').type('Example source');
-        cy.get('input[placeholder="URL"]').type('www.example.com');
-        cy.get('div[data-testid="format-select"]').click();
-        cy.contains('li', 'JSON').click();
-        cy.get('div[data-testid="op-select"]').scrollIntoView().click();
-        cy.contains('li', 'from exactly').click();
-        cy.get('input[placeholder="days"]').type('42');
-
-        cy.get('button[title="Save"]').click();
-
-        cy.contains('Example source');
-        cy.contains('www.example.com');
-        cy.contains('JSON');
-        cy.contains('Only parse data from 42 days ago');
-    });
-
     it('Can edit a source', function () {
         cy.addSource('Example source', 'www.example.com');
         cy.visit('/sources');
