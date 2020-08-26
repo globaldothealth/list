15105418d0406076a5be8d934f1f5808b7903cf5
calremmel <calremmel@gmail.com>
2020-08-26 15:22:57 -0400

    parser and tests for germany

--- ingestion/functions/parsing/germany/germany.py
+++ ingestion/functions/parsing/germany/germany.py
@@ -6,27 +6,32 @@ import csv
 # Layer code, like parsing_lib, is added to the path by AWS.
 # To test locally (e.g. via pytest), we have to modify sys.path.
 # pylint: disable=import-error
-if ('lambda' not in sys.argv[0]):
+if "lambda" not in sys.argv[0]:
     sys.path.append(
         os.path.join(
-            os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-            'common/python'))
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "common/python"
+        )
+    )
 import parsing_lib
 
 _DATE_INDEX = 8
 _AGE_INDEX = 4
 _GENDER_INDEX = 5
-_CONFIRMED_INDEX = 6
+_CASECOUNT = 6
+_DEATHCOUNT = 7
+_RECOVERYCOUNT = 15
+_ADMIN1 = 2
+_ADMIN2 = 3
+
 
 def convert_date(raw_date):
     """
     Convert raw date field into a value interpretable by the dataserver.
 
-    The date is listed in dd/mm/YYYY format, but the data server API will
-    assume that ambiguous cases (e.g. "05/06/2020") are in mm/dd/YYYY format.
     """
-    date = datetime.strptime(raw_date, "%d/%m/%Y")
+    date = datetime.strptime(raw_date, "%Y/%m/%d %H:%M:%S")
     return date.strftime("%m/%d/%YZ")
+
 
 def convert_gender(raw_gender):
     if raw_gender == "M":
@@ -35,46 +40,67 @@ def convert_gender(raw_gender):
         return "Female"
     return None
 
+
 def convert_demographics(gender: str, age: str):
     demo = {}
     if gender:
         demo["gender"] = convert_gender(gender)
     if age:
-        age_range = age.split('-')
+        age_range = age.split("-")
         if len(age_range) > 1:
             demo["ageRange"] = {
-                "start": float(''.join([x for x in age_range[0] if x.isdigit()])),
-                "end": float(''.join([x for x in age_range[1] if x.isdigit()]))
+                "start": float("".join([x for x in age_range[0] if x.isdigit()])),
+                "end": float("".join([x for x in age_range[1] if x.isdigit()])),
             }
         else:
             demo["ageRange"] = {
-                "start": float(''.join([x for x in age_range[0] if x.isdigit()])),
-                "end": float(''.join([x for x in age_range[0] if x.isdigit()]))
+                "start": float("".join([x for x in age_range[0] if x.isdigit()])),
+                "end": float("".join([x for x in age_range[0] if x.isdigit()])),
             }
     return demo
+
 
 def parse_cases(raw_data_file: str, source_id: str, source_url: str):
     """
     Parses G.h-format case data from raw API data.
     """
     with open(raw_data_file, "r") as f:
-        with open(raw_data_file, "r") as f:
-            reader = csv.reader(f)
-        next(reader) # Skip the header.
+        reader = csv.reader(f)
+        next(reader)  # Skip the header.
         cases = []
         for row in reader:
-            num_confirmed_cases = int(row[_CONFIRMED_INDEX])
-            if not num_confirmed_cases:
+            num_confirmed_cases = int(row[_CASECOUNT])
+            if num_confirmed_cases < 1:
                 continue
             try:
                 case = {
-                    "caseReference": {
-                        "sourceId": source_id,
-                        "sourceUrl": source_url
+                    "caseReference": {"sourceId": source_id, "sourceUrl": source_url},
+                    "location": {
+                        "country": "Germany",
+                        "administrativeAreaLevel1": row[_ADMIN1],
+                        "administrativeAreaLevel2": row[_ADMIN2],
+                        "geoResolution": "Admin2",
+                        "name": row[_ADMIN2],
+                        "query": ", ".join(("Germany", row[_ADMIN1], row[_ADMIN2])),
                     },
+                    "events": [
+                        {
+                            "name": "confirmed",
+                            "dateRange": {
+                                "start": convert_date(row[_DATE_INDEX]),
+                                "end": convert_date(row[_DATE_INDEX]),
+                            },
+                        },
+                    ],
                     "demographics": convert_demographics(
-                        row[_GENDER_INDEX], row[_AGE_INDEX]),
+                        row[_GENDER_INDEX], row[_AGE_INDEX]
+                    ),
                 }
+                # Death and recovery counts are always equal to case counts for the row.
+                if int(row[_DEATHCOUNT]) > 0:
+                    case["events"].append({"name": "outcome", "value": "Death"})
+                if int(row[_RECOVERYCOUNT]) > 0:
+                    case["events"].append({"name": "outcome", "value": "Recovered"})
                 cases.extend([case] * num_confirmed_cases)
             except ValueError as ve:
                 print(ve)
