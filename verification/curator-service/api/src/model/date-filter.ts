ea9c380a85d9f66d27984713690ea4274b02f765
axmb <alexbenjamin@google.com>
2020-08-27 20:33:54 -0400

    Don't create _id for dateFilter subdocs

--- verification/curator-service/api/src/model/date-filter.ts
+++ verification/curator-service/api/src/model/date-filter.ts
@@ -1,17 +1,20 @@
 import mongoose from 'mongoose';
 
-export const dateFilterSchema = new mongoose.Schema({
-    numDaysBeforeToday: Number,
-    op: {
-        type: String,
-        enum: [
-            // Only import cases from a given day.
-            'EQ',
-            // Import all cases strictly prior to a given day.
-            'LT',
-        ],
+export const dateFilterSchema = new mongoose.Schema(
+    {
+        numDaysBeforeToday: Number,
+        op: {
+            type: String,
+            enum: [
+                // Only import cases from a given day.
+                'EQ',
+                // Import all cases strictly prior to a given day.
+                'LT',
+            ],
+        },
     },
-});
+    { _id: false },
+);
 
 export type DateFilterDocument = mongoose.Document & {
     numDaysBeforeToday: number;
