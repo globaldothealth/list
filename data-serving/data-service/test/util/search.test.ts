1102401b1c2ecf82fd6e4e0acba6e28e4a41fc2a
Timothé <attwad@users.noreply.github.com>
2020-09-07 11:43:05 +0200

    Throw upon invalid search request and display an error message in the UI (#950)
    * throw upon invalid search
    
    * better error display
    
    * add integ test
    
    * remove cypress.only

--- data-serving/data-service/test/util/search.test.ts
+++ data-serving/data-service/test/util/search.test.ts
@@ -16,6 +16,10 @@ describe('search query', () => {
         expect(res).toEqual({ fullTextSearch: 'want -nogood', filters: [] });
     });
 
+    it('errors if no value given for keyword', () => {
+        expect(() => parseSearchQuery('country:')).toThrow(/country/);
+    });
+
     it('is parses tokens', () => {
         const res = parseSearchQuery(
             'curator:foo@bar.com,baz@meh.com gender:male nationality:swiss occupation:"clock maker" country:switzerland outcome:recovered caseid:abc123 uploadid:def456 source:wsj.com admin1:"some admin 1" admin2:"some admin 2" admin3:"some admin 3"',
