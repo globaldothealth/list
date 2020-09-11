6e107a0e53c5816258355f6728378f682f79e393
calremmel <calremmel@gmail.com>
2020-09-11 15:09:57 -0400

    implement review changes

--- ingestion/functions/parsing/germany/germany.py
+++ ingestion/functions/parsing/germany/germany.py
@@ -6,14 +6,14 @@ import csv
 # Layer code, like parsing_lib, is added to the path by AWS.
 # To test locally (e.g. via pytest), we have to modify sys.path.
 # pylint: disable=import-error
-if "lambda" not in sys.argv[0]:
+try:
+    import parsing_lib
+except ImportError:
     sys.path.append(
         os.path.join(
-            os.path.dirname(os.path.dirname(
-                os.path.abspath(__file__))), "common/python"
-        )
-    )
-import parsing_lib
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+            'common/python'))
+    import parsing_lib
 
 _DATE_INDEX = 8
 _AGE_INDEX = 4
@@ -112,8 +112,7 @@ def parse_cases(raw_data_file: str, source_id: str, source_url: str):
                 for _ in range(num_confirmed_cases):
                     yield case
             except ValueError as ve:
-                print(ve)
-                print(row)
+                raise ValueError("Unhandled data: {}".format(ve))
 
 
 def lambda_handler(event, context):
