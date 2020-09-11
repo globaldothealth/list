0bfbb62408397ddad6cca074b97ccd8491f33a3e
fojackson8 <fojackson1@gmail.com>
2020-09-11 07:28:03 +0100

    Peru (#864)
    * Added test version of Peru Parsing Function
    
    * First commit of Peru Parsing Function
    
    * Update peru.py
    
    * Changed location converter, swapped pandas for csv, cleaned
    
    * merging main into branch
    
    * removed colombia, just peru
    
    * changed peru template timeout from 60 to 300
    
    * Included diagnostic test type, reverted unwanted files, autopep8
    
    * Adding diagnostic test map, and hopefully removing unwanted files
    
    * Adding diagnostic test map, and hopefully removing unwanted files
    
    * Removed a modified file from pull request
    
    * fixing valid_scheduled_event.json
    
    * staged changes to retrieval function
    
    * Changed Peru parser to generator template yielding individual cases
    
    Co-authored-by: Alex Benjamin <63060924+axmb@users.noreply.github.com>

---
+++ ingestion/functions/parsing/peru/peru.py
@@ -0,0 +1,98 @@
+import json
+import os
+import sys
+from datetime import date, datetime
+import csv
+
+# Layer code, like parsing_lib, is added to the path by AWS.
+# To test locally (e.g. via pytest), we have to modify sys.path.
+# pylint: disable=import-error
+try:
+    import parsing_lib
+except ImportError:
+    sys.path.append(
+        os.path.join(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+            'common/python'))
+    import parsing_lib
+
+
+def convert_date(raw_date: str):
+    """ 
+    Convert raw date field into a value interpretable by the dataserver.
+
+    The date is listed in YYYYmmdd format, but the data server API will
+    assume that ambiguous cases (e.g. "05/06/2020") are in mm/dd/YYYY format.
+    """
+    date = datetime.strptime(raw_date, "%Y%m%d")
+    return date.strftime("%m/%d/%Y")
+
+
+def convert_gender(raw_gender):
+    if raw_gender == "MASCULINO":
+        return "Male"
+    if raw_gender == "FEMENINO":
+        return "Female"
+    return None
+
+
+def convert_location(raw_entry):
+    query_terms = [
+        term for term in [
+            raw_entry.get("DISTRITO", ""),
+            raw_entry.get("PROVINCIA", ""),
+            raw_entry.get("DEPARTAMENTO", ""),
+            "Peru"]
+        if term != "EN INVESTIGACIÓN"]
+
+    return {"query":  ", ".join(query_terms)}
+
+
+def parse_cases(raw_data_file, source_id, source_url):
+    """
+    Parses G.h-format case data from raw API data.
+    Creates a dict to map type of confirming diagnostic test from Spanish abbreviation to English.
+    Assuming PR = prueba rapida (rapid serological test) and PCR = PCR test
+    """
+
+    conf_methods = {
+        'PR': 'Serological test',
+        'PCR': 'PCR test'
+    }
+
+    with open(raw_data_file, "r") as f:
+        reader = csv.DictReader(f)
+        cases = []
+        for entry in reader:
+            case =  {
+                    "caseReference": {
+                        "sourceId": source_id,
+                        "sourceEntryId": entry["UUID"],
+                        "sourceUrl": source_url
+                    },
+                    "location": convert_location(entry),
+                    "events": [
+                        {
+                            "name": "confirmed",
+                            "value": conf_methods.get(entry['METODODX']),
+                            "dateRange":
+                            {
+                                "start": convert_date(entry["FECHA_RESULTADO"]),
+                                "end": convert_date(entry["FECHA_RESULTADO"])
+                            }
+                        }
+                    ],
+                    "demographics": {
+                        "ageRange": {
+                            "start": float(entry["EDAD"]),
+                            "end": float(entry["EDAD"])
+                        },
+                        "gender": convert_gender(entry["SEXO"])
+                    }
+                }
+            yield case
+    
+
+
+def lambda_handler(event, context):
+    return parsing_lib.run_lambda(event, context, parse_cases)
