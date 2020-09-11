84ccd6571e1e505898acd29d5bf448163781e20a
Timothe Faudot <timothe@google.com>
2020-09-11 10:51:13 +0200

    add taiwan parser

---
+++ ingestion/functions/parsing/taiwan/taiwan.py
@@ -0,0 +1,118 @@
+import os
+import sys
+from datetime import datetime
+import csv
+
+# Layer code, like parsing_lib, is added to the path by AWS.
+# To test locally (e.g. via pytest), we have to modify sys.path.
+# pylint: disable=import-error
+try:
+    import parsing_lib
+except ImportError:
+    sys.path.append(
+        os.path.join(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+            'common/python'))
+    import parsing_lib
+
+TAIWAN_LOCATION = {
+    "country": "Taiwan",
+    # Go to wikipedia to debate please, thank you.
+    "geoResolution": "Country",
+    "name": "Taiwan",
+    "geometry": {
+        "longitude": "120.930229378541",
+        "latitude": "23.7779779950014"
+    }
+}
+
+
+def convert_date(raw_date):
+    """
+    Convert raw date field into a value interpretable by the dataserver.
+
+    Date filtering API expects mm/dd/YYYYZ format.
+    """
+    date = datetime.strptime(raw_date, "%Y/%m/%d")
+    return date.strftime("%m/%d/%YZ")
+
+
+def convert_gender(raw_gender: str):
+    if raw_gender == "男":
+        return "Male"
+    elif raw_gender == "女":
+        return "Female"
+    else:
+        raise ValueError(f'Unhandled gender: {raw_gender}')
+
+
+def convert_demographics(gender: str, age: str):
+    demo = {}
+    if gender:
+        demo["gender"] = convert_gender(gender)
+    if age:
+        # Brackets are like "35-39" or "over 85".
+        if '-' in age:
+            start, _, end = age.partition("-")
+            demo["ageRange"] = {
+                "start": float(start),
+                "end": float(end),
+            }
+        elif age == "70+":
+            demo["ageRange"] = {
+                "start": 70.0,
+            }
+        else:
+            raise ValueError(f'Unhandled age: {age}')
+    return demo or None
+
+
+def convert_notes(immigration_status: str):
+    return (
+        f"Case is an immigrant" if immigration_status == "是"
+        else "Case is from Taiwan")
+
+
+def parse_cases(raw_data_file: str, source_id: str, source_url: str):
+    """Parses G.h-format case data from raw API data.
+    
+    Remarks: No per-case ID is available so we can't dedupe cases.
+    """
+    with open(raw_data_file, "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            case = {
+                "caseReference": {
+                    "sourceId": source_id,
+                    "sourceUrl": source_url,
+                },
+                "location": (
+                    # 空值 means "empty value", it's not a city.
+                    {"query": f"{row['縣市']}, Taiwan"} if row['縣市'] != '空值'
+                    else TAIWAN_LOCATION),
+                "events": [
+                    {
+                        "name": "confirmed",
+                        "dateRange":
+                        {
+                            "start": convert_date(row['個案研判日']),
+                            "end": convert_date(row['個案研判日']),
+                        },
+                    },
+                ],
+                "demographics": convert_demographics(
+                    # Sex.
+                    row['性別'],
+                    # Age range.
+                    row['年齡層']),
+                # Immigration status.
+                "notes": convert_notes(row["是否為境外移入"]),
+            }
+            # Number of cases that this row represents.
+            print(case)
+            for _ in range(int(row["確定病例數"])):
+                yield case
+
+
+def lambda_handler(event, context):
+    return parsing_lib.run_lambda(event, context, parse_cases)
---
+++ ingestion/functions/parsing/taiwan/taiwan_test.py
@@ -0,0 +1,72 @@
+import os
+import unittest
+
+from taiwan import taiwan
+
+_SOURCE_ID = "abc123"
+_SOURCE_URL = "some-source-url"
+_PARSED_CASES = [
+    {
+        "caseReference": {
+            "sourceId": _SOURCE_ID,
+            "sourceUrl": _SOURCE_URL,
+        },
+        'location': taiwan.TAIWAN_LOCATION,
+        'events': [
+            {
+                'name': 'confirmed',
+                'dateRange': {'start': '01/22/2020Z', 'end': '01/22/2020Z'},
+            },
+        ],
+        'demographics': {
+            'gender': 'Female',
+            'ageRange': {'start': 55.0, 'end': 59.0},
+        },
+        'notes': 'Case is an immigrant'
+    }, {
+        "caseReference": {
+            "sourceId": _SOURCE_ID,
+            "sourceUrl": _SOURCE_URL,
+        },
+        'location': taiwan.TAIWAN_LOCATION,
+        'events': [
+            {
+                'name': 'confirmed',
+                'dateRange': {'start': '01/22/2020Z', 'end': '01/22/2020Z'},
+            },
+        ],
+        'demographics': {
+            'gender': 'Female',
+            'ageRange': {'start': 55.0, 'end': 59.0},
+        },
+        'notes': 'Case is an immigrant'
+    }, {
+        "caseReference": {
+            "sourceId": _SOURCE_ID,
+            "sourceUrl": _SOURCE_URL,
+        },
+        'location': {'query': '彰化縣, Taiwan'},
+        'events': [
+            {
+                'name': 'confirmed',
+                'dateRange': {'start': '01/24/2020Z', 'end': '01/24/2020Z'},
+            },
+        ],
+        'demographics': {
+            'gender': 'Male',
+            'ageRange': {'start': 55.0, 'end': 59.0},
+        },
+        'notes': 'Case is from Taiwan'
+    },
+]
+
+
+class TaiwanTest(unittest.TestCase):
+    def test_parse(self):
+        # Default of 1500 is not enough to show diffs when there is one.
+        self.maxDiff = 5000
+        current_dir = os.path.dirname(__file__)
+        sample_data_file = os.path.join(current_dir, "sample_data.csv")
+
+        result = taiwan.parse_cases(sample_data_file, _SOURCE_ID, _SOURCE_URL)
+        self.assertCountEqual(list(result), _PARSED_CASES)
