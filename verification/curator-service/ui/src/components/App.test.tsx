08bd547f4688bd8fef1cd3c9e9898b8f65115553
Remigiusz Poltorak <remigiusz.poltorak@htdevelopers.com>
2020-11-17 16:03:03 +0100

    Logo navigates to home screen
     - Main logo is now clickable and it navigates to root path /

--- verification/curator-service/ui/src/components/App.test.tsx
+++ verification/curator-service/ui/src/components/App.test.tsx
@@ -1,8 +1,9 @@
 import App from './App';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Router } from 'react-router-dom';
 import React from 'react';
 import axios from 'axios';
-import { render } from '@testing-library/react';
+import { fireEvent, render, wait } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 
 jest.mock('axios');
 // Mock charts page so that requests for mongo charts are not sent
@@ -75,4 +76,35 @@ it('has drawer links', async () => {
         'https://github.com/globaldothealth/list/blob/main/data-serving/scripts/export-data/case_fields.yaml',
     );
     expect(await findByTestId('termsButton')).toHaveAttribute('href', '/terms');
+});
+
+it('navigates to the home screen (charts) after clicking on home button', async () => {
+    const axiosResponse = {
+        data: { name: 'Alice Smith', email: 'foo@bar.com', roles: ['admin'] },
+        status: 200,
+        statusText: 'OK',
+        config: {},
+        headers: {},
+    };
+    mockedAxios.get.mockResolvedValueOnce(axiosResponse);
+    const history = createMemoryHistory({
+        initialEntries: ['/cases'],
+        initialIndex: 0,
+    });
+
+    const { getByText, findByText, findByTestId } = render(
+        <Router history={history}>
+            <App />
+        </Router>,
+    );
+
+    expect(await findByText('Linelist')).toBeInTheDocument();
+
+    fireEvent.click(await findByTestId('home-button'));
+
+    wait(() => {
+        expect(getByText('Cumulative')).toBeInTheDocument();
+        expect(getByText('Completeness')).toBeInTheDocument();
+        expect(getByText('Freshness')).toBeInTheDocument();
+    });
 });
