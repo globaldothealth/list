9b23d60f6b396247c381d3b2cfd51a872b8cd4e2
Remigiusz Poltorak <remigiusz.poltorak@htdevelopers.com>
2020-11-19 15:54:22 +0100

    Polishing styles in search guide dialog & adding the final content

--- verification/curator-service/ui/src/components/SearchGuideDialog.tsx
+++ verification/curator-service/ui/src/components/SearchGuideDialog.tsx
@@ -20,17 +20,43 @@ type Props = WithStyles<typeof styles> & {
 const styles = (theme: Theme) =>
     createStyles({
         root: {
+            maxWidth: 520,
             backgroundColor: theme.palette.primary.main,
             color: theme.palette.primary.contrastText,
             cursor: 'move',
             position: 'absolute',
             zIndex: 1500,
-            padding: theme.spacing(2),
+            padding: theme.spacing(4),
         },
         closeIcon: {
+            width: 30,
+            height: 30,
             cursor: 'pointer',
         },
+        title: {
+            fontSize: 22,
+            fontWeight: 'bold',
+        },
+        subtitle: {
+            fontSize: 18,
+            fontWeight: 'bold',
+        },
+        list: {
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-between',
+            padding: 0,
+            fontSize: 16,
+
+            '& > li': {
+                flex: '0 0 33.3333%',
+                listStyleType: 'none',
+            },
+        },
     });
+
+// additional offset to place dialog just below search guide button
+const LEFT_OFFSET = 116;
 
 const SearchGuideDialog = ({
     triggerRef,
@@ -44,12 +70,10 @@ const SearchGuideDialog = ({
 
     useEffect(() => {
         const container = triggerRef.current;
-        // additional offset to place dialog just below search guide button
-        const leftOffset = 116;
 
         if (container) {
             setPositionOffset({
-                x: container.offsetLeft + leftOffset,
+                x: container.offsetLeft + LEFT_OFFSET,
                 y: container.offsetTop + container.offsetHeight,
             });
         }
@@ -70,8 +94,50 @@ const SearchGuideDialog = ({
                                 onClick={onToggle}
                             />
                         </Box>
-                        <Typography>Search syntax</Typography>
-                        <Typography>Lorem ipsum dolor sit amet</Typography>
+                        <Box mb={1}>
+                            <Typography className={classes.title}>
+                                Search syntax
+                            </Typography>
+                        </Box>
+                        <Box mt={3} mb={1}>
+                            <Typography className={classes.subtitle}>
+                                Full text search
+                            </Typography>
+                        </Box>
+                        <Typography>
+                            Example: <i>"got infected at work" - India</i>
+                            <br />
+                            You can use arbitrary strings to search over those
+                            text fields:
+                        </Typography>
+                        <ul className={classes.list}>
+                            <li>notes</li>
+                            <li>country</li>
+                            <li>pathogen</li>
+                            <li>curator</li>
+                            <li>admin1</li>
+                            <li>name</li>
+                            <li>occupation</li>
+                            <li>admin2</li>
+                            <li>source URL</li>
+                            <li>nationalities</li>
+                            <li>place</li>
+                            <li>upload ID</li>
+                            <li>ethnicity</li>
+                            <li>location name</li>
+                            <li>verification status</li>
+                        </ul>
+                        <Box mt={3} mb={1}>
+                            <Typography className={classes.subtitle}>
+                                Keyboard text search
+                            </Typography>
+                        </Box>
+                        <Typography>
+                            Example: lorem ipsum
+                            <br />
+                            Supported keywords are shown when the filter button
+                            is clicked.
+                        </Typography>
                     </Box>
                 </Box>
             </Draggable>
