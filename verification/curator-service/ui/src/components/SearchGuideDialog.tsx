e7d60ba5d892777f283fc7699ef1c30173cea180
Remigiusz Poltorak <remigiusz.poltorak@htdevelopers.com>
2020-11-19 15:06:35 +0100

    Fix search guide dialog initial positioning

--- verification/curator-service/ui/src/components/SearchGuideDialog.tsx
+++ verification/curator-service/ui/src/components/SearchGuideDialog.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     Box,
-    Button,
     createStyles,
     Theme,
     Typography,
@@ -9,9 +8,12 @@ import {
     WithStyles,
 } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
-import Draggable from 'react-draggable';
+import Draggable, { ControlPosition } from 'react-draggable';
 
-type Props = WithStyles<typeof styles>;
+type Props = WithStyles<typeof styles> & {
+    triggerRef: React.RefObject<HTMLDivElement>;
+    onToggle: () => void;
+};
 
 // Return type isn't meaningful.
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
@@ -30,19 +32,50 @@ const styles = (theme: Theme) =>
         },
     });
 
-const SearchGuideDialog = ({ classes }: Props): JSX.Element | null => {
+const SearchGuideDialog = ({
+    triggerRef,
+    onToggle,
+    classes,
+}: Props): JSX.Element | null => {
+    const [
+        positionOffset,
+        setPositionOffset,
+    ] = React.useState<ControlPosition | null>(null);
+
+    useEffect(() => {
+        const container = triggerRef.current;
+        // additional offset to place dialog just below search guide button
+        const leftOffset = 116;
+
+        if (container) {
+            setPositionOffset({
+                x: container.offsetLeft + leftOffset,
+                y: container.offsetTop + container.offsetHeight,
+            });
+        }
+    }, [triggerRef]);
+
     return (
-        <Draggable handle="#draggable-search-guide" bounds="body">
-            <Box className={classes.root} id="draggable-search-guide">
-                <Box position="relative">
-                    <Box position="absolute" top={0} right={0}>
-                        <CloseIcon className={classes.closeIcon} />
+        positionOffset && (
+            <Draggable
+                handle="#draggable-search-guide"
+                bounds="body"
+                defaultPosition={positionOffset}
+            >
+                <Box className={classes.root} id="draggable-search-guide">
+                    <Box position="relative">
+                        <Box position="absolute" top={0} right={0}>
+                            <CloseIcon
+                                className={classes.closeIcon}
+                                onClick={onToggle}
+                            />
+                        </Box>
+                        <Typography>Search syntax</Typography>
+                        <Typography>Lorem ipsum dolor sit amet</Typography>
                     </Box>
-                    <Typography>Search syntax</Typography>
-                    <Typography>Lorem ipsum dolor sit amet</Typography>
                 </Box>
-            </Box>
-        </Draggable>
+            </Draggable>
+        )
     );
 };
 
