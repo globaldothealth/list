fbec45104c0be3351c2b76d216563d179555d986
Remigiusz Poltorak <remigiusz.poltorak@htdevelopers.com>
2020-12-04 11:23:12 +0100

    Refactor case status change & add case reincluding

--- verification/curator-service/ui/src/components/VerificationStatusIndicator.tsx
+++ verification/curator-service/ui/src/components/VerificationStatusIndicator.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Tooltip } from '@material-ui/core';
+
 import { ReactComponent as UnverifiedIcon } from './assets/unverified_icon.svg';
-import { VerificationStatus } from './Case';
 import { ReactComponent as VerifiedIcon } from './assets/verified_icon.svg';
+import { ReactComponent as ExcludedIcon } from './assets/excluded_icon.svg';
+import { VerificationStatus } from './Case';
 
 interface Props {
     status?: VerificationStatus;
@@ -14,9 +16,12 @@ export default function VerificationStatusIndicator(props: Props): JSX.Element {
     if (props.status === VerificationStatus.Verified) {
         helpText = 'Verified';
         iconElement = <VerifiedIcon data-testid="verified-svg" />;
-    } else {
+    } else if (props.status === VerificationStatus.Unverified) {
         helpText = 'Unverified';
         iconElement = <UnverifiedIcon data-testid="unverified-svg" />;
+    } else {
+        helpText = 'Excluded';
+        iconElement = <ExcludedIcon data-testid="excluded-svg" />;
     }
     return (
         <Tooltip title={helpText} placement="right">
