586622836b6ca081602ca6be770486c64091b8d9
Sergio <sergioloporto@gmail.com>
2021-01-14 15:26:22 +0100

    WIP fixing colors

--- verification/curator-service/ui/src/hooks/useCookieBanner.tsx
+++ verification/curator-service/ui/src/hooks/useCookieBanner.tsx
@@ -1,7 +1,18 @@
 import { useEffect } from 'react';
+import { useTheme, Theme } from '@material-ui/core/styles';
 
 const POLICY_ID = process.env.REACT_APP_POLICY_PUBLIC_ID;
 const SITE_ID = process.env.REACT_APP_COOKIE_CONSENT_PUBLIC_ID;
+
+
+
+
+export default function useCookieBanner(): void {
+    
+const theme = useTheme<Theme>();
+
+console.log(theme);
+
 
 const configuration = {
     whitelabel: false,
@@ -9,20 +20,23 @@ const configuration = {
     siteId: SITE_ID,
     cookiePolicyId: POLICY_ID,
     banner: {
-        rejectButtonColor: '#ECF3F0',
-        rejectButtonCaptionColor: '#0E7569',
+        rejectButtonColor: theme.palette.background.default,
+        rejectButtonCaptionColor: theme.palette.primary.main,
         position: 'float-bottom-center',
         textColor: 'white',
-        backgroundColor: '#0E7569',
+        backgroundColor: theme.palette.primary.main,
         acceptButtonDisplay: true,
-        acceptButtonColor: 'white',
-        acceptButtonCaptionColor: '#0E7569',
+        acceptButtonColor: theme.palette.background.paper,
+        acceptButtonCaptionColor: theme.palette.primary.main,
         customizeButtonDisplay: true,
         customizeButtonColor: '#ECF3F0',
-        customizeButtonCaptionColor: '#0E7569',
+        customizeButtonCaptionColor: theme.overrides.MuiAppBar.colorPrimary.backgroundColor,
     },
 };
-export default function useCookieBanner(): void {
+    
+
+    
+
     const insertConfiguration = (): void => {
         const script = document.createElement('script');
 
