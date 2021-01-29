607c7f926e1a28db149e3187ad5cfa0c2808d1f6
Maciej Zarzeczny <maciej.zarzeczny@htdevelopers.com>
2021-01-29 15:11:33 +0100

    Updated curator-ui-node workflow

--- verification/curator-service/ui/amplify-pull.sh
+++ verification/curator-service/ui/amplify-pull.sh
@@ -2,37 +2,39 @@
 set -e
 IFS='|'
 
+AWS_ACCESS_KEY_ID=$1
+AWS_SECRET_ACCESS_KEY=$2
+
 REACTCONFIG="{\
 \"SourceDir\":\"src\",\
 \"DistributionDir\":\"build\",\
 \"BuildCommand\":\"npm run-script build\",\
 \"StartCommand\":\"npm run-script start\"\
 }"
-
 AWSCLOUDFORMATIONCONFIG="{\
 \"configLevel\":\"project\",\
-\"useProfile\":true,\
+\"useProfile\":false,\
 \"profileName\":\"default\",\
-\"region\":\"us-east-2\"\
+\"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
+\"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
+\"region\":\"us-east-1\"\
 }"
-
 AMPLIFY="{\
 \"projectName\":\"curatorui\",\
+\"appId\":\"dh5s5jmk7z8dz\",\
 \"envName\":\"develop\",\
 \"defaultEditor\":\"code\"\
 }"
-
 FRONTEND="{\
 \"frontend\":\"javascript\",\
 \"framework\":\"react\",\
 \"config\":$REACTCONFIG\
 }"
-
 PROVIDERS="{\
 \"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
 }"
 
-amplify init \
+amplify pull \
 --amplify $AMPLIFY \
 --frontend $FRONTEND \
 --providers $PROVIDERS \
