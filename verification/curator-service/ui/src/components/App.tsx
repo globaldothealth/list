2bc09c638269c5de74388248c6f7564c422ad41c
Sergio <sergioloporto@gmail.com>
2021-02-15 10:15:41 +0100

    1540 - changed menu dropdown text

--- verification/curator-service/ui/src/components/App.tsx
+++ verification/curator-service/ui/src/components/App.tsx
@@ -308,7 +308,7 @@ function ProfileMenu(props: { user: User }): JSX.Element {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <MenuItem>Global.Health</MenuItem>
+                    <MenuItem>About Global.Health</MenuItem>
                 </a>
                 <a
                     className={classes.link}
