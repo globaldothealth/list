3c872e455977b466a9c5f3a5411ba70be24791ad
calremmel <chris.remmel@hey.com>
2021-02-22 13:16:28 -0500

    alter taiwan parser to run in isolation

--- ingestion/functions/parsing/taiwan/taiwan.py
+++ ingestion/functions/parsing/taiwan/taiwan.py
@@ -2,18 +2,12 @@ import os
 import sys
 from datetime import datetime
 import csv
+import json
 
 # Layer code, like parsing_lib, is added to the path by AWS.
 # To test locally (e.g. via pytest), we have to modify sys.path.
 # pylint: disable=import-error
-try:
-    import parsing_lib
-except ImportError:
-    sys.path.append(
-        os.path.join(
-            os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-            'common/python'))
-    import parsing_lib
+import parsing_lib
 
 TAIWAN_LOCATION = {
     "country": "Taiwan",
@@ -116,5 +110,10 @@ def parse_cases(raw_data_file: str, source_id: str, source_url: str):
                 yield case
 
 
-def lambda_handler(event, context):
-    return parsing_lib.run_lambda(event, context, parse_cases)
+def lambda_handler(event):
+    return parsing_lib.run_lambda(event, parse_cases)
+
+if __name__ == "__main__":
+    with open('input_event.json') as f:
+        event = json.load(f)
+        lambda_handler(event)
