7ff81b010de380e82e5da3f29a33366cc6a96616
fojackson8 <fojackson1@gmail.com>
2021-03-18 08:20:38 +0000

    Add manual geocoding of travelHistory for Colombia

--- ingestion/functions/parsing/colombia/colombia.py
+++ ingestion/functions/parsing/colombia/colombia.py
@@ -17,12 +17,13 @@ except ImportError:
     import parsing_lib
 
 
-# Load manual geocoding dictionaries, made from Colombia municipality -> coordinate lookup table
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "geocoding_dictionaries.json")) as json_file:
-    dictionaries = json.load(json_file)
-mun_code_coord = dictionaries["mun_code_coord"]
-mun_code_place_name = dictionaries["mun_code_place_name"]
+    geocoding_dictionaries = json.load(json_file)
 
+mun_code_coord = geocoding_dictionaries['mun_code_coord']
+mun_code_place_name = geocoding_dictionaries['mun_code_place_name']
+spanish_country_code_dict = geocoding_dictionaries['spanish_country_code_dict']
+iso_country_coord_map = geocoding_dictionaries['iso_country_coord_map']
 
 
 def convert_date(raw_date: str, dataserver=True):
@@ -52,10 +53,10 @@ def get_location(raw_entry):
     Uses a dict mapping based on common municipality code of lookup table ('MPIO_CCNCT') and source dataset ('Código DIVIPOLA municipio')
     '''
     try:
-        long,lat =  mun_code_coord[raw_entry['Código DIVIPOLA municipio']][1:]
+        long, lat = mun_code_coord[raw_entry['Código DIVIPOLA municipio']][1:]
         geometry = {'latitude': lat,
-                   'longitude': long }
-        mun,dep = mun_code_place_name[raw_entry['Código DIVIPOLA municipio']]
+                    'longitude': long}
+        mun, dep = mun_code_place_name[raw_entry['Código DIVIPOLA municipio']]
         place_name = f"{mun}, {dep}, Colombia"
 
         location = {}
@@ -68,30 +69,45 @@ def get_location(raw_entry):
 
         return location
 
-    except Exception as e:
+    except BaseException:
+        print(raw_entry)
         return None
 
 
+def get_travel_history_location(raw_entry):
+    country_spanish = raw_entry['Nombre del país']
 
+    try:
+        iso2 = spanish_country_code_dict[country_spanish.lower()]
+        lat, long, country = iso_country_coord_map[iso2]
+        geometry = {'latitude': lat,
+                    'longitude': long}
 
-    
+        location = {}
+        location["country"] = country
+        location["geoResolution"] = "Country"
+        location["name"] = country
+        location["geometry"] = geometry
+
+        return location
+    except BaseException:
+        print(country_spanish)
 
 
 def convert_demographics(entry):
     '''
-    This takes a whole row, and returns Age, Gender and Ethnicity in a dictionary. 
-    Age is given as an int, but the adjacent field, 'Age Measurement Unit', determines what this int represents. 
+    This takes a whole row, and returns Age, Gender and Ethnicity in a dictionary.
+    Age is given as an int, but the adjacent field, 'Age Measurement Unit', determines what this int represents.
     1 = Years; 2 = Months; 3 = Days
     '''
 
-    
-    ethnicity_map = {'1':'Indigenous',
-                     '2':'ROM',
-                     '3':'Raizal',
-                     '4':'Palenquero',
-                     '5':'Black',
-                     '6':'Other'}
-    
+    ethnicity_map = {'1': 'Indigenous',
+                     '2': 'ROM',
+                     '3': 'Raizal',
+                     '4': 'Palenquero',
+                     '5': 'Black',
+                     '6': 'Other'}
+
     demo = {}
     if entry['Edad']:
         if str(entry['Unidad de medida de edad']) == '1':
@@ -101,28 +117,26 @@ def convert_demographics(entry):
             }
         elif str(entry['Unidad de medida de edad']) == '2':
             demo["ageRange"] = {
-                "start": float(entry['Edad'])/12,
-                "end": float(entry['Edad'])/12
+                "start": float(entry['Edad']) / 12,
+                "end": float(entry['Edad']) / 12
             }
         elif str(entry['Unidad de medida de edad']) == '3':
             demo["ageRange"] = {
-                "start": float(entry['Edad'])/365,
-                "end": float(entry['Edad'])/365   
+                "start": float(entry['Edad']) / 365,
+                "end": float(entry['Edad']) / 365
             }
     if entry['Sexo']:
         demo["gender"] = convert_gender(entry['Sexo'])
-        
-    
+
     if entry['Pertenencia étnica']:
-        ethnicity = ethnicity_map.get(str(entry['Pertenencia étnica']),"")
-        if entry['Nombre del grupo étnico']:  
+        ethnicity = ethnicity_map.get(str(entry['Pertenencia étnica']), "")
+        if entry['Nombre del grupo étnico']:
             ethnicity += f", {entry['Nombre del grupo étnico']}"
     else:
         ethnicity = 'Unknown'
     demo["ethnicity"] = ethnicity
-        
-    return demo or None
 
+    return demo or None
 
 
 def parse_cases(raw_data_file, source_id, source_url):
@@ -132,27 +146,30 @@ def parse_cases(raw_data_file, source_id, source_url):
     Caveats:
     - Assuming the date confirmed is the date of diagnosis (Fecha diagnostico) rather than
     Fecha de notificación (generally several days earlier). When date of diagnosis, using date reported online as proxy.
-    
-    - Case can have date of death, but 'Recuperado' column says recovered. This corresponds to patients who died but 
+
+    - Case can have date of death, but 'Recuperado' column says recovered. This corresponds to patients who died but
     not from Covid-19.
 
-    - Notes added include date reported online and date that SIVIGILA (national health alert system) was notiifed. 
+    - Notes added include date reported online and date that SIVIGILA (national health alert system) was notiifed.
     Also whether case was imported, and how patient recovery was confirmed.
-    
+
     - Tipo recuperación refers to how they decided the patient had recovered: either by 21 days elapsing since
     symptoms, or a negative PCR/antigen test
 
-    - No dates for travel history, only distinction is between cases of type: 'Importado' vs. 'Relacionado'
-    
+    - No dates for travel history, only distinction is between cases of type: 'Importado' vs. 'Relacionado'.
+    We assume cases listed as importado (imported) have travelled in the last 30 days, and geocode their country of origin.
+
     """
 
     symptom_map = {'leve': 'Mild',
                    'moderado': 'Moderate',
                    'grave': 'Serious'}
+    ref_date = datetime.strptime('21-02-2021', '%d-%m-%Y')
 
     with open(raw_data_file, "r") as f:
         reader = csv.DictReader(f)
         cases = []
+        count = 0
         for entry in reader:
             location = get_location(entry)
             if entry["Fecha de diagnóstico"]:
@@ -163,13 +180,12 @@ def parse_cases(raw_data_file, source_id, source_url):
                 notes = []
                 case = {
                     "caseReference": {
-#                         "verificationStatus": "VERIFIED",
                         "sourceId": source_id,
                         "sourceEntryId": entry["ID de caso"],
                         "sourceUrl": source_url
-                    }, 
-                "location": location,
-                   "demographics": convert_demographics(entry)
+                    },
+                    "location": location,
+                    "demographics": convert_demographics(entry)
                 }
 
                 if entry["Fecha de diagnóstico"]:
@@ -194,11 +210,14 @@ def parse_cases(raw_data_file, source_id, source_url):
                             }
                         },
                     ]
-                    notes.append("No Date of Diagnosis provided, so using Date Reported Online (fecha reporte web) as a proxy. This is normally approx. 1 week later.")
+                    notes.append(
+                        "No Date of Diagnosis provided, so using Date Reported Online (fecha reporte web) as a proxy. This is normally approx. 1 week later.")
 
                 # If patient was symptomatic, mark date of onsetSymptoms, otherwise
                 # asymptomatic
-                if entry["Fecha de inicio de síntomas"]:  # maybe change to elif clause and check if can parse field as date
+                # maybe change to elif clause and check if can parse field as
+                # date
+                if entry["Fecha de inicio de síntomas"]:
                     case["symptoms"] = {
                         "status": "Symptomatic",
                     }
@@ -217,7 +236,8 @@ def parse_cases(raw_data_file, source_id, source_url):
 
                 # Include severity of symptoms
                 if entry["Estado"].lower() in symptom_map.keys():
-                    case["symptoms"]["values"] = [symptom_map.get(entry['Estado'].lower())]
+                    case["symptoms"]["values"] = [
+                        symptom_map.get(entry['Estado'].lower())]
 
                 # Patient Outcome
                 # If patient died, mark date
@@ -231,7 +251,8 @@ def parse_cases(raw_data_file, source_id, source_url):
                         }
                     })
                     if entry["Recuperado"].lower() != "fallecido":
-                        notes.append("Died from something other than Covid-19.")
+                        notes.append(
+                            "Died from something other than Covid-19.")
 
                 elif entry["Recuperado"].lower() == "recuperado":
                     case["events"].append({
@@ -258,32 +279,50 @@ def parse_cases(raw_data_file, source_id, source_url):
                     })
 
                 if entry["Ubicación del caso"].lower() == 'casa':
-                    notes.append("Patient self-isolated and recovered at home.")
+                    notes.append(
+                        "Patient self-isolated and recovered at home.")
 
-                # Add notes for each case
-                # Travel History - we currently do not have any travel dates, so unknown whether in last 30 days 
+                # Add travelHistory and notes for imported cases
+                # Travel History - we currently do not have any travel dates,
+                # so unknown whether in last 30 days, assuming YES
                 if entry["Tipo de contagio"].lower() == "importado":
-                    notes.append(f"Case is reported as importing the disease into Colombia, and country of origin is {entry['Nombre del país']}.")
+                    country_of_origin = entry['Nombre del país']
+                    case["travelHistory"] = {
+                        "traveledPrior30Days": True,
+                        "travel": [
+                            {
+                                "location": get_travel_history_location(entry)
+                            }]
+                    }
+                    notes.append(
+                        f"Case is reported as importing the disease into Colombia, and country of origin is {entry['Nombre del país']}.")
                 elif entry["Tipo de contagio"].lower() == 'relacionado':
                     notes.append("Case was transmitted within Colombia.")
                 elif entry["Tipo de contagio"].lower() == 'en estudio':
-                    notes.append("Case transmission under investigation (currently unknown).")
+                    notes.append(
+                        "Case transmission under investigation (currently unknown).")
 
                 if entry['fecha reporte web']:
-                    notes.append(f"Date reported online was {convert_date(entry['fecha reporte web'],dataserver=False)}.")
+                    notes.append(
+                        f"Date reported online was {convert_date(entry['fecha reporte web'],dataserver=False)}.")
                 if entry['Fecha de notificación']:
-                    notes.append(f"Date reported to SIVIGILA was {convert_date(entry['Fecha de notificación'],dataserver=False)}.")
+                    notes.append(
+                        f"Date reported to SIVIGILA was {convert_date(entry['Fecha de notificación'],dataserver=False)}.")
 
                 if entry['Tipo de recuperación'] == 'PCR':
-                    notes.append(f"Patient recovery was confirmed by a negative PCR test.")
+                    notes.append(
+                        f"Patient recovery was confirmed by a negative PCR test.")
                 elif entry['Tipo de recuperación'] == 'Tiempo':
-                    notes.append(f"Patient recovery was confirmed by 21 days elapsing with no symptoms.")
+                    notes.append(
+                        f"Patient recovery was confirmed by 21 days elapsing with no symptoms.")
 
-
-                if notes: 
+                if notes:
                     case["notes"] = ", ".join(notes)
 
+                count +=1
+                print(count)
                 yield case
+
 
 def lambda_handler(event, context):
     return parsing_lib.run_lambda(event, context, parse_cases)
