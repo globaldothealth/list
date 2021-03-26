0d415128cab45b87228f1a9f493839f55d74e93f
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-03-26 16:23:15 +0000

    actually make the API call

--- verification/curator-service/api/src/workers/downloadAsync.js
+++ verification/curator-service/api/src/workers/downloadAsync.js
@@ -1,4 +1,19 @@
 const { workerData } = require('worker_threads');
-
-console.log(`worker spawned for ${workerData.query} by ${workerData.email}`);
-
+const axios = require('axios');
+// make the request to the data service
+try {
+    axios({
+        method: 'post',
+        url: workerData.url,
+        data: {
+            query: workerData.query,
+        },
+        responseType: 'stream',
+    }).then((response) => {
+        // send the email using SES
+        console.log(`got data for query ${workerData.query} for user ${workerData.email}`);
+    });
+} catch (err) {
+    console.error(err);
+    throw err; // err is available in the main process
+}
