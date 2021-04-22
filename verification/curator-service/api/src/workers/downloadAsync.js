19347be841bd064df53d4c76f31e92c5479ab158
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-04-22 10:29:56 +0100

    Actually received an email with an attachment!

--- verification/curator-service/api/src/workers/downloadAsync.js
+++ verification/curator-service/api/src/workers/downloadAsync.js
@@ -10,8 +10,10 @@ try {
         data: {
             query: workerData.query,
         },
-        responseType: 'stream',
+        responseType: 'blob',
     }).then((response) => {
+        const buffer = Buffer.from(response.data, 'utf-8');
+        const base64 = buffer.toString('base64');
         // send the email using SES
         AWS.config.update({
             accessKeyId: workerData.accessKeyId,
@@ -19,25 +21,39 @@ try {
             region: workerData.region,
         });
         const ses = new AWS.SES({apiVersion: '2010-12-01'});
-        ses.sendEmail({
-            Destination: {
-                ToAddresses: [workerData.email],
-            },
-            Message: {
-                Body: {
-                    Text: {
-                        Charset: 'UTF-8',
-                        Data: 'Here is your global.health download',
-                    },
-                },
-                Subject: {
-                    Charset: 'UTF-8',
-                    Data: 'Greetings from global.health',
-                },
-            },
-            ReturnPath: workerData.sourceAddress,
-            Source: workerData.sourceAddress,
-        },(err, data) => {
+        /* I originally tried using the mimemessage library for this, but because the
+         * worker is outside the main .ts app the build pipeline just shook the module
+         * out of the tree so it would crash at runtime. So, old-school knowledge of how
+         * to write email attachments by hand (and trolling Usenet groups with spurious
+         * attachments) will have to suffice.
+         */
+        var mailMessage = `From: "Global.Health" <${workerData.sourceAddress}>
+To: ${workerData.email}
+Subject: Here is your global.health download
+Content-Type: multipart/mixed; boundary="part-boundary"
+
+--part-boundary
+Content-Type: text/plain; charset=utf-8
+Content-Transfer-Encoding: quoted-printable
+
+Please see the attached list of cases in response to your query ${workerData.query}.
+
+Kind regards,
+
+The Global.Health team.
+
+--part-boundary
+Content-Type: text/plain; name="g_dot_h_cases.csv"
+Content-Disposition: attachment; filename="g_dot_h_cases.csv"
+Content-Transfer-Encoding: base64
+
+${base64}
+
+--part-boundary--`;
+
+        ses.sendRawEmail({
+            RawMessage: { Data: mailMessage },
+          }, (err, data) => {
             console.log(`sent email with correlation id ${workerData.correlationId}`);
             if (err)
             {
