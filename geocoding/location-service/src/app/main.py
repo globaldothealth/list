79e5dac91646e6048288b0e69a490b8b661f8ad4
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-04-26 15:23:17 +0100

    Provide a geocoding endpoint

--- geocoding/location-service/src/app/main.py
+++ geocoding/location-service/src/app/main.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from os import environ
+
+from src.lib.mapbox_client import mapbox_geocode
 
 app = Flask(__name__)
 
@@ -6,6 +9,12 @@ app = Flask(__name__)
 @app.route("/health")
 def index() -> str:
     return jsonify({"status": "Healthy"})
-        
+
+@app.route("/geocode")
+def geocode():
+    query = request.args.get('q', type=str)
+    api_key = environ['MAPBOX_TOKEN']
+    return mapbox_geocode(api_key, query)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
