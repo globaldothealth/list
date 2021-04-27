829ba2f1f941e54dd72f2f20c8d38037b40db32b
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-04-27 16:21:06 +0100

    First test from data-service geocoding fixture ported.

--- geocoding/location-service/src/integration/mapbox_client.py
+++ geocoding/location-service/src/integration/mapbox_client.py
@@ -1,7 +1,7 @@
 import mapbox
 
-def mapbox_geocode(access_token, query):
+def mapbox_geocode(access_token, query, types=None, languages=None, limit=None):
     """Request the location of the point of interest identified in the search query.
     For the moment this is just whatever gets returned by mapbox."""
     geocoder = mapbox.Geocoder(access_token=access_token)
-    return geocoder.forward(query).geojson()
+    return geocoder.forward(query, types=types, languages=languages, limit=limit).geojson()
