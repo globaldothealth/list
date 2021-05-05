aabed4c92bae0386dc98377c815b49ea9024e963
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-05-05 12:43:22 +0100

    Unhappy path geocode suggestion case

--- geocoding/location-service/test/test_suggestions.py
+++ geocoding/location-service/test/test_suggestions.py
@@ -22,3 +22,8 @@ class SuggestionsTest(unittest.TestCase):
         response = self.client.get('/geocode/suggest?q=Lyon&limitToResolution=Country,Admin1')
         assert response.status == '200 OK'
         assert response.json == [self.lyon]
+
+    def test_badResolutionLimitsResultInError(self):
+        response = self.client.get('/geocode/suggest?q=Lyon&limitToResolution=nopenope')
+        assert response.status == '422 UNPROCESSABLE ENTITY'
+        assert response.data.find(b'nopenope') != -1
