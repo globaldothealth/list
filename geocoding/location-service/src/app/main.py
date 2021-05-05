176a42825707f8dfc860e03655d8283fe41fbb08
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-05-05 17:07:07 +0100

    integrate mongo into the geolocation service

--- geocoding/location-service/src/app/main.py
+++ geocoding/location-service/src/app/main.py
@@ -1,6 +1,8 @@
+import pymongo
 from flask import Flask, jsonify, request
 from os import environ
 
+from src.app.admins_fetcher import AdminsFetcher
 from src.app.fake_geocoder import FakeGeocoder
 from src.app.geocoder import Geocoder
 from src.app.geocoder_suggester import GeocodeSuggester
@@ -14,7 +16,13 @@ fake_geocoder = FakeGeocoder()
 if 'ENABLE_FAKE_GEOCODER' in environ:
     geocoders.append(fake_geocoder)
 if 'MAPBOX_TOKEN' in environ:
-    mapbox_geocoder = Geocoder(environ['MAPBOX_TOKEN'])
+    access_token = environ['MAPBOX_TOKEN']
+    mongo_client = None
+    if 'DB_CONNECTION_STRING' in environ:
+        mongo_client = pymongo.MongoClient(environ['DB_CONNECTION_STRING'])
+
+    admins_fetcher = AdminsFetcher(access_token, mongo_client)
+    mapbox_geocoder = Geocoder(access_token, admins_fetcher)
     geocoders.append(mapbox_geocoder)
 
 suggester = GeocodeSuggester(geocoders)
