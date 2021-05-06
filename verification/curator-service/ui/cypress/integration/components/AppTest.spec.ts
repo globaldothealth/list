45b7799ae3fa8d1402e4a5101924a7a06ef800e6
Sergio <sergioloporto@gmail.com>
2021-05-06 16:11:16 +0200

    fixed merge conflicts

--- verification/curator-service/ui/cypress/integration/components/AppTest.spec.ts
+++ verification/curator-service/ui/cypress/integration/components/AppTest.spec.ts
@@ -43,7 +43,7 @@ describe('App', function () {
         cy.contains('2020-02-15').should('not.exist');
     });
 
-    
+
     it('allows the user to search by nationality', function () {
         cy.login();
         cy.visit('/cases');
@@ -62,7 +62,7 @@ describe('App', function () {
 
         cy.contains('American, Filipino, Polish');
     });
-    
+
 
     it('allows the user to search by variant', function () {
         cy.login();
@@ -101,14 +101,13 @@ describe('App', function () {
         }
 
         cy.visit('/cases');
-
+        
         cy.get('body').then(($body) => {
             if ($body.find('.iubenda-cs-accept-btn').length) {
                 cy.get('.iubenda-cs-accept-btn').click();
             }
-        });
-
-        cy.get('.filter-button').click();
+        });        cy.get('.filter-button').click();
+        
         cy.get('#dateconfirmedafter').type('2020-04-30');
         cy.get('#country').type('italy{Enter}');
 
