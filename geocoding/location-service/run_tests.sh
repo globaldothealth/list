29d66a97f60bb36b8a6fbe3b337b2bf96a30e1d6
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-05-06 10:39:01 +0100

    run tests in poetry

--- geocoding/location-service/run_tests.sh
+++ geocoding/location-service/run_tests.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-ENV=`mktemp -d`
-python3 -mvenv "${ENV}"
-. "${ENV}/bin/activate"
-pip install -r requirements.txt
-ENABLE_FAKE_GEOCODER=YES pytest
-deactivate
-rm -rf "${ENV}"
+poetry install
+poetry update
+ENABLE_FAKE_GEOCODER=YES poetry run pytest
