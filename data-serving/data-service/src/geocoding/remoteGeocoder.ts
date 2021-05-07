0357ed78dd759fe262836d997eacf2f95d2020fa
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-05-07 11:08:57 +0100

    New geocoder that calls out to the remote service

---
+++ data-serving/data-service/src/geocoding/remoteGeocoder.ts
@@ -0,0 +1,19 @@
+import { GeocodeOptions, GeocodeResult } from './geocoder';
+import axios from 'axios';
+
+export default class Geocoder {
+    constructor(private readonly baseUrl: string) {}
+
+    async geocode(
+        query: string,
+        opts?: GeocodeOptions,
+    ): Promise<GeocodeResult[]> {
+        const result = await axios.get(this.baseUrl + '/geocode', {
+            params: {
+                q: query,
+                ...opts,
+            },
+        });
+        return result.data;
+    }
+}
