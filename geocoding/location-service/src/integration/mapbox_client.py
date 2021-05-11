814289d84ec6c46ccf90959c80eea93670a0a40b
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-05-11 15:19:37 +0100

    Pass the limitToCountry option to the geocoder

--- geocoding/location-service/src/integration/mapbox_client.py
+++ geocoding/location-service/src/integration/mapbox_client.py
@@ -2,10 +2,10 @@ import mapbox
 import requests
 
 
-def mapbox_geocode(access_token, query, types=None, languages=None, limit=None):
+def mapbox_geocode(access_token, query, types=None, languages=None, limit=None, country=None):
     """Request the location of the point of interest identified in the search query."""
     geocoder = mapbox.Geocoder(access_token=access_token)
-    return geocoder.forward(query, types=types, languages=languages, limit=limit).geojson()
+    return geocoder.forward(query, types=types, languages=languages, limit=limit, country=country).geojson()
 
 
 def mapbox_tile_query(access_token, query):
