98a7338104014317eb567a13f2e4c8269d20819b
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-05-11 15:56:23 +0100

    review suggestions

--- geocoding/location-service/src/integration/mapbox_client.py
+++ geocoding/location-service/src/integration/mapbox_client.py
@@ -1,25 +1,19 @@
+from contextlib import nullcontext
 import mapbox
 import requests
 
 
-class EmptyContext:
-    def __enter__(self):
-        pass
-    def __exit__(self):
-        pass
-
-
-def mapbox_geocode(access_token, query, types=None, languages=None, limit=None, country=None, rate_limit=EmptyContext()):
+def mapbox_geocode(access_token, query, types=None, languages=None, limit=None, country=None, rate_limit=nullcontext()):
     """Request the location of the point of interest identified in the search query."""
     with rate_limit:
         geocoder = mapbox.Geocoder(access_token=access_token)
         return geocoder.forward(query, types=types, languages=languages, limit=limit, country=country).geojson()
 
 
-def mapbox_tile_query(access_token, query, rate_limit=EmptyContext()):
+def mapbox_tile_query(access_token, query, rate_limit=nullcontext()):
     """Request the full hierarchy of administrative areas for a given location."""
     with rate_limit:
         uri = f"https://api.mapbox.com/v4/mapbox.enterprise-boundaries-a1-v2,mapbox.enterprise-boundaries-a2-v2,mapbox.enterprise-boundaries-a3-v2/tilequery/{query.longitude},{query.latitude}.json?access_token={access_token}"
         res = requests.get(uri)
-        return res.json
+        return res.json()
 
