09f0a9560452b56b04b69134b2c3588397b04d0c
Sergio <sergioloporto@gmail.com>
2021-05-27 14:53:57 +0200

    added redux test utils

--- verification/curator-service/ui/src/components/App/App.test.tsx
+++ verification/curator-service/ui/src/components/App/App.test.tsx
@@ -3,14 +3,14 @@ import { MemoryRouter, Router } from 'react-router-dom';
 import React from 'react';
 import axios from 'axios';
 import {
-    fireEvent,
-    render,
     wait,
-    screen,
     within,
 } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { createMemoryHistory } from 'history';
+import { render, fireEvent, screen } from '../util/test-utils'
+
+
 
 jest.mock('axios');
 // Mock charts page so that requests for mongo charts are not sent
