f3d067444c27102313f492a84ec2c33f54aed218
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-06-07 09:48:46 +0100

    Ensure pymongo knows how to authenticate

--- geocoding/location-service/src/app/main.py
+++ geocoding/location-service/src/app/main.py
@@ -34,7 +34,7 @@ if 'MAPBOX_TOKEN' in environ:
     access_token = environ['MAPBOX_TOKEN']
     mongo_client = None
     if 'DB_CONNECTION_STRING' in environ:
-        mongo_client = pymongo.MongoClient(environ['DB_CONNECTION_STRING'])
+        mongo_client = pymongo.MongoClient(environ['DB_CONNECTION_STRING'], appname='location-service', authSource='admin')
     rate_limit = int(environ.get('MAPBOX_GEOCODE_RATE_LIMIT_PER_MIN', 600))
     admins_fetcher = AdminsFetcher(access_token, mongo_client[environ['DB']], rate_limit=rate_limit)
     mapbox_geocoder = Geocoder(access_token, admins_fetcher, rate_limit=rate_limit)
