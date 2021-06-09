0b6a52f4c50a9b584a172af3d1d4a5204479e824
Maciej Zarzeczny <maciej.zarzeczny@htdevelopers.com>
2021-06-09 10:56:31 +0200

    Refactored LandingPage component and created Login UI

--- verification/curator-service/ui/src/components/landing-page/LandingPage.tsx
+++ verification/curator-service/ui/src/components/landing-page/LandingPage.tsx
@@ -1,19 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Paper, Typography } from '@material-ui/core';
 import { Theme, makeStyles } from '@material-ui/core/styles';
 import GoogleButton from 'react-google-button';
 import { useLastLocation } from 'react-router-last-location';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
-import getRandomString from '../util/randomString';
-import { Auth } from 'aws-amplify';
 import { CognitoUser } from '@aws-amplify/auth';
 import User from '../User';
-import WrongCodeDialog from './WrongCodeDialog';
-import ErrorDialog from './ErrorDialog';
-import axios from 'axios';
 import SignInForm from './SignInForm';
-import VerificationCodeForm from './VerificationCodeForm';
 
 import PolicyLink from '../PolicyLink';
 import PartnerLogos from './PartnerLogos';
@@ -97,170 +91,16 @@ interface LandingPageProps {
 export default function LandingPage({
     setUser,
 }: LandingPageProps): JSX.Element {
-    const [isAgreementChecked, setIsAgreementChecked] = useState<boolean>(
-        false,
-    );
-    const [isAgreementMessage, setIsAgreementMessage] = useState<boolean>(
-        false,
-    );
-    const [isNewsletterChecked, setIsNewsletterChecked] = useState<boolean>(
-        false,
-    );
-    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
-    const [codeSent, setCodeSent] = useState<boolean>(false);
-    const [failedAttempts, setFailedAttempts] = useState<number>(0);
-    const [wrongCodeMessage, setWrongCodeMessage] = useState<boolean>(false);
-    const [wrongCodeDialog, setWrongCodeDialog] = useState<boolean>(false);
-    const [errorDialog, setErrorDialog] = useState<boolean>(false);
-    const [authState, setAuthState] = useState<{
-        user: CognitoUserExtended | null;
-        email: string;
-        status: string;
-    }>();
     const smallHeight = useMediaQuery('(max-height:1050px)');
     const classes = useStyles({ smallHeight });
     const lastLocation = useLastLocation();
 
-    const resetState = () => {
-        setCodeSent(false);
-        setWrongCodeMessage(false);
-        setFailedAttempts(0);
-        setIsAgreementChecked(false);
-        setAuthState({ user: null, email: '', status: '' });
-    };
-
-    useEffect(() => {
-        if (!authState) return;
-
-        const { user, status, email } = authState;
-        switch (status) {
-            case 'UserNotFoundException':
-                signUp(email);
-                break;
-            case 'signedUp':
-                signIn(email);
-                break;
-            case 'signedIn':
-                setCodeSent(true);
-                setIsSubmitting(false);
-                break;
-            case 'authenticated':
-                if (!user) return;
-
-                axios
-                    .post('/auth/signup', {
-                        name: undefined,
-                        email: user.attributes.email,
-                        roles: [],
-                        newsletter: isNewsletterChecked,
-                    })
-                    .then((res) => {
-                        const { _id, email, name, roles, picture } = res.data;
-                        const newUser: User = {
-                            _id,
-                            email,
-                            name,
-                            roles,
-                            picture,
-                        };
-                        setUser(newUser);
-                        setIsSubmitting(false);
-                    })
-                    .catch((err) => {
-                        setErrorDialog(true);
-                        setIsSubmitting(false);
-                    });
-                break;
-            case 'wrongCode':
-                if (failedAttempts >= 3) {
-                    setWrongCodeDialog(true);
-                } else {
-                    setWrongCodeMessage(true);
-                }
-                setIsSubmitting(false);
-                break;
-            case 'error':
-                if (failedAttempts < 2) {
-                    setErrorDialog(true);
-                }
-                setIsSubmitting(false);
-                break;
-            default:
-                setErrorDialog(true);
-                setIsSubmitting(false);
-                break;
-        }
-        // eslint-disable-next-line
-    }, [authState, failedAttempts, setUser]);
-
+    // Store searchQuery in localStorage to apply filters after going through login process
     useEffect(() => {
         if (!lastLocation || lastLocation.search === '') return;
 
         localStorage.setItem('searchQuery', lastLocation.search);
     }, [lastLocation]);
-
-    const signUp = (email: string) => {
-        const params = {
-            username: email,
-            password: getRandomString(30),
-            attributes: {
-                email,
-            },
-        };
-
-        Auth.signUp(params)
-            .then((res) => {
-                setAuthState({ user: null, email, status: 'signedUp' });
-            })
-            .catch((err) => {
-                setAuthState({ user: null, email, status: err.code });
-            });
-    };
-
-    const signIn = async (email: string, resetForm?: () => void) => {
-        setIsSubmitting(true);
-
-        Auth.signIn(email)
-            .then((user) => {
-                setAuthState({ user, email, status: 'signedIn' });
-                if (resetForm) resetForm();
-            })
-            .catch((err) => {
-                setAuthState({ user: null, email, status: err.code });
-            });
-    };
-
-    const submitVerificationCode = async (code: string) => {
-        if (!authState?.user) return;
-        setIsSubmitting(true);
-
-        try {
-            await Auth.sendCustomChallengeAnswer(authState.user, code);
-        } catch (err) {
-            setAuthState({
-                user: authState.user,
-                email: '',
-                status: 'error',
-            });
-        }
-
-        Auth.currentAuthenticatedUser()
-            .then((user) => {
-                setAuthState({
-                    user,
-                    email: '',
-                    status: 'authenticated',
-                });
-            })
-            .catch((err) => {
-                setFailedAttempts((val) => val + 1);
-                setAuthState({
-                    user: authState.user,
-                    email: '',
-                    status: 'wrongCode',
-                });
-            });
-    };
 
     return (
         <Paper classes={{ root: classes.paper }}>
@@ -345,64 +185,10 @@ export default function LandingPage({
                 </div>
             </div>
 
-            {!codeSent && (
-                <GoogleButton
-                    className={classes.googleButton}
-                    disabled={isSubmitting}
-                    onClick={() => {
-                        if (!isAgreementChecked) {
-                            setIsAgreementMessage(true);
-                        } else {
-                            window.location.href = `${process.env
-                                .REACT_APP_LOGIN_URL!}?newsletterAccepted=${isNewsletterChecked}`;
-                        }
-                    }}
-                />
-            )}
+            <GoogleButton className={classes.googleButton} />
 
-            <Typography className={classes.authFormTitle}>
-                {codeSent
-                    ? 'Enter verification code sent to your email address'
-                    : 'Or sign in with email'}
-            </Typography>
+            <SignInForm />
 
-            {!codeSent && (
-                <SignInForm
-                    handleSubmit={signIn}
-                    isAgreementChecked={isAgreementChecked}
-                    setIsAgreementChecked={setIsAgreementChecked}
-                    setIsAgreementMessage={setIsAgreementMessage}
-                    isAgreementMessage={isAgreementMessage}
-                    isSubmitting={isSubmitting}
-                    isNewsletterChecked={isNewsletterChecked}
-                    setIsNewsletterChecked={setIsNewsletterChecked}
-                    classes={{
-                        emailField: classes.emailField,
-                        divider: classes.divider,
-                        loader: classes.loader,
-                        signInButton: classes.signInButton,
-                    }}
-                />
-            )}
-            {codeSent && (
-                <VerificationCodeForm
-                    handleSubmit={submitVerificationCode}
-                    setWrongCodeMessage={setWrongCodeMessage}
-                    wrongCodeMessage={wrongCodeMessage}
-                    isSubmitting={isSubmitting}
-                    classes={{
-                        emailField: classes.emailField,
-                        loader: classes.loader,
-                        signInButton: classes.signInButton,
-                    }}
-                />
-            )}
-            <WrongCodeDialog
-                isOpen={wrongCodeDialog}
-                setIsOpen={setWrongCodeDialog}
-                onClose={resetState}
-            />
-            <ErrorDialog isOpen={errorDialog} setIsOpen={setErrorDialog} />
             <PartnerLogos />
         </Paper>
     );
