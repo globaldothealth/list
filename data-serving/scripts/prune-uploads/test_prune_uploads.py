5fa398a2a785d67f3d0053d8ee9443976fd0cc85
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2021-07-20 21:27:24 +0000

    Add prune uploads script for async deletion #2002

---
+++ data-serving/scripts/prune-uploads/test_prune_uploads.py
@@ -0,0 +1,60 @@
+import pytest
+from bson.objectid import ObjectId
+from datetime import datetime
+from enum import Enum
+
+from prune_uploads import find_successful_upload
+
+Status = Enum("Status", "SUCCESS IN_PROGRESS ERROR")
+
+
+def _u(i, status, date):
+    return {
+        "_id": ObjectId(i),
+        "status": status.name,
+        "created": datetime.fromisoformat(date),
+    }
+
+
+S0 = {"_id": ObjectId("123456789012345678901231"), "hasStableIdentifiers": True}
+S1 = {"_id": ObjectId("123456789012345678901232"), "uploads": []}
+
+S2 = {
+    "_id": ObjectId("123456789012345678901230"),
+    "uploads": [
+        _u("60f733dcfae8bf76717d598e", Status.SUCCESS, "2021-01-01"),
+        _u("60f734296e50eb2592992fb0", Status.ERROR, "2020-12-31"),
+        _u("60f7343a6e50eb2592992fb1", Status.ERROR, "2020-12-25"),
+        _u("60f7343a6e50eb2592992fb2", Status.SUCCESS, "2021-02-11"),
+        _u("60f7343a6e50eb2592992fb3", Status.ERROR, "2020-05-05"),
+        _u("60f7343a6e50eb2592992fb2", Status.IN_PROGRESS, "2021-02-12"),
+    ],
+}
+
+S2_expected = (
+    "60f7343a6e50eb2592992fb2",
+    [
+        "60f733dcfae8bf76717d598e",
+        "60f734296e50eb2592992fb0",
+        "60f7343a6e50eb2592992fb1",
+        "60f7343a6e50eb2592992fb3",
+    ],
+)
+
+S3 = {
+    "_id": ObjectId("123456789012345678901233"),
+    "hasStableIdentifiers": False,
+    "uploads": [
+        _u("60f734296e50eb2592992fb0", Status.ERROR, "2020-12-31"),
+        _u("60f7343a6e50eb2592992fb1", Status.ERROR, "2020-12-25"),
+        _u("60f7343a6e50eb2592992fb3", Status.ERROR, "2020-05-05"),
+        _u("60f7343a6e50eb2592992fb4", Status.IN_PROGRESS, "2021-01-01"),
+    ],
+}
+
+T = [(S1, None), (S2, S2_expected), (S3, None)]
+
+
+@pytest.mark.parametrize("source,expected", T)
+def test_find_successful_upload(source, expected):
+    assert find_successful_upload(source) == expected
