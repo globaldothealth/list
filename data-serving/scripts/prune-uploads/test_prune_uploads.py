b49e6dc9273f047a2c316e5cbb67aab03852d0a0
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2021-07-28 21:35:39 +0000

    prune-uploads: Mark accepted uploads in sources collection
    Ignore previously accepted or rejected uploads

--- data-serving/scripts/prune-uploads/test_prune_uploads.py
+++ data-serving/scripts/prune-uploads/test_prune_uploads.py
@@ -12,17 +12,16 @@ ERROR_THRESHOLD = 0.1
 Status = Enum("Status", "SUCCESS IN_PROGRESS ERROR")
 
 
-def _u(i, status, date, created=0, errors=0, updated=0):
-    return {
+def _u(i, status, date, created=0, errors=0, updated=0, accepted=None):
+    u = {
         "_id": ObjectId(i),
         "status": status.name,
         "created": datetime.fromisoformat(date),
-        "summary": {
-            "numCreated": created,
-            "numUpdated": updated,
-            "numError": errors
-        }
+        "summary": {"numCreated": created, "numUpdated": updated, "numError": errors},
     }
+    if accepted is not None:
+        u['accepted'] = accepted
+    return u
 
 
 S0 = {"_id": ObjectId("123456789012345678901231"), "hasStableIdentifiers": True}
@@ -43,8 +42,24 @@ S2 = {
     ],
 }
 
+S2_accepted = {
+    "_id": ObjectId("123456789012345678901230"),
+    "uploads": [
+        _u("60f733dcfae8bf76717d598e", Status.SUCCESS, "2021-01-01", 100, 5, accepted=True),
+        _u("60f734296e50eb2592992fb0", Status.ERROR, "2020-12-31", accepted=False),
+        _u("60f7343a6e50eb2592992fb1", Status.ERROR, "2020-12-25", accepted=False),
+        _u("60f7343a6e50eb2592992fb2", Status.SUCCESS, "2021-02-11", 10, 1),
+        _u("60f7343a6e50eb2592992fc2", Status.SUCCESS, "2021-02-12", 10, 2),
+        # ^ rejected as errors above threshold
+        _u("60f7343a6e50eb2592992fb3", Status.ERROR, "2020-05-05", accepted=False),
+        _u("60f7343a6e50eb2592992fb4", Status.ERROR, "2020-02-13", accepted=False),
+        _u("60f7343a6e50eb2592992fb5", Status.IN_PROGRESS, "2021-02-14"),
+    ],
+}
+
+
 S2_expected = (
-    "60f7343a6e50eb2592992fb2",
+    ["60f7343a6e50eb2592992fb2"],
     [
         "60f733dcfae8bf76717d598e",
         "60f734296e50eb2592992fb0",
@@ -54,6 +69,8 @@ S2_expected = (
         "60f7343a6e50eb2592992fc2",
     ],
 )
+
+S2_accepted_expected = ["60f7343a6e50eb2592992fb2"], ["60f7343a6e50eb2592992fc2"]
 
 S3 = {
     "_id": ObjectId("123456789012345678901233"),
@@ -67,7 +84,7 @@ S3 = {
     ],
 }
 
-T = [(S1, None), (S2, S2_expected), (S3, None)]
+T = [(S1, None), (S2, S2_expected), (S2_accepted, S2_accepted_expected), (S3, None)]
 
 
 @pytest.mark.parametrize("source,expected", T)
@@ -77,6 +94,8 @@ def test_find_acceptable_upload(source, expected):
 
 @pytest.mark.parametrize("source,expected", [(x, None) for x in [S1, S2, S3]])
 def test_find_acceptable_upload_with_epoch(source, expected):
-    assert find_acceptable_upload(
-        source, ERROR_THRESHOLD, datetime(2021, 3, 1)
-    ) == expected
+    assert (
+        find_acceptable_upload(source, ERROR_THRESHOLD, datetime(2021, 3, 1))
+        == expected
+    )
+
