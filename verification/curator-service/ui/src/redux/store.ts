b7c97804bb4525658e34e3cb4cad0eb9e966c787
Sergio <sergioloporto@gmail.com>
2021-08-19 16:05:30 +0200

    first part of task - connecting redux

--- verification/curator-service/ui/src/redux/store.ts
+++ verification/curator-service/ui/src/redux/store.ts
@@ -2,10 +2,12 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 import appReducer from './app/slice';
 import authReducer from './auth/slice';
+import countryListReducer from './countryList/CountryList';
 
 export const rootReducer = combineReducers({
     app: appReducer,
     auth: authReducer,
+    countryListReducer
 });
 
 const store = configureStore({
@@ -13,6 +15,7 @@ const store = configureStore({
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
