dab6646ada617f5fe1b2c43c5e069187dcf33145
Sergio <sergioloporto@gmail.com>
2021-08-31 09:01:50 +0200

    fixed types

--- verification/curator-service/ui/src/redux/filters/selectors.ts
+++ verification/curator-service/ui/src/redux/filters/selectors.ts
@@ -1,3 +1,4 @@
 import { RootState } from '../store';
 
 export const countryList = (state: RootState) => state.countryListReducer.countryList;
+export const isLoading = (state: RootState) => state.countryListReducer.isLoading;
