86de7b82fb3e78bb67bbd84a9890bf11d6a2352e
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-09-02 14:16:09 +0100

    Replace dev script for setup database with migration #1880

---
+++ data-serving/scripts/setup-db/migrate-mongo-config.js
@@ -0,0 +1,34 @@
+// In this file you can configure migrate-mongo
+
+const config = {
+  mongodb: {
+    // TODO Change (or review) the url to your MongoDB:
+    url: process.env.CONN,
+
+    // TODO Change this to your database name:
+    databaseName: process.env.DB,
+
+    options: {
+      useNewUrlParser: true, // removes a deprecation warning when connecting
+      useUnifiedTopology: true, // removes a deprecating warning when connecting
+      //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
+      //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
+    }
+  },
+
+  // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
+  migrationsDir: "migrations",
+
+  // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
+  changelogCollectionName: "changelog",
+
+  // The file extension to create migrations and search for in migration dir 
+  migrationFileExtension: ".js",
+
+  // Enable the algorithm to create a checksum of the file contents and use that in the comparison to determin
+  // if the file should be run.  Requires that scripts are coded to be run multiple times.
+  useFileHash: false
+};
+
+// Return the config as a promise
+module.exports = config;
