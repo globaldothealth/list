7ac1ee8b61c130df8b13e3d4874b1304a068fc7e
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-09-02 15:43:18 +0100

    setup_db.sh can only connect to local mongo #1880

--- dev/setup_db.sh
+++ dev/setup_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONN="${CONN:-mongodb://localhost/covid19}"
+CONN="mongodb://localhost/covid19"
 
 npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script migrate
 npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script drop-cases-and-import-sample-data
