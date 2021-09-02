36aa984a505d3ccabfb299569e7c216d723ac0bf
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-09-02 10:16:57 +0100

    move sample data preparation to the setup-db script #1880

--- dev/setup_db.sh
+++ dev/setup_db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+CONN="${CONN:-mongodb://localhost/covid19}"
+
 npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script setup-cases
 npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script setup-restricted-cases
-npm --prefix=`dirname "$0"`/../data-serving/data-service/ run-script drop-cases
-npm --prefix=`dirname "$0"`/../data-serving/data-service/ run-script import-data
+npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script drop-cases-and-import-sample-data
