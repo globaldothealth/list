6f77733dac68120459c4d3ad6276f30461f6900e
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-09-02 14:16:09 +0100

    Replace dev script for setup database with migration #1880

--- dev/setup_db.sh
+++ dev/setup_db.sh
@@ -2,6 +2,5 @@
 
 CONN="${CONN:-mongodb://localhost/covid19}"
 
-npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script setup-cases
-npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script setup-restricted-cases
+npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script migrate
 npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script drop-cases-and-import-sample-data
