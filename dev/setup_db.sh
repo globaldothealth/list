898bf6a6836d72839573b142665e40912b2551a1
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-09-06 16:39:34 +0100

    Set the DB environment variable in the dev setup script #1880

--- dev/setup_db.sh
+++ dev/setup_db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CONN="mongodb://localhost/covid19"
+DB="covid19"
 
 npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script migrate
 npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script drop-cases-and-import-sample-data
