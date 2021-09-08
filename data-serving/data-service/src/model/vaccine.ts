4240ca3d6505a1b2828fc8dbb7b7934ff28ebd10
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-09-08 16:32:41 +0100

    There should be one side-effects document (with multiple side-effects) per vaccine #2120
    Note that this change will need a backend migration too

--- data-serving/data-service/src/model/vaccine.ts
+++ data-serving/data-service/src/model/vaccine.ts
@@ -17,7 +17,7 @@ export type VaccineDocument = mongoose.Document & {
     name: string;
     batch: string;
     date: Date;
-    sideEffects: [SymptomsDocument];
+    sideEffects: SymptomsDocument;
     previousInfection: 'yes' | 'no' | 'NA';
     previousInfectionDetectionMethod: string;
 };
