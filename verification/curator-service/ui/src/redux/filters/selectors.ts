83e1e89e7356bfc3927ad383e307ec9269f32aae
Sergio <sergioloporto@gmail.com>
2021-09-16 15:55:02 +0200

    fixed redux and cypress

--- verification/curator-service/ui/src/redux/filters/selectors.ts
+++ verification/curator-service/ui/src/redux/filters/selectors.ts
@@ -2,3 +2,4 @@ import { RootState } from '../store';
 
 export const countryList = (state: RootState) => state.filtersReducer.countryList;
 export const isLoading = (state: RootState) => state.filtersReducer.isLoading;
+export const filterError = (state: RootState) => state.filtersReducer.error;
