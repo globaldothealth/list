1ec74c182d80d95d4f74cea752190dccfbf0ffd2
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2021-10-29 19:54:00 +0000

    export: Add support for TSV,JSON,JSONL exports
    Selection of formats can be controlled by setting the
    FORMAT environment variable when calling country_export.sh.

--- data-serving/scripts/export-data/country_export.sh
+++ data-serving/scripts/export-data/country_export.sh
@@ -2,8 +2,13 @@
 
 set -euo pipefail
 
+FORMAT="${FORMAT:-csv,tsv,json}"
 SLUG=$(echo "$COUNTRY" | sed "s/ /_/g;s/[.,']//g" | tr '[:upper:]' '[:lower:]')
 QUERY="{\"list\": true, \"location.country\": \"$COUNTRY\"}"
 mongoexport --query="$QUERY" --uri="$CONN" --collection=cases \
-    --fieldFile=fields.txt --type=csv | python3 transform.py | gzip > "$SLUG.csv.gz"
-aws s3 cp "$SLUG.csv.gz" s3://covid-19-country-export/csv/
+    --fieldFile=fields.txt --type=csv | python3 transform.py -f "$FORMAT" "$SLUG"
+
+for fmt in ${FORMAT//,/ }
+do
+    aws s3 cp "$SLUG.$fmt.gz" s3://covid-19-country-export/$fmt/
+done
