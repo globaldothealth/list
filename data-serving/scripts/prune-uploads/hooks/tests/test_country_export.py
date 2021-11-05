7470be10e258e92b4f6ef0c4b8ba6dda6ae97ffc
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2021-11-05 21:07:23 +0000

    prune-uploads: Add hook for submitting country-export jobs
    This also adds a dependency on boto3 and pycountry

---
+++ data-serving/scripts/prune-uploads/hooks/tests/test_country_export.py
@@ -0,0 +1,39 @@
+import pytest
+import hooks.country_export as T
+
+# some sources (only name and countryCodes)
+_NORTH_AMERICA = {"name": "North America", "countryCodes": ["US", "CA", "MX"]}
+_REUNION = {"name": "Reunion", "countryCodes": ["RE"]}
+_INVALID = {"name": "Asgard", "countryCodes": ["AA"]}
+_NEW_ZEALAND = {"name": "New Zealand"}
+
+
+@pytest.mark.parametrize(
+    "source,expected", [("Réunion", "Reunion"), ("Côte d'Ivoire", "Cote d'Ivoire")]
+)
+def test_to_ascii(source, expected):
+    assert T.to_ascii(source) == expected
+
+
+@pytest.mark.parametrize(
+    "source,expected",
+    [("Cote d'Ivoire", "cote_divoire"), ("Virgin Islands, U.S.", "virgin_islands_us")],
+)
+def test_slug(source, expected):
+    assert T.slug(source) == expected
+
+
+@pytest.mark.parametrize(
+    "source,expected",
+    [
+        (
+            _NORTH_AMERICA,
+            {"exporter_united_states", "exporter_canada", "exporter_mexico"},
+        ),
+        (_REUNION, {"exporter_reunion"}),
+        (_NEW_ZEALAND, {"exporter_new_zealand"}),
+        (_INVALID, set()),
+    ],
+)
+def test_get_exporters(source, expected):
+    assert T.get_exporters(source) == expected
