8c8b867484c5beb609ca6c4c3b5d060db218b1b7
Graham Lee <graham.lee@cs.ox.ac.uk>
2021-11-12 10:28:45 +0000

    Fix bugs in migration #1875

--- data-serving/scripts/setup-db/migrations/20211109102327-denormalise-confirmation-date-1875.js
+++ data-serving/scripts/setup-db/migrations/20211109102327-denormalise-confirmation-date-1875.js
@@ -2,6 +2,16 @@ const confirmationDate = {
   bsonType: "date",
 };
 
+const indexes = [
+  {
+    name: 'byConfirmationDateIfListed',
+    key: {
+      'confirmationDate': 1,
+      'list': 1,
+    }
+  },
+];
+
 module.exports = {
   async up(db, client) {
     let res = await db.command({listCollections: undefined, filter: {name: 'cases'}});
@@ -11,23 +21,29 @@ module.exports = {
 
     // now amend the existing cases. This must be done one by one so will take a lot of time!
     const collection = db.collection('cases');
-    collection.find({}).forEach(function(err, doc) {
-      if(!doc) {
-        console.error(`error ${err} migrating confirmation date on doc ${doc?._id ?? ''}`);
-        // continue as subsequent case modifications may still work
-        return;
-      }
+    collection.find({}).forEach(function(doc) {
       doc.events.forEach((anEvent) => {
         if (anEvent.name === 'confirmed') {
           const confirmationDate = anEvent.dateRange.start;
-          collection.updateOne({ _id: doc._id }, { confirmationDate });
+          collection.updateOne({ _id: doc._id }, { $set: { confirmationDate } });
           return;
         }
       });
     });
+
+    // and index the above
+    await db.command({
+      createIndexes: 'cases',
+      indexes: indexes,
+    });
   },
 
   async down(db, client) {
+    await db.command({
+      dropIndexes: 'cases',
+      index: ['byConfirmationDateIfListed']
+    });
+    
     let res = await db.command({listCollections: undefined, filter: {name: 'cases'}});
     let schema = res.cursor.firstBatch[0].options.validator.$jsonSchema;
     delete schema.properties.confirmationDate;
