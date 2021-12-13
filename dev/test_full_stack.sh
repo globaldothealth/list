304da7b06c12ff2c2235fb9ced934d9de17761d4
jim-sheldon <jimisasheldon@gmail.com>
2021-12-13 09:27:09 -0500

    Allow calling the test script from any directory

--- dev/test_full_stack.sh
+++ dev/test_full_stack.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
-
+# Store current directory.
+pushd `pwd`
+# We have to run docker compose from this directory for it to pick up the .env file.
+cd `dirname "$0"`
 docker compose -f docker-compose.yml -f docker-compose.dev.full.test.yml up --build --force-recreate --renew-anon-volumes --exit-code-from test
+# Restore directory.
+popd
