cb548c7852251daee5030a3fbad50e150e4ad98b
jim-sheldon <jimisasheldon@gmail.com>
2021-12-16 21:43:56 -0500

    Replacing data script print statements with logging

--- data-serving/scripts/prune-uploads/hooks/aggregate.py
+++ data-serving/scripts/prune-uploads/hooks/aggregate.py
@@ -1,8 +1,10 @@
 # Runs aggregate job after prune-uploads
+import logging
 import os
 from typing import Any
 
 import boto3
+
 
 AWS_REGION = os.getenv("GDH_AGGREGATE_AWS_REGION", "eu-central-1")
 # Job definition names are of the form PREFIX-<env>
@@ -11,10 +13,10 @@ JOB_QUEUE = "gdh-map-aggregation"
 
 
 def run(sources: list[dict[str, Any]], env: str, dry_run: bool = False):
-    print("*** Running hook: aggregate ***")
+    logging.info("*** Running hook: aggregate ***")
     batch = boto3.client("batch", region_name=AWS_REGION)
     if not sources:
-        print("No sources to run hook for, quitting.")
+        logging.info("No sources to run hook for, quitting.")
         return
     try:
         jobdefs = set(
@@ -23,20 +25,18 @@ def run(sources: list[dict[str, Any]], env: str, dry_run: bool = False):
             if j["jobDefinitionName"].startswith(PREFIX)
         )
     except Exception as e:
-        print("Error occurred when fetching job definitions")
-        print(e)
+        logging.exception("Error occurred when fetching job definitions")
         return
 
     if (jobdef := f"{PREFIX}-{env}") in jobdefs:
-        print(f"Submitting aggregation job: {jobdef} ...")
+        logging.info(f"Submitting aggregation job: {jobdef} ...")
         try:
             if not dry_run:
                 batch.submit_job(
                     jobName=jobdef, jobDefinition=jobdef, jobQueue=JOB_QUEUE
                 )
-                print(f"Successfully submitted job for {jobdef}")
+                logging.info(f"Successfully submitted job for {jobdef}")
         except Exception as e:
-            print(f"Error occurred while trying to submit {jobdef}")
-            print(e)
+            logging.exception(f"Error occurred while trying to submit {jobdef}")
     else:
-        print(f"Could not find job definition: {jobdef}")
+        logging.info(f"Could not find job definition: {jobdef}")
