16aa499af11853b62ec5414a94a701e775b9daab
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-01-10 11:35:12 +0000

    Use the official list of country names, instead of fetching from the DB

--- verification/curator-service/ui/src/components/util/countryNames.tsx
+++ verification/curator-service/ui/src/components/util/countryNames.tsx
@@ -1,4 +1,4 @@
-import countries from 'i18n-iso-countries';
+import countries, { LocalizedCountryNames } from 'i18n-iso-countries';
 import en from 'i18n-iso-countries/langs/en.json';
 
 // register en locale so we don't pack all the others
@@ -6,4 +6,10 @@ countries.registerLocale(en);
 
 export default function nameCountry(isoCode: string): string {
     return countries.getName(isoCode, 'en', { select: 'official' });
+}
+
+export function allCountryNames(): LocalizedCountryNames<{
+    select: 'official';
+}> {
+    return countries.getNames('en', { select: 'official' });
 }
