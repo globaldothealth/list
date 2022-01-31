568c7ea4ebda026a579e306ff8e4d81110b8acf9
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2022-01-31 18:10:55 +0000

    Rework completeness script to support S3 fetch and add automation
    Related: #2297
    
    Format of the new metrics/completeness.json is as follows
    
    {
      "country1": {
        "field1": c1
        "field2": c2
      },
      "country2": {
      ...
      }
    ...
    }
    
    where c1 and c2 are the completeness (as a percentage) of the
    respective fields.

--- ingestion/monitoring/completeness.py
+++ ingestion/monitoring/completeness.py
@@ -1,33 +1,102 @@
-import pickle 
-from collections import defaultdict
+import os
+import sys
+import json
+import tempfile
+import logging
+import contextlib
+from pathlib import Path
+
+import boto3
 import pandas as pd
 
-'''
-This script makes a dictionary containing the completeness of each country within G.h
-Completeness is calculated naively as proportion of non-empty rows for each column in a country df.
-'''
 
-if __name__ == '__main__':
-    # Set raw_data_dir as path to a complete data download from Global.health. 
-    # Alternatively can specify list of countries and pull from the API.
-    raw_data_dir = '/path/to/global_health_all_countries.csv.gz'
-    save_dir = '/path/to/save/completeness_dict'
-    countries = [u.split('.')[0] for u in os.listdir(raw_data_dir)]
+def data_files(bucket, prefix="csv", suffix=".csv.gz", endpoint_url=None):
+    try:
+        bucket = boto3.resource("s3", endpoint_url=endpoint_url).Bucket(bucket)
+    except Exception as e:
+        logging.error(f"Failed to access bucket={bucket} with exception {e}")
+    return [
+        file
+        for file in bucket.objects.all()
+        if file.key.startswith(prefix) and file.key.endswith(suffix)
+    ]
 
-    country_completeness = defaultdict(list)
-    for country in countries:
-        print(f"starting {country}")
-        country_df = pd.read_csv(f'{raw_data_dir}{country}.csv.gz')
-        country_length = country_df.shape[0]
-        for field in country_df.columns:
-            if field in country_df.columns:
-                completeness = 100*country_df.loc[pd.notnull(country_df[field])].shape[0]/country_length
-                country_completeness[field].append(completeness)
-            else:
-                country_completeness[field].append(float(0))
-        del country_df # save space
-        print(f"completed {country}")
 
-    with open(f'{save_dir}Gh_full_country_completeness_dict.pickle', 'wb') as handle:
-        pickle.dump(country_completeness, handle)
+def non_null(df):
+    counts = {"N": df.shape[0]}
+    counts.update(
+        {field: df.loc[pd.notnull(df[field])].shape[0] for field in df.columns}
+    )
+    return pd.Series(counts)
 
+
+def completeness_file(filename, chunksize=100000):
+    it = pd.read_csv(
+        filename, compression="gzip", iterator=True, dtype=str, chunksize=chunksize
+    )
+    _chunks = []
+    with contextlib.suppress(StopIteration):
+        while not (chunk := it.get_chunk()).empty:
+            _chunks.append(non_null(chunk))
+    completeness = pd.DataFrame(_chunks).sum()
+    completeness = dict(100 * completeness / completeness.N)
+    del completeness["N"]  # not needed, always 100
+    return completeness
+
+
+def completeness_s3(s3_object, endpoint_url=None):
+    """Returns completeness of various fields in country
+
+    s3_object should refer to a file in csv.gz format
+    """
+    bucket = boto3.resource("s3", endpoint_url=endpoint_url).Bucket(
+        s3_object.bucket_name
+    )
+    with tempfile.NamedTemporaryFile() as tmp:
+        logging.info(f"Downloading {s3_object.key} from {bucket}")
+        try:
+            bucket.download_file(s3_object.key, tmp.name)
+        except Exception as e:
+            logging.error(f"Failed to download with exception {e}")
+            return None
+        logging.info(f"Calculating completeness for {s3_object.key}")
+        return completeness_file(tmp.name)
+
+
+def upload(data, bucket, endpoint_url=None):
+    logging.info(f"Uploading data to {bucket}")
+    s3 = boto3.client("s3", endpoint_url=endpoint_url)
+    try:
+        s3.put_object(
+            ACL="public-read",
+            Body=json.dumps(data, indent=2, sort_keys=True),
+            Bucket=bucket,
+            Key="metrics/completeness.json",
+        )
+    except Exception as e:
+        logging.error(f"Failed to upload with exception:\n{e}")
+
+
+def completeness_s3_many(objects, endpoint_url=None):
+    return {
+        Path(obj.key).stem.split(".")[0]: completeness_s3(obj, endpoint_url)
+        for obj in objects
+    }
+
+
+def setup_logger():
+    h = logging.StreamHandler(sys.stdout)
+    rootLogger = logging.getLogger()
+    rootLogger.addHandler(h)
+    rootLogger.setLevel(logging.INFO)
+
+
+if __name__ == "__main__":
+    setup_logger()
+    endpoint_url = os.getenv("ENDPOINT_URL")
+    objects = data_files(
+        os.getenv("COUNTRY_EXPORT_BUCKET", "covid-19-country-export"),
+        endpoint_url=endpoint_url,
+    )
+    data = completeness_s3_many(objects, endpoint_url)
+    upload(data, os.getenv("METRICS_BUCKET", "covid-19-aggregates"), endpoint_url)
