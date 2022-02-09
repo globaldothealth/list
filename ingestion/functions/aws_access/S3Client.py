b331370388b9db329ac62e2d0edc7f62452fd7e6
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-02-09 13:06:03 +0000

    Delete old files from S3.

---
+++ ingestion/functions/aws_access/S3Client.py
@@ -0,0 +1,23 @@
+import boto3
+
+class S3Client:
+    def __init__(self):
+        self.s3_adapter = boto3.resource('s3')
+    
+    def objects_in_bucket_for_ingestion_source(self, bucket_name, source_id):
+        """
+        Lists all of the ingestion source objects for a given source in the sources bucket.
+        Relies on the convention that files are named source_id/date/filename.ext
+        Returns an iterator containing all of the matching objects
+        """
+        bucket = self.s3_adapter.Bucket(bucket_name)
+        objects = bucket.objects.filter(Prefix = source_id)
+        return objects
+
+    def delete_object_in_bucket(self, bucket_name, file_name):
+        """
+        Deletes the given object from the bucket.
+        Note that S3 is not hierarchical, so file_name is the "full path" to the object.
+        """
+        self.s3_adapter.Bucket(bucket_name).delete_objects([file_name])
+
