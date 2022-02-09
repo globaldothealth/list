bfb751b4a900849c095573345e7913dc23cd0af7
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-02-09 13:06:03 +0000

    Delete old files from S3.

--- ingestion/functions/aws_access/globaldothealth_configuration.py
+++ ingestion/functions/aws_access/globaldothealth_configuration.py
@@ -1,1 +1,2 @@
 AWS_REGION = "us-east-1"
+INGESTION_SOURCES_BUCKET = "epid-sources-raw"
--- ingestion/functions/clean_old_ingestion_source_files.py
+++ ingestion/functions/clean_old_ingestion_source_files.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from aws_access.EventBridgeClient import EventBridgeClient, ScheduleRule
+from aws_access.S3Client import S3Client
 import aws_access.globaldothealth_configuration as gdoth
 
 def get_schedule_rules():
@@ -8,5 +9,27 @@ def get_schedule_rules():
     event_bridge = EventBridgeClient(gdoth.AWS_REGION)
     return [ScheduleRule(r) for r in event_bridge.get_rule_descriptions()]
 
+
 if __name__ == "__main__":
-    print({r.ingestion_source_id(): r.oldest_file_age(datetime.today()) for r in get_schedule_rules() if r.is_ingestion_rule()})
+    print("Let's do this!")
+    sources_and_ages = {r.ingestion_source_id(): r.oldest_file_age(datetime.today()) for r in get_schedule_rules() if r.is_ingestion_rule()}
+    s3 = S3Client()
+    for source_id, oldest_age in sources_and_ages.items():
+        print(f"Working on {source_id}")
+        earliest_date = datetime.now() + oldest_age
+        print(f"Threshold date is {earliest_date}")
+        for file in s3.objects_in_bucket_for_ingestion_source(gdoth.INGESTION_SOURCES_BUCKET, source_id):
+            file_name = file.key
+            # expected convention: source_id/year/month/day/hhmm/filename.ext
+            components = file_name.split('/')
+            if len(components) != 6:
+                print(f"file name {file_name} does not meet expectations, skipping")
+                continue
+            _, their_year, their_month, their_day, _, _ = components
+            their_date = datetime(int(their_year), int(their_month), int(their_day))
+            if their_date < earliest_date:
+                print(f"deleting {file_name}")
+                s3.delete_object_in_bucket(gdoth.INGESTION_SOURCES_BUCKET, file_name)
+            else:
+                print(f"keeping {file_name}")
+    print("done!")
