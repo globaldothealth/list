99ced900bc3bc62ae76aa459347444342e4891b8
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-02-11 13:18:34 +0000

    Allow environment to set AWS configuration

--- ingestion/functions/aws_access/globaldothealth_configuration.py
+++ ingestion/functions/aws_access/globaldothealth_configuration.py
@@ -1,5 +1,7 @@
-AWS_REGION = "us-east-1"
-INGESTION_SOURCES_BUCKET = "epid-sources-raw"
-DEFAULT_SCHEDULE_EXPRESSION = "rate(1 day)"
+import os
+
+AWS_REGION = os.environ.get("AWS_REGION", "us-east-1")
+INGESTION_SOURCES_BUCKET = os.environ.get("INGESTION_SOURCES_BUCKET", "epid-sources-raw")
+DEFAULT_SCHEDULE_EXPRESSION = os.environ.get("DEFAULT_SCHEDULE_EXPRESSION", "rate(1 day)")
 # Our file retention policy is twice the schedule period plus 10 days
-GRACE_PERIOD_IN_DAYS = 10
+GRACE_PERIOD_IN_DAYS = int(os.environ.get("GRACE_PERIOD_IN_DAYS", "10"))
