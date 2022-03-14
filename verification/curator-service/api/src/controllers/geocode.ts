97d165662683d4529a67821eb9bb2161ad5cb266
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-03-14 11:43:13 +0000

    Look up the distinct country codes used in the database #2559

--- verification/curator-service/api/src/controllers/geocode.ts
+++ verification/curator-service/api/src/controllers/geocode.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from 'express';
 
 import axios, { AxiosError } from 'axios';
+import mongoose from 'mongoose';
+
 import { logger } from '../util/logger';
 
-/** Dumb proxy to geocoder in data service */
+/**
+ * Dumb proxy to geocoder in location service
+ * For the most part, anyway: see countryNames
+ */
 export default class GeocodeProxy {
     constructor(private readonly locationServiceURL: string) {}
 
@@ -39,6 +44,23 @@ export default class GeocodeProxy {
             }
         }
     }
+
+    /**
+     * This is the only "meaty" method on this controller. It does proxy the
+     * location service, but it also uses the database and two npm modules to
+     * find out what ISO-3166-1 country codes are in use in the database and
+     * what names are used for those countries.
+     * @param req Express request
+     * @param res Express response
+     */
+    countryNames = async (req: Request, res: Response): Promise<void> => {
+        const mongoClient = mongoose.connection.getClient();
+        const locationCountryCodes = await mongoClient.db().collection('cases').distinct('location.country');
+        const travelHistoryCodes = await mongoClient.db().collection('cases').distinct('travelHistory.travel.location.country');
+        const allCodes = Array.from(new Set(locationCountryCodes.concat(travelHistoryCodes)).values());
+        res.status(200).json({'codes': allCodes});
+    }
+
     seed = async (req: Request, res: Response): Promise<void> => {
         const response = await axios.post(
             this.locationServiceURL + req.url,
