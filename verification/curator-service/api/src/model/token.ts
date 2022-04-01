5e53024e0cd4a52d3b83265aa5030c97e13e7216
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-04-01 10:18:01 +0100

    Completely remove mongoose Token document #2559

--- verification/curator-service/api/src/model/token.ts
+++ verification/curator-service/api/src/model/token.ts
@@ -1,5 +1,4 @@
 import { ObjectId } from 'mongodb';
-import { Schema, Document, model } from 'mongoose';
 import db from './database';
 
 export interface IToken {
@@ -9,24 +8,4 @@ export interface IToken {
     createAt: Date;
 }
 
-export type TokenDocument = Document & IToken;
-
-const tokenSchema = new Schema<TokenDocument>({
-    userId: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: 'user',
-    },
-    token: {
-        type: String,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        expires: 3600, //expiry time in seconds
-    },
-});
-
-export const Token = model<TokenDocument>('Token', tokenSchema);
 export const tokens = () => db().collection('tokens');
