0b917a35def412e559b1f1d8b90a474631573791
Maciej Zarzeczny <maciej.zarzeczny@htdevelopers.com>
2022-04-07 13:51:21 +0200

    Fixed bug

--- verification/curator-service/api/src/controllers/auth.ts
+++ verification/curator-service/api/src/controllers/auth.ts
@@ -632,7 +632,7 @@ export class AuthController {
         // @ts-ignore
         passport.serializeUser((user: IUser, done: any) => {
             // Serializes the user id in the cookie, no user info should be in there, just the id.
-            done(null, user.id);
+            done(null, user._id);
         });
 
         passport.deserializeUser((id: string, done: any) => {
@@ -708,7 +708,7 @@ export class AuthController {
                             <p>The G.h Team</p>`,
                         );
 
-                        done(null, userPublicFields(newUser));
+                        done(null, newUser);
                     } catch (error) {
                         done(error);
                     }
@@ -742,7 +742,7 @@ export class AuthController {
                             });
                         }
 
-                        done(null, userPublicFields(user));
+                        done(null, user);
                     } catch (error) {
                         done(error);
                     }
