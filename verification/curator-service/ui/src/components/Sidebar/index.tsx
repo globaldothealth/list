4c739ef133a0473d33f12f43e5f1bf633c38523c
Maciej Zarzeczny <maciej.zarzeczny@htdevelopers.com>
2022-04-08 13:07:17 +0200

    Removed unused import

--- verification/curator-service/ui/src/components/Sidebar/index.tsx
+++ verification/curator-service/ui/src/components/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo, useCallback } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useAppSelector } from '../../hooks/redux';
 import { selectUser } from '../../redux/auth/selectors';
