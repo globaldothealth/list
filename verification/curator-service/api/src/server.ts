0de34a746e93d6a11d753dce16fa33adf59cc149
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-04-13 16:27:19 +0100

    Server start is now async #2559

--- verification/curator-service/api/src/server.ts
+++ verification/curator-service/api/src/server.ts
@@ -1,15 +1,22 @@
-import app from './index';
+import makeApp from './index';
 import { logger } from './util/logger';
 
 // Start Express server.
-const server = app.listen(app.get('port'), () => {
-    logger.info(`Curator service listening on port ${app.get('port')}`);
-    logger.info('  Press CTRL-C to stop\n');
-});
+let server;
 
-// Set global 1-hour timeout.
-// TODO: Make this more fine-grained once we fix
-//   https://github.com/globaldothealth/list/issues/961.
-server.setTimeout(60 * 60 * 1000);
+makeApp().then(app => {
+    server = app.listen(app.get('port'), () => {
+        logger.info(`Curator service listening on port ${app.get('port')}`);
+        logger.info('  Press CTRL-C to stop\n');
+    });
+    // Set global 1-hour timeout.
+    // TODO: Make this more fine-grained once we fix
+    //   https://github.com/globaldothealth/list/issues/961.
+    server.setTimeout(60 * 60 * 1000);
+})
+.catch(e => {
+    logger.error('Could not start server!', e);
+    process.exit(1);
+});
 
 export default server;
