478078dd31c72d16037aad1d249481f3f34e5cdc
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-04-29 12:51:08 +0100

    Import script can create a user if no API key given #2670

--- data-serving/scripts/setup-db/import-sample-data.py
+++ data-serving/scripts/setup-db/import-sample-data.py
@@ -5,6 +5,8 @@ import json
 import os
 import requests
 import sys
+import time
+
 
 def convert_dict_to_float(aDict):
     return float(aDict['$numberDouble'])
@@ -54,17 +56,37 @@ def convert_case(case):
         
     return converted
 
+
+def api_key_for_generated_curator(base_url: str) -> str:
+    """Create a user and retrieve their API key."""
+    register_user_endpoint = f'{base_url}/auth/register'
+    user = {
+        'name': 'G.h test data importer',
+        'email': f'robot_{time.time()}@global.health',
+        'roles': ['curator'],
+    }
+    response = requests.post(register_user_endpoint, json=user)
+    if response.ok:
+        return response.json()['apiKey']
+    else:
+        print(f'Failure registering test user: {response.text}')
+        sys.exit(1)
+
+
 def main():
-    """Import sample data from ../../samples/cases.json into the G.h instance at $GH_BASE_URL
-    using the curator API key at $GH_API_KEY. This is done through the curator-service
-    rather than loading directly into the database via mongoimport because the links to
-    other collections, particularly the age buckets, need setting up in the application."""
+    """Import sample data from ../../samples/cases.json into a G.h instance at $GH_BASE_URL
+    (by default, the local-testing instance on localhost) using the curator API.
+    This is done through the curator-service rather than loading directly into the database
+    via mongoimport because the links to other collections, particularly the age buckets, need
+    setting up in the application.
+    
+    You can set an API key using the environment variable $GH_API_KEY. If you do not, then
+    this script will register a new curator user and use their API key; this only works in
+    local testing."""
     if not (base_url := os.getenv('GH_BASE_URL')):
-        print("Define the Global.health API location in $GH_BASE_URL.")
-        sys.exit(1)
+        base_url = 'http://localhost:3001'
     if not (api_key := os.getenv('GH_API_KEY')):
-        print("Supply a Global.health API key in $GH_API_KEY.")
-        sys.exit(1)
+        api_key = api_key_for_generated_curator(base_url)
     batch_upsert_endpoint = f"{base_url}/api/cases/batchUpsert"
     with open("../../samples/cases.json") as f:
         sample_cases = json.load(f)
