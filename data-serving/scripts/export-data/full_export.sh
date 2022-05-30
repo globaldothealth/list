3131166357e2ed001956aee6e8b0aac9a53384d2
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-05-30 08:21:10 +0100

    Add country codes maps to the data dictionaries #2559

--- data-serving/scripts/export-data/full_export.sh
+++ data-serving/scripts/export-data/full_export.sh
@@ -6,6 +6,8 @@ set -xeuo pipefail
 source ./common.sh
 require_env "${COUNTRY_EXPORT_BUCKET:-}" "Specify COUNTRY_EXPORT_BUCKET"
 require_env "${FULL_EXPORT_BUCKET:-}" "Specify FULL_EXPORT_BUCKET"
+require_env "${GDOTH_API_BASEURL:-}" "Specify GDOTH_API_BASEURL"
+require_env "${GDOTH_API_KEY:-}" "Specify GDOTH_API_KEY"
 
 # https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-retries.html#cli-usage-retries-configure
 export AWS_MAX_ATTEMPTS=10
@@ -25,6 +27,9 @@ echo "Made temporary directory $DIR"
 trap 'rm -rf "$DIR"' EXIT  # Cleanup before exit
 
 cp data_dictionary.txt citation.txt "$DIR"
+
+echo "Getting country list from ${GDOTH_API_BASEURL}..."
+curl -H "X-API-Key: ${GDOTH_API_KEY}" "${GDOTH_API_BASEURL}/api/geocode/countryNames" > "${DIR}/countries.json"
 
 mkdir "${DIR}/country"
 echo "Downloading files from ${COUNTRY_EXPORT_BUCKET}..."
