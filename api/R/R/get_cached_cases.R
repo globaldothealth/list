fcc8d831b9ddf8aa384c615cd7a33b48dfdbe02b
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-06-13 16:22:57 +0100

    Correct the R documentation #2712

--- api/R/R/get_cached_cases.R
+++ api/R/R/get_cached_cases.R
@@ -1,5 +1,5 @@
 get_cached_cases <-
-function(apikey, disease = DISEASE, environment = ENVIRONMENT, refresh = FALSE, folder = "cache", ...) {
+function(apikey, disease = 'covid-19', environment = 'production', refresh = FALSE, folder = "cache", ...) {
     if(!file.exists(folder)) {
         dir.create(folder)
     }
