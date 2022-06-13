8ee6c90ef1b7886686eeda2f07fff299b3c3380a
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-06-13 16:22:57 +0100

    Correct the R documentation #2712

--- api/R/R/constants.R
+++ api/R/R/constants.R
@@ -1,4 +1,2 @@
 FILTERS <- c("country")
-DISEASE <- "covid-19"
-ENVIRONMENT <- 'production'
 downloadAsync <- "/api/cases/downloadAsync"
--- api/R/R/get_cases.R
+++ api/R/R/get_cases.R
@@ -1,5 +1,5 @@
 get_cases <-
-function(apikey, disease = DISEASE, environment = ENVIRONMENT, ...) {
+function(apikey, disease = 'covid-19', environment = 'production', ...) {
     server <- get_server(disease, environment)
     data <- sprintf('{"format": "csv", "query": "%s"}', trimws(stringify_filters(...)))
     res <- POST(paste0(server, downloadAsync), body = data,
