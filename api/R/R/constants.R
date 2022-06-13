03644eb812b10faa9f19bc54436b35fd44424476
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-06-13 15:55:04 +0100

    Consistent server name resolution #2712
    If you set the env GDH_URL, we use that
    
    (R users can set this in .Renvironment, apparently)
    
    Otherwise build from the disease/environment you specify.

--- api/R/R/constants.R
+++ api/R/R/constants.R
@@ -1,3 +1,4 @@
 FILTERS <- c("country")
 DISEASE <- "covid-19"
+ENVIRONMENT <- 'production'
 downloadAsync <- "/api/cases/downloadAsync"
--- api/R/R/get_cached_cases.R
+++ api/R/R/get_cached_cases.R
@@ -1,9 +1,9 @@
 get_cached_cases <-
-function(apikey, disease = DISEASE, refresh = FALSE, folder = "cache", ...) {
+function(apikey, disease = DISEASE, environment = ENVIRONMENT, refresh = FALSE, folder = "cache", ...) {
     if(!file.exists(folder)) {
         dir.create(folder)
     }
-    server <- get_server(disease)
+    server <- get_server(disease, environment)
     cachename <- cases_cachefile(server, folder, ...)
     if(!refresh && file.exists(cachename)) {
         return(read_csv(cachename))
