881a634e8a2299457be062975f1b82c8892a8fdc
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-06-13 16:07:47 +0100

    Fix R syntax error #2712

--- api/R/R/get_server.R
+++ api/R/R/get_server.R
@@ -2,7 +2,7 @@ get_server <-
 function(disease = DISEASE, environment = ENVIRONMENT) {
     server_from_environment <- Sys.getenv('GDH_URL')
     if (server_from_environment != "") {
-        return server_from_environment
+        return(server_from_environment)
     }
     if (environment == 'production') {
         return(sprintf('https://data.%s.global.health', disease))
