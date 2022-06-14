eaeffd929d1e6be899b07cc09c34fc3b5af1595e
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-06-14 11:51:39 +0100

    Allow the setup_db script to run against different (local-only) databases #2712

--- dev/setup_db.sh
+++ dev/setup_db.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export CONN="mongodb://localhost/covid19"
-export DB="covid19"
+export DB="${DB:-covid19}"
 
 npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script migrate
 npm --prefix=`dirname "$0"`/../data-serving/scripts/setup-db/ run-script delete-all-cases
