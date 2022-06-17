5ff064467de1724e9da01749662744711c484349
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-06-17 13:12:29 +0100

    Require confirmation date to be set in the data model #2714

--- data-serving/reusable-data-service/reusable_data_service/model/case.py
+++ data-serving/reusable-data-service/reusable_data_service/model/case.py
@@ -1,11 +1,37 @@
 import dataclasses
+import datetime
+import json
 
-@dataclasses.dataclass
+@dataclasses.dataclass()
 class DayZeroCase:
     """This class implements the "day-zero" data schema for Global.health.
     At the beginning of an outbreak, we want to collect at least this much
-    information about an individual case for the line list."""
-    pass
+    information about an individual case for the line list.
+    
+    Parameters here are defined to be keyword-only and not set in the
+    initialiser, so that clients can use Builder to populate them. Use
+    the validate() method to determine whether an instance is in a
+    consistent state (this also means we can add custom validation logic
+    to that function)."""
+    _: dataclasses.KW_ONLY
+    confirmation_date: datetime.datetime = dataclasses.field(init=False)
+
+    @classmethod
+    def from_json(cls, obj: str) -> type:
+        """Create an instance of this class from a JSON representation."""
+        case = cls()
+        source = json.loads(obj)
+        for key in source:
+            setattr(case, key, source[key])
+        case.validate()
+        return case
+
+    def validate(self):
+        """Check whether I am consistent. Raise ValueError if not."""
+        if not hasattr(self, 'confirmation_date'):
+            raise ValueError("Confirmation Date is mandatory")
+        elif self.confirmation_date is None:
+            raise ValueError("Confirmation Date must have a value")
 
 # Actually we want to capture extra fields which can be specified dynamically:
 # so Case is the class that you should use.
