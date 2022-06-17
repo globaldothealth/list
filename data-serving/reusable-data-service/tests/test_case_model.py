c9ef37d8ce42fb700da5c1e2816713ca5da1e74b
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-06-17 13:12:29 +0100

    Require confirmation date to be set in the data model #2714

--- data-serving/reusable-data-service/tests/test_case_model.py
+++ data-serving/reusable-data-service/tests/test_case_model.py
@@ -1,5 +1,6 @@
+import pytest
 from reusable_data_service import Case
 
-def test_instantiating_case():
-    case = Case()
-    assert case is not None
+def test_instantiating_case_from_empty_json_is_error():
+    with pytest.raises(ValueError):
+        case = Case.from_json('{}')
