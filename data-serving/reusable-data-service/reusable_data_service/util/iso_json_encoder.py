1009920cbd4b7d281dc0a30d0994de397832f803
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-06-20 16:13:03 +0100

    Use ISO8601 format for dates #2714

---
+++ data-serving/reusable-data-service/reusable_data_service/util/iso_json_encoder.py
@@ -0,0 +1,14 @@
+import flask.json
+import datetime
+
+class ISOJSONEncoder(flask.json.JSONEncoder):
+    def default(self, obj):
+        try:
+            if isinstance(obj, datetime.date):
+                return obj.isoformat()
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return flask.json.JSONEncoder.default(self, obj)
