0a137c405e072c909930dd71915f8260827e508e
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-06-21 11:35:46 +0100

    Refactor: give tests access to controller store, put in fixture #2714

--- data-serving/reusable-data-service/tests/test_case_controller.py
+++ data-serving/reusable-data-service/tests/test_case_controller.py
@@ -20,45 +20,39 @@ class MemoryStore:
 
 
 @pytest.fixture
-def app_context():
+def case_controller():
     with app.app_context():
-        yield
+        store = MemoryStore()
+        controller = CaseController(store)
+        yield controller
 
 
-def test_one_present_item_should_return_200_OK(app_context):
-    store = MemoryStore()
+def test_one_present_item_should_return_200_OK(case_controller):
     with open("./tests/data/case.minimal.json", "r") as minimal_file:
         case = Case.from_json(minimal_file.read())
-        store.put_case("foo", case)
-    controller = CaseController(store)
-    (response, status) = controller.get_case("foo")
+        case_controller.store.put_case("foo", case)
+    (response, status) = case_controller.get_case("foo")
     assert status == 200
     assert response is not None
 
 
-def test_one_absent_item_should_return_400_not_found(app_context):
-    store = MemoryStore()
-    controller = CaseController(store)
-    (response, status) = controller.get_case("foo")
+def test_one_absent_item_should_return_400_not_found(case_controller):
+    (response, status) = case_controller.get_case("foo")
     assert status == 404
     assert response == "No case with ID foo"
 
 
-def test_list_cases_should_return_200_OK(app_context):
-    store = MemoryStore()
-    controller = CaseController(store)
-    (response, status) = controller.list_cases()
+def test_list_cases_should_return_200_OK(case_controller):
+    (response, status) = case_controller.list_cases()
     assert status == 200
     assert response.json == []
 
 
-def test_list_cases_should_list_the_cases(app_context):
-    store = MemoryStore()
-    controller = CaseController(store)
+def test_list_cases_should_list_the_cases(case_controller):
     with open("./tests/data/case.minimal.json", "r") as minimal_file:
         case = Case.from_json(minimal_file.read())
-        store.put_case("foo", case)
-        store.put_case("bar", case)
-    (response, status) = controller.list_cases()
+        case_controller.store.put_case("foo", case)
+        case_controller.store.put_case("bar", case)
+    (response, status) = case_controller.list_cases()
     assert status == 200
     assert len(response.json) == 2
