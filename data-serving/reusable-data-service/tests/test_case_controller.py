08c245190066fd621737112b773864f5d4b6ae1a
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-06-21 13:40:50 +0100

    Extra unit tests for pagination #2714

--- data-serving/reusable-data-service/tests/test_case_controller.py
+++ data-serving/reusable-data-service/tests/test_case_controller.py
@@ -71,3 +71,24 @@ def test_list_cases_should_paginate(case_controller):
     assert response.json["nextPage"] == 2
     assert response.json["total"] == 15
 
+def test_list_cases_last_page(case_controller):
+    with open("./tests/data/case.minimal.json", "r") as minimal_file:
+        case = Case.from_json(minimal_file.read())
+        for i in range(15):
+            case_controller.store.put_case(f"case_{i}", case)
+    (response, status) = case_controller.list_cases(page=2, limit=10)
+    assert status == 200
+    assert len(response.json["cases"]) == 5
+    assert response.json["total"] == 15
+    assert "nextPage" not in response.json
+
+def test_list_cases_nonexistent_page(case_controller):
+    with open("./tests/data/case.minimal.json", "r") as minimal_file:
+        case = Case.from_json(minimal_file.read())
+        for i in range(15):
+            case_controller.store.put_case(f"case_{i}", case)
+    (response, status) = case_controller.list_cases(page=43, limit=10)
+    assert status == 200
+    assert len(response.json["cases"]) == 0
+    assert response.json["total"] == 15
+    assert "nextPage" not in response.json
