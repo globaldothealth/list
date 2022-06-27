5b9e84ba58c5518cc3ee21614bae2dc9e4f8e975
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-06-27 13:23:45 +0100

    Ensure that a populated case reference gets upserted #2714

--- data-serving/reusable-data-service/tests/test_mongo_store.py
+++ data-serving/reusable-data-service/tests/test_mongo_store.py
@@ -1,6 +1,8 @@
 import pytest
 import mongomock
 
+from bson import ObjectId
+
 from reusable_data_service.model.case import Case
 from reusable_data_service.model.case_reference import CaseReference
 from reusable_data_service.model.filter import Anything
@@ -29,6 +31,18 @@ def test_store_inserts_case_with_empty_case_reference(mongo_store):
     with open("./tests/data/case.minimal.json", "r") as minimal_file:
         case = Case.from_json(minimal_file.read())
     case.caseReference = CaseReference()
+    (created, updated) = mongo_store.batch_upsert([case])
+    assert created == 1
+    assert updated == 0
+    assert mongo_store.count_cases(Anything()) == 1
+
+def test_store_inserts_case_with_populated_case_reference(mongo_store):
+    with open("./tests/data/case.minimal.json", "r") as minimal_file:
+        case = Case.from_json(minimal_file.read())
+    caseReference = CaseReference()
+    caseReference.sourceId = ObjectId()
+    caseReference.sourceEntryId = "1234"
+    case.caseReference = caseReference
     (created, updated) = mongo_store.batch_upsert([case])
     assert created == 1
     assert updated == 0
