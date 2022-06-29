4b850efb02a59319db2dee1af512e92ae4705d24
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-06-29 09:58:53 +0100

    Remove source entry ID #2714
    This obviates the problem with trying to keep hashed source entry IDs stable.
    
    See discussion in https://github.com/globaldothealth/list/pull/2739

--- data-serving/reusable-data-service/reusable_data_service/stores/mongo_store.py
+++ data-serving/reusable-data-service/reusable_data_service/stores/mongo_store.py
@@ -55,17 +55,10 @@ class MongoStore:
         self.get_case_collection().insert_one(to_insert)
 
     def batch_upsert(self, cases: List[Case]) -> Tuple[int, int]:
-        dicts = [MongoStore.case_to_bson_compatible_dict(c) for c in cases]
-        needs_insert = lambda d: d['caseReference'] is None or d['caseReference']['sourceId'] is None or d['caseReference']['sourceEntryId'] is None
-        to_insert = [d for d in dicts if needs_insert(d)]
-        to_upsert = [d for d in dicts if not needs_insert(d)]
+        to_insert = [MongoStore.case_to_bson_compatible_dict(c) for c in cases]
         inserts = [pymongo.InsertOne(d) for d in to_insert]
-        replacements = [pymongo.ReplaceOne(filter = {
-            'caseReference.sourceId': d['caseReference']['sourceId'],
-            'caseReference.sourceEntryId': d['caseReference']['sourceEntryId'],
-            }, replacement = d, upsert = True) for d in to_upsert]
-        results = self.get_case_collection().bulk_write(inserts + replacements)
-        return results.inserted_count, results.modified_count
+        results = self.get_case_collection().bulk_write(inserts)
+        return results.inserted_count, 0
 
     @staticmethod
     def setup():
