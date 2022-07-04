81b03ffe1e63336498e4447235167beaf0a56c36
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-07-04 09:53:21 +0100

    Redesign: introduce model object for representing upsert results #2714

---
+++ data-serving/reusable-data-service/reusable_data_service/model/case_upsert_outcome.py
@@ -0,0 +1,13 @@
+import dataclasses
+from typing import Dict
+
+from reusable_data_service.model.case import Case
+
+
+@dataclasses.dataclass
+class CaseUpsertOutcome:
+    """Represents the result of a batch upsert operation: the number
+    of cases created, the number updated, and the errors encountered."""
+    numCreated: int
+    numUpdated: int
+    errors: Dict[str, str]
