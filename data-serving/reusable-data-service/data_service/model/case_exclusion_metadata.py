21c5a48f4ed93d9bf5f392a0208ec4ae49c6312b
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-07-06 15:33:07 +0100

    Failing E2E test for batchStatusChange #2714

--- data-serving/reusable-data-service/data_service/model/case_exclusion_metadata.py
+++ data-serving/reusable-data-service/data_service/model/case_exclusion_metadata.py
@@ -1,6 +1,8 @@
 import dataclasses
 import datetime
 
+from typing import Any
+
 
 @dataclasses.dataclass
 class CaseExclusionMetadata:
@@ -18,3 +20,35 @@ class CaseExclusionMetadata:
     @classmethod
     def exclude_from_download(cls):
         return True
+
+    @classmethod
+    def date_fields(cls) -> list[str]:
+        """Record where dates are kept because they sometimes need special treatment."""
+        return [f.name for f in dataclasses.fields(cls) if f.type == datetime.date]
+
+    @classmethod
+    def from_dict(cls, dictionary: dict[str, Any]) -> type:
+        """Create a CaseExclusionMetadata from a dictionary representation."""
+        exclusion = CaseExclusionMetadata()
+        exclusion.note = dictionary.get('note')
+        # TODO move this and the Case implementation into a common place
+        maybe_date = dictionary.get('date')
+        value = None
+        if maybe_date is None:
+            raise ValueError(f"date missing in CaseExclusionMetadata dictionary {dictionary}")
+        if isinstance(maybe_date, datetime.datetime):
+            value = maybe_date.date()
+        elif isinstance(maybe_date, datetime.date):
+            value = maybe_date
+        elif isinstance(maybe_date, str):
+            value = datetime.datetime.strptime(
+                maybe_date, "%Y-%m-%dT%H:%M:%S.%fZ"
+            ).date()
+        elif isinstance(maybe_date, dict) and "$date" in maybe_date:
+            value = datetime.datetime.strptime(
+                maybe_date["$date"], "%Y-%m-%dT%H:%M:%SZ"
+            ).date()
+        else:
+            raise ValueError(f"Cannot interpret date {maybe_date}")
+        exclusion.date = value
+        return exclusion
