f84fcf3c28c498cab76ea8110bfae9189c78b5b3
Oskar Kocjan <oskarkocjan@Oskar-Kocjan.local>
2022-07-12 15:02:39 +0200

    case insensitive searching for user email when sign in or up

--- data-serving/scripts/setup-db/migrations/20211119121032-additional-user-indexes.js
+++ data-serving/scripts/setup-db/migrations/20211119121032-additional-user-indexes.js
@@ -1,24 +1,28 @@
 const indexes = [
-  {
-    name: 'googleID',
-    key: {
-      googleID: 1,
+    {
+        name: 'googleID',
+        key: {
+            googleID: 1,
+        },
+        collation: {
+            locale: 'en_US',
+            strength: 2,
+        },
     },
-  },
-]
+];
 
 module.exports = {
-  async up(db, client) {
-    await db.command({
-      createIndexes: 'users',
-      indexes: indexes,
-    });
-  },
+    async up(db, client) {
+        await db.command({
+            createIndexes: 'users',
+            indexes: indexes,
+        });
+    },
 
-  async down(db, client) {
-    await db.command({
-      dropIndexes: 'users',
-      index: ['googleID'],
-    });
-  }
+    async down(db, client) {
+        await db.command({
+            dropIndexes: 'users',
+            index: ['googleID'],
+        });
+    },
 };
