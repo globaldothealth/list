d6dc08a5886690731a64b3d7020f1ac17d175a29
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-07-19 11:13:20 +0100

    Fix CSV encoding when there are commas in custom fields #2714

--- data-serving/reusable-data-service/tests/test_case_schema_integration.py
+++ data-serving/reusable-data-service/tests/test_case_schema_integration.py
@@ -1,3 +1,6 @@
+import csv
+import io
+
 from tests.end_to_end_fixture import client_with_patched_mongo
 
 def test_adding_field_then_downloading_case(client_with_patched_mongo):
@@ -30,3 +33,38 @@ def test_adding_field_then_downloading_case(client_with_patched_mongo):
     assert case_list["total"] == 1
     assert len(case_list["cases"]) == 1
     assert case_list["cases"][0]["mySymptoms"] == "coughs, sneezles"
+
+def test_adding_field_then_downloading_csv(client_with_patched_mongo):
+    response = client_with_patched_mongo.post(
+        "/api/schema",
+        json={
+            "name": "someField",
+            "type": "string",
+            "description": "Another custom field",
+        },
+    )
+    assert response.status_code == 201
+    response = client_with_patched_mongo.post(
+        "/api/cases",
+        json={
+            "confirmationDate": "2022-06-01T00:00:00.000Z",
+            "caseReference": {
+                "status": "UNVERIFIED",
+                "sourceId": "24680135792468013579fedc"
+            },
+            "someField": "well, what have we here"
+        },
+    )
+    assert response.status_code == 201
+    response = client_with_patched_mongo.post(
+        "/api/cases/download", json = {
+            "format": "csv"
+        }
+    )
+    assert response.status_code == 200
+    csv_file = io.StringIO(response.get_data().decode("utf-8"))
+    csv_reader = csv.DictReader(csv_file)
+    cases = [c for c in csv_reader]
+    assert len(cases) == 1
+    case = cases[0]
+    assert case['someField'] == 'well, what have we here'
