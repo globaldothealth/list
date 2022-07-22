b5726d64a713f4ee7309de8a27e9e353538ae324
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-07-22 12:39:15 +0100

    Data classes for geojson features and points #2714

---
+++ data-serving/reusable-data-service/data_service/model/geojson.py
@@ -0,0 +1,52 @@
+import dataclasses
+
+from typing import Any, Dict, List
+
+from data_service.model.document import Document
+from data_service.util.errors import ValidationError
+
+
+@dataclasses.dataclass
+class Point(Document):
+    """Represents a GeoJSON point, but because we only deal with lat/long
+    coordinates it has the additional validation constraint that it must be
+    two-dimensional and be in the range ((-90<=lat<=90), (-180<=lon<=180))."""
+    _: dataclasses.KW_ONLY
+    type: str = dataclasses.field(init=False, default="Point")
+    coordinates: List[float] = dataclasses.field(init=False, default_factory= lambda: [])
+
+    def validate(self):
+        if self.type != "Point":
+            raise ValidationError(f"Type must be Point for a Point, not {self.type}")
+        if self.coordinates is None:
+            raise ValidationError("Point must have coordinates")
+        if len(self.coordinates) != 2:
+            raise ValidationError(f"Point must have two coordinates, I have {len(self.coordinates)}")
+        latitude = self.coordinates[0]
+        if latitude < -90.0 or latitude > 90.0:
+            raise ValidationError(f"latitude must be between -90º and 90º, got {latitude}")
+        longitude = self.coordinates[1]
+        if longitude < -180.0 or longitude > 180.0:
+            raise ValidationError(f"longitude must be between -180º and 180º, got {longitude}")
+
+@dataclasses.dataclass
+class Feature(Document):
+    """Represents a GeoJSON feature, but with restrictions that are appropriate
+    to Global.health. To whit: the geometry _must_ be a point (it cannot be a MultiPoint,
+    a Line, or any other type of geometry, and nor can it be None);
+    the properties dictionmary _must_ include a three-letter
+    country code. These constraints are tested at validation time, not construction time."""
+    _: dataclasses.KW_ONLY
+    type: str = dataclasses.field(init=False, default="Feature")
+    geometry: Point = dataclasses.field(init=False, default=None)
+    properties: Dict[str, Any] = dataclasses.field(init=False, default_factory = lambda: {})
+
+    def validate(self):
+        if self.type != "Feature":
+            raise ValidationError(f"Type must be Feature for a Feature, not {self.type}")
+        if not isinstance(self.geometry, Point):
+            raise ValidationError(f"geometry of a Feature must be a Point in G.h, {self.geometry} is not one")
+        self.geometry.validate()
+        country = self.properties.get('country', 'None')
+        if len(country) != 3:
+            raise ValidationError(f"country must be defined as an ISO 3166-1 alpha-3 code, not {country}")
