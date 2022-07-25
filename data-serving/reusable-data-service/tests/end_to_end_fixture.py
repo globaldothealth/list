36beb6bac063ea75954053f0298b9ee0592cf95b
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-07-25 15:07:23 +0100

    Integrate geocoding controller in main #2714

--- data-serving/reusable-data-service/tests/end_to_end_fixture.py
+++ data-serving/reusable-data-service/tests/end_to_end_fixture.py
@@ -18,6 +18,7 @@ def client_with_patched_mongo(monkeypatch):
     monkeypatch.setenv("MONGO_CASE_COLLECTION", "cases")
     monkeypatch.setenv("MONGO_SCHEMA_COLLECTION", "schema")
     monkeypatch.setenv("OUTBREAK_DATE", "2019-11-01")
+    monkeypatch.setenv("LOCATION_SERVICE", "https://geocoder.example")
     db = mongomock.MongoClient()
 
     def fake_mongo(connection_string):
