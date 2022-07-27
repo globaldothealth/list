564f7d3840c271c0b62f474aedb08f363b68b2b4
Graham Lee <graham.lee@cs.ox.ac.uk>
2022-07-27 10:18:12 +0100

    Remove redundant test #2714

--- data-serving/reusable-data-service/tests/test_case_reference.py
+++ data-serving/reusable-data-service/tests/test_case_reference.py
@@ -4,16 +4,7 @@ import pytest
 from data_service.model.case_reference import CaseReference
 
 
-def test_csv_row_unexcluded():
-    identifier = "abcd12903478565647382910"
-    oid = bson.ObjectId(identifier)
-    ref = CaseReference()
-    ref.sourceId = oid
-    csv = ref.to_csv()
-    assert csv == "abcd12903478565647382910\r\n"
-
-
-def test_csv_row_excluded():
+def test_csv_row():
     identifier = "abcd12903478565647382910"
     oid = bson.ObjectId(identifier)
     ref = CaseReference()
