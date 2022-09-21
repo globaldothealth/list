f9d2bb6a74d732786888441cd1c6b2ab924aa890
Oskar Kocjan <oskarkocjan@Oskar-Kocjan.local>
2022-09-21 07:47:00 +0200

    inceased number of attempts for requests

--- verification/curator-service/api/src/util/single-window-rate-limiters.ts
+++ verification/curator-service/api/src/util/single-window-rate-limiters.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const loginLimiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 60 minutes
-    max: 4, // Limit each IP to 4 requests per `window` (here, per 20 minutes)
+    max: 6, // Limit each IP to 4 requests per `window` (here, per 20 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     handler: function (req, res /*next*/) {
@@ -15,7 +15,7 @@ export const loginLimiter = rateLimit({
 
 export const registerLimiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 60 minutes
-    max: 4,
+    max: 6,
     standardHeaders: true,
     legacyHeaders: false,
     handler: function (req, res) {
@@ -29,7 +29,7 @@ export const registerLimiter = rateLimit({
 
 export const resetPasswordLimiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 60 minutes
-    max: 4,
+    max: 6,
     standardHeaders: true,
     legacyHeaders: false,
     handler: function (req, res) {
@@ -57,7 +57,7 @@ export const forgotPasswordLimiter = rateLimit({
 
 export const resetPasswordWithTokenLimiter = rateLimit({
     windowMs: 60 * 60 * 1000, // 60 minutes
-    max: 4,
+    max: 6,
     standardHeaders: true,
     legacyHeaders: false,
     handler: function (req, res) {
