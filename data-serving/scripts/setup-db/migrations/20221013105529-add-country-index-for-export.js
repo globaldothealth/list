4e0f0837e159326c2eab4cb43c1bbcd470cd69b6
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2022-10-13 11:01:43 +0100

    migrations: add country if listed index for export
    Add index used by mongoexport in the export-data scripts

---
+++ data-serving/scripts/setup-db/migrations/20221013105529-add-country-index-for-export.js
@@ -0,0 +1,26 @@
+// index used by mongoexport in export-data scripts
+
+const indexes = [
+  {
+      name: 'byCountryIfListed',
+      key: {
+          'location.country': 1
+          list: 1,          
+      },
+  },
+];
+module.exports = {
+  async up(db, client) {
+      await db.command({
+          createIndexes: 'cases',
+          indexes: indexes,
+      });
+  },
+
+  async down(db, client) {
+      await db.command({
+          dropIndexes: 'cases',
+          index: ['byCountryIfListed'],
+      });
+  },
+};
