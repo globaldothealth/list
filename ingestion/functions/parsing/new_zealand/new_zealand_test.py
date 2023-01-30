23f6b2cfbac0505687ab839d7c123844fb4956e0
jsbrittain <98161205+jsbrittain@users.noreply.github.com>
2023-01-30 15:48:45 +0000

    NZ ingestion parser updated to match governmental data availability (#2962)
    * NZ governmental dataset changed url and format; parser updated to match.
    
    * Updated flake8 config
    
    * Updated NZ test data to match current available data

--- ingestion/functions/parsing/new_zealand/new_zealand_test.py
+++ ingestion/functions/parsing/new_zealand/new_zealand_test.py
@@ -1,8 +1,7 @@
 import os
 import unittest
 from new_zealand import new_zealand
-# import new_zealand
-from pprint import pprint
+# from pprint import pprint
 
 _SOURCE_ID = "placeholder_ID"
 _SOURCE_URL = "placeholder_URL"
@@ -10,8 +9,8 @@ _SOURCE_URL = "placeholder_URL"
 _caseReference = {"sourceId": "placeholder_ID", "sourceUrl": "placeholder_URL"}
 _NZ = {
     "country": "New Zealand",
-    "name": "New Zealand",
     "geoResolution": "Country",
+    "name": "New Zealand",
     "geometry": {
         "latitude": -40.900557,
         "longitude": 174.885971,
@@ -20,9 +19,9 @@ _NZ = {
 
 _Bay_of_Plenty = {
     "country": "New Zealand",
+    "geoResolution": "Admin1",
     "name": "Bay of Plenty",
     "administrativeAreaLevel1": "Bay of Plenty",
-    "geoResolution": "Admin1",
     "geometry": {
         "latitude": -37.9519223,
         "longitude": 176.9945977,
@@ -31,9 +30,9 @@ _Bay_of_Plenty = {
 
 _West_Coast = {
     "country": "New Zealand",
+    "geoResolution": "Admin1",
     "name": "West Coast",
     "administrativeAreaLevel1": "West Coast",
-    "geoResolution": "Admin1",
     "geometry": {
         "latitude": -42.87387,
         "longitude": 171.15336,
@@ -42,11 +41,12 @@ _West_Coast = {
 
 _notes = (
     "Case imported from abroad. "
-    "Case identified at border and placed into managed quarantine."
+    "Case identified at border."
 )
 
 
-def _c(location, gender, age_start, age_end, confirmed_date, travel=False, notes=None):
+def _c(location, gender, age_start, age_end, confirmed_date,
+       travel=False, notes=None):
     demographics = {}
     if age_start is not None and age_end is not None:
         demographics["ageRange"] = {"start": age_start, "end": age_end}
@@ -55,6 +55,7 @@ def _c(location, gender, age_start, age_end, confirmed_date, travel=False, notes
     C = {
         "caseReference": _caseReference,
         "location": location,
+        "demographics": demographics,
         "events": [
             {
                 "name": "confirmed",
@@ -62,8 +63,8 @@ def _c(location, gender, age_start, age_end, confirmed_date, travel=False, notes
             }
         ],
     }
-    if demographics:
-        C["demographics"] = demographics
+    if not demographics:
+        del C["demographics"]
     if travel:
         C["travelHistory"] = {"traveledPrior30Days": True}
     if notes:
@@ -71,19 +72,25 @@ def _c(location, gender, age_start, age_end, confirmed_date, travel=False, notes
     return C
 
 
+"""
+Dataset (including sample_data) contains rows with Number_of_cases_reported.
+These are tested by duplicating rows below as counts should be imported as
+individuals.
+"""
 _CASES = [
-    _c(_NZ, "Male", 40, 49, "01/11/2021Z", True, _notes),
-    _c(_NZ, "Female", 40, 49, "01/11/2021Z", True, _notes),
-    _c(_NZ, "Male", 20, 29, "01/11/2021Z", True, _notes),
-    _c(_NZ, "Female", 10, 19, "01/11/2021Z", True, _notes),
-    _c(_NZ, "Male", 30, 39, "01/10/2021Z", True, _notes),
-    _c(_NZ, "Male", 0, 9, "01/10/2021Z", True, _notes),
-    _c(_NZ, "Male", 30, 39, "01/10/2021Z", True, _notes),
-    _c(_NZ, "Male", 20, 29, "01/10/2021Z", True, _notes),
-    _c(_Bay_of_Plenty, "Female", 90, 120, "05/05/2021Z", True, "Case imported from abroad."),
-    _c(_West_Coast, "Female", 20, 29, "05/06/2021Z", False),
-    _c(_West_Coast, None, None, None, "07/01/2021Z", False),
-    _c(_West_Coast, None, None, None, "07/01/2021Z", False),
+    _c(_NZ, "Male", 40, 49, "11/01/2021Z", True, _notes),
+    _c(_NZ, "Female", 40, 49, "11/01/2021Z", True, _notes),
+    _c(_NZ, "Male", 20, 29, "11/01/2021Z", True, _notes),
+    _c(_NZ, "Female", 10, 19, "11/01/2021Z", True, _notes),
+    _c(_NZ, "Male", 30, 39, "10/01/2021Z", True, _notes),
+    _c(_NZ, "Male", 0, 9, "10/01/2021Z", True, _notes),
+    _c(_NZ, "Male", 30, 39, "10/01/2021Z", True, _notes),
+    _c(_NZ, "Male", 20, 29, "10/01/2021Z", True, _notes),
+    _c(_Bay_of_Plenty, "Female", 90, 120, "05/05/2021Z", True,
+       "Case imported from abroad."),
+    _c(_West_Coast, "Female", 20, 29, "06/05/2021Z", False),
+    _c(_West_Coast, "Female", 20, 29, "06/05/2021Z", False),
+    _c(_West_Coast, "Female", 20, 29, "06/05/2021Z", False),
 ]
 
 
