0e3b9b1f19e4be5e37e0b418a0a2fd55094dba7a
John Brittain <jsb1982@gmail.com>
2023-04-17 15:25:18 +0100

    Update prune_db.py to permit selective pruning

--- data-serving/scripts/prune-uploads/prune_db.py
+++ data-serving/scripts/prune-uploads/prune_db.py
@@ -2,6 +2,7 @@ import os
 import sys
 import logging
 import pymongo
+from bson.objectid import ObjectId
 import argparse
 from time import time
 from logger import setup_logger
@@ -12,7 +13,42 @@ def prune_db():
     parser = argparse.ArgumentParser(
         description="Deletes cases marked list=False."
     )
-    parser.add_argument("--live", help="Live action", action="store_true")
+    parser.add_argument(
+        "--live",
+        help="Live action",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--get_removal_count",
+        help="Estimate removal count (can be very slow)",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--get_source_ids",
+        help="Query server for source ids (checks 'sources' collections)",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--get_source_ids_with_upload_counts",
+        help="Query server for source ids and also return upload counts",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--get_orphaned_source_ids",
+        help="Returns unique SourceIds from Cases (uses list=False)",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--get_upload_ids",
+        help="Query server for upload ids",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--sourceid",
+        help="SourceId for selective removal",
+        default=None,
+        type=str
+    )
     args = parser.parse_args()
 
     # make database connection
@@ -29,41 +65,120 @@ def prune_db():
 
     # removal criteria
     criteria = {"$or": [{"list": False}, {"list": {"$exists": False}}]}
+    sourceid = args.sourceid
+    if sourceid:
+        criteria = {"$and": [{"caseReference.sourceId": sourceid}, criteria]}
 
     # report attempted database query
     logging.info(f"Removal criteria is set to {criteria=}")
     logging.info(f"Database is set to {CONN}")
-    logging.info(f"Flag is set to {args.live=}")
+    logging.info(f"Flags are set to {args=}")
 
     # get removal statistics
     ts = time()
     count_total = db.cases.estimated_document_count()
     logging.info(f"Got case count: {count_total} ({(time()-ts):.3f} secs)")
 
-    ts = time()
-    count_listFalse = db.cases.count_documents(criteria)
-    logging.info(f"Got removal count: {count_listFalse} ({(time()-ts):.3f} secs)")
-    prc = 100 * count_listFalse / count_total
+    # get number of documents to remove (warning: whole collection query - likely to be very slow!)
+    if args.get_removal_count:
+        ts = time()
+        count_listFalse = db.cases.count_documents(criteria)
+        logging.info(f"Got removal count: {count_listFalse} ({(time()-ts):.3f} secs)")
+    else:
+        logging.warning(
+            "Skipping removal count estimation (can be very slow); "
+            "total records to be removed unknown."
+        )
+
+    # return a list of unique source IDs from the server
+    if args.get_source_ids:
+        ts = time()
+        if sourceid:
+            logging.error("Cannot specify a sourceid to get SourceIds")
+            sys.exit(1)
+        try:
+            response = db.sources.find()
+        except pymongo.errors.PyMongoError as e:
+            logging.error(f"Error getting upload IDs: {e}")
+            sys.exit(1)
+        items = [str(item["_id"]) for item in response]
+        logging.info(f"Source IDs (n={len(items)}): {items}")
+        logging.info(f"Query time ({(time()-ts):.3f} secs)")
+        return
+
+    # query all unique SourceIds from the 'cases' collection
+    if args.get_orphaned_source_ids:
+        ts = time()
+        if sourceid:
+            logging.error("Cannot specify a sourceid to get SourceIds")
+            sys.exit(1)
+        try:
+            response = db.cases.distinct("caseReference.sourceId", criteria)
+        except pymongo.errors.PyMongoError as e:
+            logging.error(f"Error getting upload IDs: {e}")
+            sys.exit(1)
+        items = [str(item) for item in response]
+        logging.info(f"Source IDs (n={len(items)}): {items}")
+        logging.info(f"Query time ({(time()-ts):.3f} secs)")
+        return
+
+    # return a list of unique source IDs from the server, with upload counts
+    if args.get_source_ids_with_upload_counts:
+        ts = time()
+        if sourceid:
+            logging.error("Cannot specify a sourceid to get SourceIds")
+            sys.exit(1)
+        try:
+            response = db.sources.find()
+        except pymongo.errors.PyMongoError as e:
+            logging.error(f"Error getting upload IDs: {e}")
+            sys.exit(1)
+        items = [(str(item["_id"]),
+                  db.sources.find({"_id": item["_id"]})[0]["name"],
+                  len(db.sources.find({"_id": item["_id"]})[0]["uploads"]))
+                 for item in response]
+        logging.info(f"Source IDs (n={len(items)}): {items}")
+        logging.info(f"Query time ({(time()-ts):.3f} secs)")
+        return
+
+    # return a list of unique upload IDs (can be filtered by sourceId)
+    if args.get_upload_ids:
+        ts = time()
+        if not sourceid:
+            logging.error("Must specify a sourceid to get UploadIds")
+            sys.exit(1)
+        try:
+            response = db.sources.find({"_id": ObjectId(sourceid)})
+        except pymongo.errors.PyMongoError as e:
+            logging.error(f"Error getting upload IDs: {e}")
+            sys.exit(1)
+        items = [str(item["_id"]) for item in response]
+        logging.info(f"Upload IDs (n={len(items)}): {items}")
+        logging.info(f"Query time ({(time()-ts):.3f} secs)")
+        return
 
     # perform records removal
-    if args.live:
-        logging.info("(LIVE) Sending delete query...")
-        logging.info(f"Removing {count_listFalse} of {count_total} records ({prc:.1f}%)")
-        try:
-            pass
-            ts = time()
-            response = db.cases.delete_many(criteria)
-            logging.info("(LIVE) Query completed (acknowledged: "
-                         f"{response.acknowledged}, "
-                         f"deleted_count={response.deleted_count}) ({(time()-ts):.3f} secs)")
-            ts = time()
-            updated_count_total = db.cases.count_documents({})
-            logging.info(f"(LIVE) Updated case count: {updated_count_total} ({(time()-ts):.3f} secs)")
-        except pymongo.errors.PyMongoError as e:
-            logging.info(f"(LIVE) Database ERROR: {e}")
-    else:
-        logging.info("(DRY-RUN) - No changes will be made")
-        logging.info(f"Removing {count_listFalse} of {count_total} records ({prc:.1f}%)")
+    try:
+        ts = time()
+        if args.live:
+            logging.info("(LIVE) Sending delete query...")
+            try:
+                response = db.cases.bulk_write([pymongo.DeleteMany(criteria)])
+            except pymongo.errors.BulkWriteError as bwe:
+                logging.addLevelNamerror("Error during Bulk delete operation:")
+                logging.error(bwe)
+            else:
+                logging.info("(LIVE) Result of Bulk delete operation:")
+                logging.info(response.bulk_api_result)
+        else:
+            logging.info("(DRY-RUN) - No changes will be made")
+        updated_count_total = db.cases.estimated_document_count()
+        logging.info(
+            f"Updated case count: {updated_count_total} ({(time()-ts):.3f} secs)"
+        )
+    except pymongo.errors.PyMongoError as e:
+        logging.info(f"Database ERROR: {e}")
+    if not args.live:
         logging.info("(DRY-RUN) - No changes made")
 
 
@@ -71,4 +186,4 @@ if __name__ == "__main__":
     setup_logger()
     logging.info("Starting database pruning...")
     prune_db()
-    logging.info("Work complete")
+    logging.info("Work complete.")
